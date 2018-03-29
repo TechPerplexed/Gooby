46c839bbf03920fbbcd1bd1966e22fbe3c0942ed
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-29 13:53:51 -0600

    Update menu.sh

--- install/menu.sh
+++ install/menu.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# Dooplex Menu
+# GooPlex Menu
 
 # --------------------------
 # Step #1: Define variables
 # --------------------------
 
+EDITOR=nano
+PASSWD=/etc/passwd
 STD='\033[0m'
 RED='\033[00;31m'
 GRN='\033[00;32m'
@@ -15,34 +17,39 @@ YLW='\033[00;33m'
 # -------------------------------
 
 pause(){
-  read -p "Press [Enter] key to continue..." fackEnterKey
+  read -p "Press [Enter] key to return to the menu..." fackEnterKey
 }
 
 # Option 1
-one(){
-  bash bash /opt/Dooplex/install/apps/menu.sh ;;
-  pause
+apps(){
+	bash /opt/GooPlex/install/apps/menu.sh
+    pause
 }
 
 # Option 2
-two(){
-  bash bash /opt/Dooplex/install/server/menu.sh ;;
-  pause
+server(){
+	bash /opt/GooPlex/install/server/menu.sh
+    pause
 }
 
 # Option 3
-three(){
-  bash bash /opt/Dooplex/install/misc/menu.sh ;;
-  pause
+misc(){
+	bash /opt/GooPlex/install/server/menu.sh
+    pause
+}
+
+# Option 4
+quit(){
+	clear
+    exit
 }
 
 # Function to display menus
 show_menus() {
 	clear
-	echo "-------------------------"	
-	echo " D O O P L E X - M E N U"
-	echo "-------------------------"
-	echo ""
+	echo "~~~~~~~~~~~~~~~~~~~~~"	
+	echo " M A I N - M E N U"
+	echo "~~~~~~~~~~~~~~~~~~~~~"
 	echo -e "${GRN}1.${STD} Install and maintain Apps"
 	echo -e "${GRN}2.${STD} Server maintenance"
 	echo -e "${GRN}3.${STD} Additional options"
@@ -52,12 +59,12 @@ show_menus() {
 # Read input from the keyboard and take a action
 read_options(){
 	local choice
-	read -p "Enter choice [ 1 - 4] " choice
+	read -p "Enter choice [ 1 - 3] " choice
 	case $choice in
-		1) one ;;
-		2) two ;;
-		3) three ;;
-		4) exit 0;;
+		1) apps ;;
+		2) server ;;
+		3) misc ;;
+		4) quit ;;
 		*) echo -e "${RED}Please select a valid option${STD}" && sleep 2
 	esac
 }
@@ -73,6 +80,6 @@ trap '' SIGINT SIGQUIT SIGTSTP
 while true
 do
  
-  show_menus
-  read_options
+	show_menus
+	read_options
 done
