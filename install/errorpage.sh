0133676bc44958f190c59eaff4a8ceb35ebf0f86
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 12:16:46 -0600

    Update errorpage.sh

--- install/errorpage.sh
+++ install/errorpage.sh
@@ -10,8 +10,8 @@ source /opt/GooPlex/install/variables.sh
 clear
 
 echo ""
-echo -e "${GRN}-------------------------------${STD}"
+echo -e "${GREEN}-------------------------------${STD}"
 echo " This option is not active yet "
-echo -e "${GRN}-------------------------------${STD}"
+echo -e "${GREEN}-------------------------------${STD}"
 echo ""
 PAUSE
