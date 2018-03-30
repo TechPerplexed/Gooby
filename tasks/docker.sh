9c86a052a512960690d383b2a32ea3e17d2f1b36
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 17:32:10 -0600

    Update docker.sh

--- tasks/docker.sh
+++ tasks/docker.sh
@@ -16,8 +16,8 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
 sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo curl -L https://raw.githubusercontent.com/docker/compose/1.19.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
