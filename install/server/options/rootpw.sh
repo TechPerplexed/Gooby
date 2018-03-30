ee7a8dc64f0b7c9c5cbe8850dab554bff608b872
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 10:27:39 -0600

    Update rootpw.sh

--- install/server/options/rootpw.sh
+++ install/server/options/rootpw.sh
@@ -6,26 +6,7 @@ function="change the root password"
 # Begin script
 # ------------
 
-STD='\033[0m'
-RED='\033[00;31m'
-GRN='\033[00;32m'
-YLW='\033[00;33m'
-BLU='\033[00;34m'
-
-pause(){
-  read -p "Press [Enter] key to return to the menu..." fackEnterKey
-}
-
-# --------------
-# Confirm action
-# --------------
-
-clear
-read -p "Are you sure you want to $function (y/N)? " -n 1 -r
-echo ""
-
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+source /opt/GooPlex/install/meta/instbeg.sh
 
 # -----------
 # Main script
