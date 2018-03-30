8f4901b4a956a68c00a31437e6e7fde5397b144e
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 17:15:28 -0600

    Create docker.sh

---
+++ tasks/docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt-get install \
+    apt-transport-https \
+    acl \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+sudo apt-get purge apparmor -y
+ 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+ 
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.19.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+sudo gpasswd -a $USER docker
+sudo setfacl -m user:$USER:rw /var/run/docker.sock
