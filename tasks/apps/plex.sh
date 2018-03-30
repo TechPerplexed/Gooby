7c47fef216b48b2b37eec889ddd03872d0f44f02
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 16:51:08 -0600

    Create plex.sh

---
+++ tasks/apps/plex.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+FUNCTION="install or update Rclone"
+
+# ---------
+# Variables
+# ---------
+
+source /opt/GooPlex/menus/variables.sh
+
+# Confirmation
+
+clear
+read -p "Are you sure you want to $FUNCTION (y/N)? " -n 1 -r
+echo ""
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+# -----------
+# Main script
+# -----------
+
+# Explanation
+
+clear
+echo -e "Coming soon"
+echo ""
+
+# Execution
+
+  REBOOT
+
+# ----------
+# Finalizing
+# ----------
+
+else
+
+  echo -e "You chose ${YELLOW}not${STD} to $FUNCTION"
+
+fi
+
+PAUSE
