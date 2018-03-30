201fa0bd8df57cbe7eb6a56746a74ade272c3e94
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 09:26:36 -0600

    Update rootpw.sh

--- install/server/options/rootpw.sh
+++ install/server/options/rootpw.sh
@@ -1,48 +1,16 @@
 #!/bin/bash
 
-# ----------------
-# Define variables
-# ----------------
-
 function="change the root password"
 
-STD='\033[0m'
-RED='\033[00;31m'
-GRN='\033[00;32m'
-YLW='\033[00;33m'
+/install/meta/instbeg.sh
 
-# Confirm
-
-clear
-read -p "Are you sure you want to $function (y/N)? " -n 1 -r
-echo ""
-
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-
-# --------------------
-# Main script function
-# --------------------
-
-# Dependencies
-
-# Open port
-
-# Installing
+# Begin code
 
   clear
   echo -e "Use a password generator to create a very strong root password"
   echo ""
   sudo -s passwd
 
-# ----------
-# Finalizing
-# ----------
+# End code
 
-# Clean up
-
-else
-
-  echo "You chose not to $function"
-
-fi
+/install/meta/instend.sh
