a2660feefcdc129c236908f1d72bf91905d7fc92
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 11:02:06 -0600

    Create variables.sh

---
+++ install/variables.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define colors
+
+STD=$(echo -en '\033[0m')
+RED=$(echo -en '\033[00;31m')
+GREEN=$(echo -en '\033[00;32m')
+YELLOW=$(echo -en '\033[00;33m')
+BLUE=$(echo -en '\033[00;34m')
+MAGENTA=$(echo -en '\033[00;35m')
+PURPLE=$(echo -en '\033[00;35m')
+CYAN=$(echo -en '\033[00;36m')
+LIGHTGRAY=$(echo -en '\033[00;37m')
+LRED=$(echo -en '\033[01;31m')
+LGREEN=$(echo -en '\033[01;32m')
+LYELLOW=$(echo -en '\033[01;33m')
+LBLUE=$(echo -en '\033[01;34m')
+LMAGENTA=$(echo -en '\033[01;35m')
+LPURPLE=$(echo -en '\033[01;35m')
+LCYAN=$(echo -en '\033[01;36m')
+WHITE=$(echo -en '\033[01;37m')
+
+# Define pause button
+
+PAUSE(){
+  read -p "Press [Enter] key to return to the menu..." fackEnterKey
+}
