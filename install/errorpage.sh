60f6cc12a58fb0b88afa04e99ca713e968b19668
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 12:10:59 -0600

    Create errorpage.sh

---
+++ install/errorpage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FUNCTION="Error Page"
+
+# ---------
+# Variables
+# ---------
+
+source /opt/GooPlex/install/variables.sh
+clear
+
+echo ""
+echo -e "${GRN}-------------------------------${STD}"
+echo " This option is not active yet "
+echo -e "${GRN}-------------------------------${STD}"
+echo ""
+PAUSE
