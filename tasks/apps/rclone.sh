4cddef80f883a3a4ef0a9bcbd6d7636cb9222817
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 16:15:52 -0600

    Create rclone.sh

---
+++ tasks/apps/rclone.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+FUNCTION="install or update Rclone"
+
+# ---------
+# Variables
+# ---------
+
+source /opt/GooPlex/menus/variables.sh
+
+# Confirmation
+
+clear
+read -p "Are you sure you want to $FUNCTION (y/N)? " -n 1 -r
+echo ""
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+# -----------
+# Main script
+# -----------
+
+# Explanation
+
+clear
+echo -e "Creating the user to do all ${CYAN}daily tasks${STD}"
+echo ""
+
+# Execution
+
+cd /tmp
+
+read -e -p "Choose "B" if you want to install the beta version: " -i "" choice
+
+case "$choice" in 
+  b|B ) curl https://rclone.org/install.sh | sudo bash -s beta ;;
+  * ) curl https://rclone.org/install.sh | sudo bash ;;
+esac
+
+cd ~
+clear
+
+if [ -e "/home/plexuser/.config/rclone/rclone.conf" ]
+
+then
+  echo "Rclone already configured, skipping"
+else
+  echo "Please follow the instructions to setup Rclone"
+  echo ""
+  sudo rclone config
+fi
+
+# ----------
+# Finalizing
+# ----------
+
+else
+
+  echo -e "You chose ${YELLOW}not${STD} to $FUNCTION"
+
+fi
+
+PAUSE
