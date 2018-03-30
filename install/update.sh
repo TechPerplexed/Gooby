ac3470cb1718d170e1110a8718888c1a0fc6cad5
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 14:10:12 -0600

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -1,4 +1,14 @@
 cd ~
 sudo rm -r /opt/GooPlex
 git clone https://github.com/TechPerplexed/GooPlex /opt/GooPlex
+
+clear
+
+echo -e "${LMAGENTA}"
+echo -e "---------------------------------------------------"
+echo -e "  GooPlex has been updated to the latest version  "
+echo -e "---------------------------------------------------"
+echo -e "${STD}"
+PAUSE
+
 gooplex
