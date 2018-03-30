5d6b39433138bfbeb6e57342876663bb8afca1f0
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 14:17:16 -0600

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -1,14 +1,37 @@
+#!/bin/bash
+
+FUNCTION="Update GooPlex"
+
+
+#!/bin/bash
+
+FUNCTION="update VPS and prepare server"
+
+# ---------
+# Variables
+# ---------
+
+source /opt/GooPlex/menus/variables.sh
+clear
+
+# -----------
+# Main script
+# -----------
+
 cd ~
 sudo rm -r /opt/GooPlex
 git clone https://github.com/TechPerplexed/GooPlex /opt/GooPlex
 
 clear
 
+# -----------
+# Explanation
+# -----------
+
 echo -e "${LMAGENTA}"
 echo -e "---------------------------------------------------"
 echo -e "  GooPlex has been updated to the latest version  "
 echo -e "---------------------------------------------------"
 echo -e "${STD}"
-PAUSE
 
-gooplex
+PAUSE
