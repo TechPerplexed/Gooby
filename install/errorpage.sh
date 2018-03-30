c2a3c9cc4730c5b16da1bbf63b81da5a80788e22
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 12:18:31 -0600

    Update errorpage.sh

--- install/errorpage.sh
+++ install/errorpage.sh
@@ -9,9 +9,9 @@ FUNCTION="Error Page"
 source /opt/GooPlex/install/variables.sh
 clear
 
-echo ""
-echo -e "${GREEN}-------------------------------${STD}"
-echo " This option is not active yet "
-echo -e "${GREEN}-------------------------------${STD}"
-echo ""
+echo -e "${RED}"
+echo -e "-------------------------------"
+echo -e " This option is not active yet "
+echo -e "-------------------------------"
+echo -e "${STD}"
 PAUSE
