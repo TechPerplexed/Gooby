cd76bfe798c7e1b439b0eb5e09fb052ed8d988d1
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 14:07:03 -0600

    Update misc.sh

--- menus/misc.sh
+++ menus/misc.sh
@@ -1,1 +1,80 @@
-echo "Coming soon!"
+#!/bin/bash
+
+FUNCTION="Additional Tasks"
+
+# ---------
+# Variables
+# ---------
+
+source /opt/GooPlex/install/variables.sh
+clear
+
+# ------------
+# Menu Options
+# ------------
+
+# Update GooPlex
+SERVER(){
+  bash /opt/GooPlex/install/update.sh
+}
+
+# Coming soon
+MISC1(){
+  bash /opt/GooPlex/menus/errorpage.sh
+}
+
+# Additional Options
+MISC2(){
+  bash /opt/GooPlex/menus/errorpage.sh
+}
+
+# Exit
+QUIT(){
+  exit
+}
+
+# ------------
+# Display menu
+# ------------
+
+show_menus() {
+  clear
+  echo -e " ${CYAN}"
+  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo -e " $FUNCTION "
+  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo -e " ${STD}"
+  echo -e " ${GRN}A${STD} - Update GooPlex"
+  echo -e " ${GRN}B${STD} - Coming Soon"
+  echo -e " ${GRN}C${STD} - Coming Soon"
+  echo -e " ${YLW}Q${STD} - Exit $FUNCTION"
+  echo -e ""
+}
+
+# ------------
+# Read Choices
+# ------------
+
+read_options(){
+  local choice
+    read -p "Choose option: " choice
+    case $choice in
+      [Aa]) SERVER ;;
+      [Bb]) MISC1 ;;
+      [Cc]) MISC2 ;;
+      [Qq]) QUIT ;;
+      *) echo -e "${RED}Please select a valid option${STD}" && sleep 2
+    esac
+}
+ 
+# ----------
+# Finalizing
+# ----------
+
+trap '' SIGINT SIGQUIT SIGTSTP
+
+while true
+do 
+  show_menus
+  read_options
+done
