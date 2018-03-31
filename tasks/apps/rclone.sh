fbc5a677f66d960b49bbfc6011f93592194145f4
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-31 12:20:51 -0600

    Update rclone.sh

--- tasks/apps/rclone.sh
+++ tasks/apps/rclone.sh
@@ -17,29 +17,6 @@ echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-# -----------
-# Main script
-# -----------
-
-#!/bin/bash
-
-FUNCTION="install or update Rclone"
-
-# ---------
-# Variables
-# ---------
-
-source /opt/GooPlex/menus/variables.sh
-
-# Confirmation
-
-clear
-read -p "Are you sure you want to $FUNCTION (y/N)? " -n 1 -r
-echo ""
-
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-
 # ----------
 # Open ports
 # ----------
@@ -56,50 +33,6 @@ sudo apt-get upgrade -y && sudo apt-get upgrade -y
 # Main script
 # -----------
 
-# Explanation
-
-clear
-echo -e "Please read the options carefully and follow the instructions"
-echo ""
-
-# Execution
-
-if [ -d "/var/lib/plexmediaserver" ];
-
-then
-
-  /opt/plexupdate/extras/installer.sh
-
-else
-
-  cd /tmp
-  sudo apt-get upgrade -y && sudo apt-get upgrade -y
-  bash -c "$(wget -qO - https://raw.githubusercontent.com/mrworf/plexupdate/master/extras/installer.sh)"
-  cd ~
-
-fi
-
-# ----------
-# Finalizing
-# ----------
-
-else
-
-  echo -e "You chose ${YELLOW}not${STD} to $FUNCTION"
-
-fi
-
-PAUSE
-
-
-# Explanation
-
-clear
-echo -e "This will " $FUNCTION
-echo ""
-
-# Execution
-
 cd /tmp
 
 read -e -p "Release (R) or Beta installation (B)? " -i "R" choice
@@ -115,11 +48,15 @@ clear
 if [ -e "/home/plexuser/.config/rclone/rclone.conf" ]
 
 then
+
   echo "Rclone already configured, skipping"
+  
 else
+
   echo "Please follow the instructions to setup Rclone"
   echo ""
   sudo rclone config
+  
 fi
 
 # -------------------
