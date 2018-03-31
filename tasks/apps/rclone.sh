b16fe60aed91ac8db2619911c83d34330427e183
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 19:21:59 -0600

    Update rclone.sh

--- tasks/apps/rclone.sh
+++ tasks/apps/rclone.sh
@@ -21,6 +21,76 @@ then
 # Main script
 # -----------
 
+#!/bin/bash
+
+FUNCTION="install or update Plex"
+
+# ---------
+# Variables
+# ---------
+
+source /opt/GooPlex/menus/variables.sh
+
+# Confirmation
+
+clear
+read -p "Are you sure you want to $FUNCTION (y/N)? " -n 1 -r
+echo ""
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+# ----------
+# Open ports
+# ----------
+
+# None
+
+# ------------
+# Dependencies
+# ------------
+
+# None
+
+# -----------
+# Main script
+# -----------
+
+# Explanation
+
+clear
+echo -e "Please read the options carefully and follow the instructions"
+echo ""
+
+# Execution
+
+if [ -d "/var/lib/plexmediaserver" ];
+
+then
+
+  /opt/plexupdate/extras/installer.sh
+
+else
+
+  cd /tmp
+  bash -c "$(wget -qO - https://raw.githubusercontent.com/mrworf/plexupdate/master/extras/installer.sh)"
+  cd ~
+
+fi
+
+# ----------
+# Finalizing
+# ----------
+
+else
+
+  echo -e "You chose ${YELLOW}not${STD} to $FUNCTION"
+
+fi
+
+PAUSE
+
+
 # Explanation
 
 clear
