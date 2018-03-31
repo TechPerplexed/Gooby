8b8017c3526637acbc1b47a7495b178a013168be
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-31 10:48:15 -0600

    Update update.sh

--- install/update.sh
+++ install/update.sh
@@ -16,7 +16,7 @@ clear
 cd ~
 sudo rm -r /opt/GooPlex
 git clone https://github.com/TechPerplexed/GooPlex /opt/GooPlex
-sudo bash /opt/GooPlex/tasks/setup.sh
+sudo bash /opt/GooPlex/install/setup.sh
 
 clear
 
