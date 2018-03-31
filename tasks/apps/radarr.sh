84506491a30ad79d629883a8a9f45311b9b8c863
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 19:36:01 -0600

    Create radarr.sh

---
+++ tasks/apps/radarr.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+FUNCTION="install or update Radarr"
+
+# ---------
+# Variables
+# ---------
+
+source /opt/GooPlex/menus/variables.sh
+
+# Confirmation
+
+clear
+read -p "Are you sure you want to $FUNCTION (y/N)? " -n 1 -r
+echo ""
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+# ----------
+# Open ports
+# ----------
+
+sudo ufw allow 7878
+
+# ------------
+# Dependencies
+# ------------
+
+sudo apt-get upgrade -y && sudo apt-get upgrade -y
+
+sudo apt-get -y install \
+  libcurl3 \
+  libmono-cil-dev \
+  mono-devel \
+  mediainfo \
+  denyhosts at sudo software-properties-common
+
+# -----------
+# Main script
+# -----------
+
+# Execution
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian jessie main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+
+cd /tmp
+clear
+echo "Copy Linux version from https://github.com/Radarr/Radarr/releases/"
+read -e -p "Paste link to Radarr.verson.linux.tar.gz: " -i "https://github.com/Radarr/Radarr/releases/download/v0.2.0.995/Radarr.develop.0.2.0.995.linux.tar.gz" radarr
+wget $radarr
+
+sudo tar -xf Radarr* -C /opt/
+
+# ----------
+# Finalizing
+# ----------
+
+else
+
+  echo -e "You chose ${YELLOW}not${STD} to $FUNCTION"
+
+fi
+
+PAUSE
