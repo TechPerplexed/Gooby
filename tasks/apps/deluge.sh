64d6a93ddab8cc7dc3311a5e818d5681d3357928
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-31 12:04:04 -0600

    Update deluge.sh

--- tasks/apps/deluge.sh
+++ tasks/apps/deluge.sh
@@ -57,8 +57,8 @@ echo "Service already configured, skipping"
 
 else
 
-sudo rsync -a /opt/GooPlex/scripts/etc/systemd/system/rclone.service /etc/systemd/system/deluged.service
-sudo rsync -a /opt/GooPlex/scripts/etc/systemd/system/rclone.service /etc/systemd/system/deluge-web.service
+sudo rsync -a /opt/GooPlex/scripts/services/rclone.service /etc/systemd/system/deluged.service
+sudo rsync -a /opt/GooPlex/scripts/services/rclone.service /etc/systemd/system/deluge-web.service
 
 sudo systemctl enable deluged.service
 sudo systemctl enable deluge-web.service
