7f75697c78606db581bdb4276f06dfb1d0c02e9b
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-03-30 19:38:11 -0600

    Create netdata.sh

---
+++ tasks/apps/netdata.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+FUNCTION="install or update Netdata"
+
+# ---------
+# Variables
+# ---------
+
+source /opt/GooPlex/menus/variables.sh
+
+# Confirmation
+
+clear
+read -p "Are you sure you want to $FUNCTION (y/N)? " -n 1 -r
+echo ""
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+# ----------
+# Open ports
+# ----------
+
+sudo ufw allow 19999
+
+# ------------
+# Dependencies
+# ------------
+
+sudo apt-get upgrade -y && sudo apt-get upgrade -y
+
+# None
+
+# -----------
+# Main script
+# -----------
+
+# Execution
+
+clear
+bash <(curl -Ss https://my-netdata.io/kickstart.sh)
+
+# ----------
+# Finalizing
+# ----------
+
+else
+
+  echo -e "You chose ${YELLOW}not${STD} to $FUNCTION"
+
+fi
+
+PAUSE
