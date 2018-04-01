fc22a41de6186155a199babbe25ce2e3050c204e
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-04-01 13:52:58 -0600

    Update radarr.sh

--- install/apps/radarr.sh
+++ install/apps/radarr.sh
@@ -17,66 +17,65 @@ echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-# ----------
-# Open ports
-# ----------
+  # ----------
+  # Open ports
+  # ----------
 
-sudo ufw allow 7878
+  sudo ufw allow 7878
 
-# ------------
-# Dependencies
-# ------------
+  # ------------
+  # Dependencies
+  # ------------
 
-sudo apt-get upgrade -y && sudo apt-get upgrade -y
+  sudo apt-get upgrade -y && sudo apt-get upgrade -y
 
-sudo -s apt-get -y install \
-  libcurl3 \
-  libmono-cil-dev \
-  mono-devel \
-  mediainfo \
-  sqlite3 \
-  denyhosts at sudo software-properties-common
+  sudo -s apt-get -y install \
+    libcurl3 \
+    libmono-cil-dev \
+    mono-devel \
+    mediainfo \
+    sqlite3 \
+    denyhosts at sudo software-properties-common
 
-# -----------
-# Main script
-# -----------
+  # -----------
+  # Main script
+  # -----------
 
-# Execution
+  # Execution
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/debian jessie main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+  echo "deb http://download.mono-project.com/repo/debian jessie main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 
-cd /tmp
-clear
-echo "Copy Linux version from https://github.com/Radarr/Radarr/releases/"
-read -e -p "Paste link to Radarr.verson.linux.tar.gz: " -i "https://github.com/Radarr/Radarr/releases/download/v0.2.0.995/Radarr.develop.0.2.0.995.linux.tar.gz" radarr
-wget $radarr
+  cd /tmp
+  clear
+  echo "Copy Linux version from https://github.com/Radarr/Radarr/releases/"
+  read -e -p "Paste link to Radarr.verson.linux.tar.gz: " -i "https://github.com/Radarr/Radarr/releases/download/v0.2.0.995/Radarr.develop.0.2.0.995.linux.tar.gz" radarr
+  wget $radarr
 
-sudo tar -xf Radarr* -C /opt/
-sudo chown -R plexuser:plexuser /opt/Radarr
+  sudo tar -xf Radarr* -C /opt/
+  sudo chown -R plexuser:plexuser /opt/Radarr
 
-# -------------------
-# Installing Services
-# -------------------
+  # -------------------
+  # Installing Services
+  # -------------------
 
-if [ -e "/etc/systemd/system/radarr.service" ]
+  if [ -e "/etc/systemd/system/radarr.service" ]
+  then
 
-then
+    echo "Service already configured, skipping"
 
-echo "Service already configured, skipping"
+  else
 
-else
+    sudo rsync -a /opt/GooPlex/scripts/services/radarr.service /etc/systemd/system/radarr.service
+    sudo systemctl enable radarr.service
+    sudo systemctl daemon-reload
+    sudo systemctl start radarr.service
 
-sudo rsync -a /opt/GooPlex/scripts/services/radarr.service /etc/systemd/system/radarr.service
-sudo systemctl enable radarr.service
-sudo systemctl daemon-reload
-sudo systemctl start radarr.service
+  fi
 
-fi
-
-# ----------
-# Finalizing
-# ----------
+  # ----------
+  # Finalizing
+  # ----------
 
 else
 
