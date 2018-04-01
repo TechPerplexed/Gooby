66324a141dc2da99cffe053dffe1054a945865fb
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-04-01 13:57:15 -0600

    Update rclone.sh

--- install/apps/rclone.sh
+++ install/apps/rclone.sh
@@ -17,70 +17,68 @@ echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-# ----------
-# Open ports
-# ----------
+  # ----------
+  # Open ports
+  # ----------
 
-# None
+  # None
 
-# ------------
-# Dependencies
-# ------------
+  # ------------
+  # Dependencies
+  # ------------
 
-sudo apt-get upgrade -y && sudo apt-get upgrade -y
+  sudo apt-get upgrade -y && sudo apt-get upgrade -y
 
-# -----------
-# Main script
-# -----------
+  # -----------
+  # Main script
+  # -----------
 
-cd /tmp
+  cd /tmp
 
-read -e -p "Release (R) or Beta installation (B)? " -i "R" choice
+  read -e -p "Release (R) or Beta installation (B)? " -i "R" choice
 
-case "$choice" in 
-  b|B ) curl https://rclone.org/install.sh | sudo bash -s beta ;;
-  * ) curl https://rclone.org/install.sh | sudo bash ;;
-esac
+  case "$choice" in 
+    b|B ) curl https://rclone.org/install.sh | sudo bash -s beta ;;
+    * ) curl https://rclone.org/install.sh | sudo bash ;;
+  esac
 
-cd ~
-clear
+  cd ~
+  clear
 
-if [ -e "/home/plexuser/.config/rclone/rclone.conf" ]
+  if [ -e "/home/plexuser/.config/rclone/rclone.conf" ]
+  then
 
-then
+    echo "Rclone already configured, skipping"
 
-  echo "Rclone already configured, skipping"
-  
-else
+  else
 
-  echo "Please follow the instructions to setup Rclone"
-  echo ""
-  sudo rclone config
-  
-fi
+    echo "Please follow the instructions to setup Rclone"
+    echo ""
+    sudo rclone config
 
-# -------------------
-# Installing Services
-# -------------------
+  fi
 
-if [ -e "/etc/systemd/system/rclone.service" ]
+  # -------------------
+  # Installing Services
+  # -------------------
 
-then
+  if [ -e "/etc/systemd/system/rclone.service" ]
+  then
 
-  echo "Service already configured, skipping"
+    echo "Service already configured, skipping"
 
-else
+  else
 
-  sudo rsync -a /opt/GooPlex/scripts/services/rclone.service /etc/systemd/system/rclone.service
-  sudo systemctl enable rclone.service
-  sudo systemctl daemon-reload
-  source /opt/GooPlex/install/reboot.sh
+    sudo rsync -a /opt/GooPlex/scripts/services/rclone.service /etc/systemd/system/rclone.service
+    sudo systemctl enable rclone.service
+    sudo systemctl daemon-reload
+    source /opt/GooPlex/install/reboot.sh
 
-fi
+  fi
 
-# ----------
-# Finalizing
-# ----------
+  # ----------
+  # Finalizing
+  # ----------
 
 else
 
