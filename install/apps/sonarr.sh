668fddba88e036a2d3711ed715f7c96c651f516e
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-04-02 08:26:49 -0600

    Update sonarr.sh

--- install/apps/sonarr.sh
+++ install/apps/sonarr.sh
@@ -17,51 +17,54 @@ echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 
-  # ----------
-  # Open ports
-  # ----------
-
-  sudo ufw allow 8989
-
-  # ------------
-  # Dependencies
-  # ------------
-
-  sudo apt-get upgrade -y && sudo apt-get upgrade -y
-
-  sudo -s apt-get -y install \
-    libcurl3 \
-    libmono-cil-dev \
-    mono-devel \
-    mediainfo \
-    sqlite3 \
-    denyhosts at sudo software-properties-common
-
-  # -----------
-  # Main script
-  # -----------
-
-  # Execution
-
-  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
-  sudo echo "deb http://apt.sonarr.tv/ master main" | sudo tee /etc/apt/sources.list.d/sonarr.list
-
-  sudo apt-get -y update
-  clear
-  sudo apt-get -y install nzbdrone
-
-  sudo chown -R plexuser:plexuser /opt/NzbDrone
-
-  # -------------------
-  # Installing Services
-  # -------------------
-
-  if [ -e "/etc/systemd/system/sonarr.service" ]
+  if [ ! -d "/opt/NzbDrone" ]
   then
 
-    echo "Service already configured, skipping"
+    # ----------
+    # Open ports
+    # ----------
+
+    sudo ufw allow 8989
+
+    # ------------
+    # Dependencies
+    # ------------
+
+    sudo apt-get upgrade -y && sudo apt-get upgrade -y
+
+    sudo -s apt-get -y install \
+      libcurl3 \
+      libmono-cil-dev \
+      mono-devel \
+      mediainfo \
+      sqlite3 \
+      denyhosts at sudo software-properties-common
+
+    # -----------
+    # Main script
+    # -----------
+
+    # Execution
+
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
+    sudo echo "deb http://apt.sonarr.tv/ master main" | sudo tee /etc/apt/sources.list.d/sonarr.list
+
+    sudo apt-get -y update
+    clear
+    sudo apt-get -y install nzbdrone
+
+    sudo chown -R plexuser:plexuser /opt/NzbDrone
 
   else
+
+    clear
+    echo -e "Sonarr is already installed!"
+    echo -e "You can update it from within the application itself."
+ 
+  fi
+
+  if [ ! -e "/etc/systemd/system/sonarr.service" ]
+  then
 
     sudo rsync -a /opt/GooPlex/scripts/sonarr.service /etc/systemd/system/sonarr.service
     sudo systemctl enable sonarr.service
