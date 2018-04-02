f3781e46e860a93b4dcea16fa7469797057ce372
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-04-01 19:32:36 -0600

    Update deluge.sh

--- install/apps/deluge.sh
+++ install/apps/deluge.sh
@@ -52,8 +52,8 @@ then
 
   else
 
-    sudo rsync -a /opt/GooPlex/scripts/services/deluged.service /etc/systemd/system/deluged.service
-    sudo rsync -a /opt/GooPlex/scripts/services/deluge-web.service /etc/systemd/system/deluge-web.service
+    sudo rsync -a /opt/GooPlex/scripts/deluged.service /etc/systemd/system/deluged.service
+    sudo rsync -a /opt/GooPlex/scripts/deluge-web.service /etc/systemd/system/deluge-web.service
 
     sudo systemctl enable deluged.service
     sudo systemctl enable deluge-web.service
