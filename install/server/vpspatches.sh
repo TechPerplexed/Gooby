b4217d6b311d5f808d0efd4e609af9978d0d8a3a
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-05-26 10:05:37 -0600

    Update vpspatches.sh

--- install/server/vpspatches.sh
+++ install/server/vpspatches.sh
@@ -31,10 +31,12 @@ then
 
   # Initial update
 
+  sudo apt-get update
   sudo apt-get upgrade -y
-  sudo apt-get upgrade -y
-  sudo apt-get full-upgrade -y
-  sudo apt-get autoremove -y
+  sudo apt-get dist-upgrade -y
+  sudo apt autoremove -y
+  sudo apt autoclean
+  sudo apt-get autoremove
 
 else
 
