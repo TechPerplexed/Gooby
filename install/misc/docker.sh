92cf9bc01045eca135253aa618a1ff3cf9bc5311
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-08-16 13:12:21 -0600

    Update docker.sh

--- install/misc/docker.sh
+++ install/misc/docker.sh
@@ -2,27 +2,22 @@
 
 # For future use
 
-sudo apt-get -y install \
+sudo apt-get install \
     apt-transport-https \
+
     acl \
+
     ca-certificates \
     curl \
     software-properties-common
 
-sudo apt-get purge apparmor -y
- 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
- 
+
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get -y update
-sudo apt-get -y install docker-ce
-sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo curl -L https://raw.githubusercontent.com/docker/compose/1.19.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+sudo apt-get update
 
-sudo gpasswd -a $USER docker
-sudo setfacl -m user:$USER:rw /var/run/docker.sock
+sudo apt-get install -y docker-ce
