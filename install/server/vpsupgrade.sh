1e3731b8ef5a083832182bc85ba88a7de6c7848e
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 13:28:16 -0600

    Update vpsupgrade.sh

--- install/server/vpsupgrade.sh
+++ install/server/vpsupgrade.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 
-FUNCTION="upgrade the server"
-
-# ---------
-# Variables
-# ---------
-
-source /opt/GooPlex/menus/variables.sh
 clear
 
 # Explanation
 
-echo -e "-----------------------------------------------------"
-echo -e "                   ${RED}DANGER ZONE!!!${STD}                    "
-echo -e " This will upgrade your server to the latest version "
-echo -e "       ${YELLOW}CAUTION!!!${STD} Make SURE you have a backup!       "
-echo -e "-----------------------------------------------------"
+echo -e "--------------------------------------------------"
+echo -e " ${RED}DANGER ZONE!!!${STD}"
+echo -e " This will upgrade your server to the latest version"
+echo -e " ${YELLOW}CAUTION!!!${STD} Make SURE you have a backup!"
+echo -e "--------------------------------------------------"
 echo ""
 
 # Confirmation
 
-read -p "Are you ${WHITE}REALLY${STD} sure you want to $FUNCTION (y/N)? " -n 1 -r
+read -p "Are you sure you want to ${PERFORM} ${TASK} (y/N)? " -n 1 -r
 echo ""
 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
-  # -----------
-  # Main script
-  # -----------
-
-  # Initial update
+  # Upgrade
 
   sudo apt-get update
   sudo apt-get upgrade -y
@@ -41,10 +29,21 @@ then
   sudo apt install update-manager-core -y
   sudo do-release-upgrade
 
+  # Task Completed
+
+  echo -e "${LMAGENTA}"
+  echo -e "--------------------------------------------------"
+  echo -e " ${PERFORM} $TASK completed"
+  echo -e "--------------------------------------------------"
+  echo -e "${STD}"
+
 else
 
   echo ""
-  echo -e "You chose ${YELLOW}not${STD} to $FUNCTION"
+  echo -e "--------------------------------------------------"
+  echo -e "You chose ${YELLOW}not${STD} to ${PERFORM} ${TASK}"
+  echo -e "--------------------------------------------------"
+  echo ""
 
 fi
 
