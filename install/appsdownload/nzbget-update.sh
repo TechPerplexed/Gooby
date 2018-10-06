258ffb06e1df568470ae2722abf45ca6c1b15887
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-05 19:19:33 -0600

    Create nzbget-update.sh

---
+++ install/appsdownload/nzbget-update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+FUNCTION="update NZBGet"
+
+# ---------
+# Variables
+# ---------
+
+source /opt/GooPlex/menus/variables.sh
+
+# Confirmation
+
+clear
+read -p "Are you sure you want to $FUNCTION (y/N)? " -n 1 -r
+echo ""
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+echo -e "Coming soon!"
+
+else
+
+  echo -e "You chose ${YELLOW}not${STD} to $FUNCTION"
+
+fi
+
+PAUSE
