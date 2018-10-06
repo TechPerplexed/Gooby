fe0b84ab21830eba3bfe8af86e57782229da9166
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 12:35:11 -0600

    Update update.sh

--- install/misc/update.sh
+++ install/misc/update.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
 clear
+read -p "Are you sure you want to ${PERFORM} ${FUNCTION} (y/N)? " -n 1 -r
+echo ""
 
-# -----------
-# Main script
-# -----------
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
-cd ~
-sudo rm -r /opt/GooPlex
-sudo git clone https://github.com/TechPerplexed/GooPlex /opt/GooPlex
-sudo chmod +x -R /opt/GooPlex/install
-sudo chmod +x -R /opt/GooPlex/menus
-sudo rsync -a /opt/GooPlex/install/gooplex /bin
-sudo chmod 755 /bin/gooplex
+  # -----------
+  # Main script
+  # -----------
 
-clear
+  cd ~
+  sudo rm -r /opt/GooPlex
+  sudo git clone https://github.com/TechPerplexed/GooPlex /opt/GooPlex
+  sudo chmod +x -R /opt/GooPlex/install
+  sudo chmod +x -R /opt/GooPlex/menus
+  sudo rsync -a /opt/GooPlex/install/gooplex /bin
+  sudo chmod 755 /bin/gooplex
 
-# -----------
-# Explanation
-# -----------
+  clear
 
-echo -e "${LMAGENTA}"
-echo -e "------------------------------------------------"
-echo -e " GooPlex has been updated to the latest version "
-echo -e "------------------------------------------------"
-echo -e "${STD}"
+  # -----------
+  # Explanation
+  # -----------
+
+  echo -e "${LMAGENTA}"
+  echo -e "------------------------------------------------"
+  echo -e " GooPlex has been updated to the latest version "
+  echo -e "------------------------------------------------"
+  echo -e "${STD}"
+
+else
+
+  echo -e "You chose ${YELLOW}not${STD} to ${PERFORM} ${FUNCTION}"
+
+fi
 
 PAUSE
