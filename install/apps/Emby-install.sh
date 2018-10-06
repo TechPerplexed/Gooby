e9c41065f566dd30a23db16b9aeeb971207fefd9
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 11:40:11 -0600

    Create Emby-install.sh

---
+++ install/apps/Emby-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+read -p "Are you sure you want to ${PERFORM} ${FUNCTION} (y/N)? " -n 1 -r
+echo ""
+
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+  echo ""
+  echo -e "Coming soon!"
+
+else
+
+  echo -e "You chose ${YELLOW}not${STD} to ${PERFORM} ${FUNCTION}"
+
+fi
+
+PAUSE
