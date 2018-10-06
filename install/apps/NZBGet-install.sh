b9ad6b680940b57515a05fca2142d26ae6b2b224
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 14:56:40 -0600

    Update NZBGet-install.sh

--- install/apps/NZBGet-install.sh
+++ install/apps/NZBGet-install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 clear
-read -p "Are you sure you want to ${PERFORM} ${FUNCTION} (y/N)? " -n 1 -r
+
+# Explanation
+
+echo -e "--------------------------------------------------"
+echo -e " This will ${PERFORM} $TASK}"
+echo -e " You probably only need to run this once..."
+echo -e "--------------------------------------------------"
+echo ""
+
+# Confirmation
+
+read -p " Are you sure you want to ${PERFORM} ${TASK} (y/N)? " -n 1 -r
 echo ""
 
 if [[ ${REPLY} =~ ^[Yy]$ ]]; then
@@ -9,9 +20,21 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
   echo ""
   echo -e "Coming soon!"
 
+  # Task Completed
+
+  echo -e "${LMAGENTA}"
+  echo -e "--------------------------------------------------"
+  echo -e " ${PERFORM} $TASK completed"
+  echo -e "--------------------------------------------------"
+  echo -e "${STD}"
+
 else
 
-  echo -e "You chose ${YELLOW}not${STD} to ${PERFORM} ${FUNCTION}"
+  echo ""
+  echo -e "--------------------------------------------------"
+  echo -e " You chose ${YELLOW}not${STD} to ${PERFORM} ${TASK}"
+  echo -e "--------------------------------------------------"
+  echo ""
 
 fi
 
