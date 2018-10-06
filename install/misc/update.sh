1e42c36c197f70c7eb3109675aaea6bba59e0200
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 12:40:08 -0600

    Update update.sh

--- install/misc/update.sh
+++ install/misc/update.sh
@@ -26,7 +26,7 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
   echo -e "${LMAGENTA}"
   echo -e "------------------------------------------------"
-  echo -e " $FUNCTION has been updated to the latest version "
+  echo -e " ${PERFORM} $FUNCTION completed "
   echo -e "------------------------------------------------"
   echo -e "${STD}"
 
