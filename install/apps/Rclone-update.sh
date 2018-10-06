a3acf85fa733cb0bb453cd6d9c3c4fc949b925fa
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 12:16:38 -0600

    Update Rclone-update.sh

--- install/apps/Rclone-update.sh
+++ install/apps/Rclone-update.sh
@@ -1,40 +1,10 @@
 #!/bin/bash
 
 clear
-read -p "Are you sure you want to $PERFORM $FUNCTION (y/N)? " -n 1 -r
+read -p "Are you sure you want to ${PERFORM} ${FUNCTION} (y/N)? " -n 1 -r
 echo ""
 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-
-  # ----------
-  # Open ports
-  # ----------
-
-  # None
-
-  # ------------
-  # Dependencies
-  # ------------
-
-  sudo apt-get upgrade -y && sudo apt-get upgrade -y
-  sudo -s apt-get -y install \
-      unzip \
-      curl \
-      fuse \
-  denyhosts at sudo software-properties-common
-  
-  # ------------------
-  # Create directories
-  # ------------------
-  
-  mkdir -p /home/plexuser/logs
-  mkdir -p /home/plexuser/uploads
-  sudo chown plexuser:plexuser -R /home/plexuser
-
-  # -----------
-  # Main script
-  # -----------
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
   cd /tmp
 
@@ -46,38 +16,10 @@ then
   esac
 
   cd ~
-  clear
-
-  if [ ! -e "/home/plexuser/.config/rclone/rclone.conf" ]
-  then
-
-    echo "Please follow the instructions to setup Rclone"
-    echo ""
-    sudo rclone config
-
-  fi
-
-  # -------------------
-  # Installing Services
-  # -------------------
-
-  if [ ! -e "/etc/systemd/system/rclone.service" ]
-  then
-
-    sudo rsync -a /opt/GooPlex/scripts/rclone.service /etc/systemd/system/rclone.service
-    sudo systemctl enable rclone.service
-    sudo systemctl daemon-reload
-    sudo systemctl start rclone.service
-
-  fi
-
-  # ----------
-  # Finalizing
-  # ----------
 
 else
 
-  echo -e "You chose ${YELLOW}not${STD} to $PERFORM $FUNCTION"
+  echo -e "You chose ${YELLOW}not${STD} to ${PERFORM} ${FUNCTION}"
 
 fi
 
