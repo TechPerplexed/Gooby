0be02b6ead4503b8aab680c84028a58b0b0f7362
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 13:32:56 -0600

    Update vpsupdate.sh

--- install/server/vpsupdate.sh
+++ install/server/vpsupdate.sh
@@ -14,7 +14,7 @@ echo ""
 
 # Confirmation
 
-read -p "Are you sure you want to ${PERFORM} ${TASK} (y/N)? " -n 1 -r
+read -p " Are you sure you want to ${PERFORM} ${TASK} (y/N)? " -n 1 -r
 echo ""
 
 if [[ ${REPLY} =~ ^[Yy]$ ]]; then
@@ -78,23 +78,23 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
     # Change root password
 
     clear
-    read -p "Change root password (y/N)? " -n 1 -r
+    read -p " Change root password (y/N)? " -n 1 -r
     echo ""
 
       if [[ $REPLY =~ ^[Yy]$ ]]
       then
 
         clear
-        echo -e "Create a ${CYAN}very strong${STD} root password"
-        echo -e "The best way is to use a ${CYAN}password generator${STD}"
-        echo -e "Then make sure to store your password in a ${CYAN}safe place${STD}"
+        echo -e " Create a ${CYAN}very strong${STD} root password"
+        echo -e " The best way is to use a ${CYAN}password generator${STD}"
+        echo -e " Then make sure to store your password in a ${CYAN}safe place${STD}"
         echo ""
 
         sudo -s passwd
 
       else
 
-        echo -e "You chose ${YELLOW}not${STD} to change the root password."
+        echo -e " You chose ${YELLOW}not${STD} to change the root password."
 
       fi
 
@@ -110,7 +110,7 @@ else
 
   echo ""
   echo -e "--------------------------------------------------"
-  echo -e "You chose ${YELLOW}not${STD} to ${PERFORM} ${TASK}"
+  echo -e " You chose ${YELLOW}not${STD} to ${PERFORM} ${TASK}"
   echo -e "--------------------------------------------------"
   echo ""
 
