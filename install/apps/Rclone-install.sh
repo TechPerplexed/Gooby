b6ba87a27fb4ba12100eb69a53c532ec0fa4319c
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 12:04:25 -0600

    Update Rclone-install.sh

--- install/apps/Rclone-install.sh
+++ install/apps/Rclone-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 clear
-read -p "Are you sure you want to $PERFORM $FUNCTION (y/N)? " -n 1 -r
+read -p "Are you sure you want to ${PERFORM} ${FUNCTION} (y/N)? " -n 1 -r
 echo ""
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
   # ----------
   # Open ports
@@ -71,7 +71,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
 else
 
-  echo -e "You chose ${YELLOW}not${STD} to $PERFORM $FUNCTION"
+  echo -e "You chose ${YELLOW}not${STD} to ${PERFORM} ${FUNCTION}"
 
 fi
 
