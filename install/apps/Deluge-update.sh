904f9529121efa4802f8b4e6534d0aea5fbb3d28
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 11:48:57 -0600

    Update Deluge-update.sh

--- install/apps/Deluge-update.sh
+++ install/apps/Deluge-update.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 clear
-read -p "Are you sure you want to $PERFORM $FUNCTION (y/N)? " -n 1 -r
+read -p "Are you sure you want to ${PERFORM} ${FUNCTION} (y/N)? " -n 1 -r
 echo ""
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
   sudo apt-get upgrade -y && sudo apt-get upgrade -y
   echo -e "${FUNCTION} has been updated to the latest version"
 
 else
 
-  echo -e "You chose ${YELLOW}not${STD} to $PERFORM $FUNCTION"
+  echo -e "You chose ${YELLOW}not${STD} to ${PERFORM} ${FUNCTION}"
 
 fi
 
