18b433838fa1ffde6c7e2b6094554b7f77aa83d5
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-07 17:36:13 -0600

    Update Plex-remove.sh

--- install/apps/Plex-remove.sh
+++ install/apps/Plex-remove.sh
@@ -17,8 +17,38 @@ else
 
 		GOAHEAD
 
-		echo ""
-		echo -e "Coming soon!"
+		# Close ports
+
+		sudo ufw delete allow 32400
+
+		# Dependencies
+
+		sudo apt-get upgrade -y && sudo apt-get upgrade -y
+
+		# Main script
+
+		sudo dpkg -r plexmediaserver
+		
+		# Cleaning up folders
+
+		clear
+		
+		CONFIRMDELETE
+		
+		echo -e "${YELLOW}"
+		echo -e "--------------------------------------------------"
+		echo -e " Delete the Plex database and libraries (y/N)?"
+		echo -e " /var/lib/plexmediaserver/"
+		echo -e "--------------------------------------------------"
+		echo -e "${STD}"
+		
+		read -e -p "Yes or No? " -i "N" choice
+		
+		
+		case "$choice" in
+			y|Y ) sudo rm -r /var/lib/plexmediaserver ;;
+			* ) echo "Folder not deleted" ;;
+		esac
 
 		TASKCOMPLETE
 
