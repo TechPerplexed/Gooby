b82afb5360ead89f196686969d468fe2d922f67e
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-07 13:41:22 -0600

    Update Radarr-install.sh

--- install/apps/Radarr-install.sh
+++ install/apps/Radarr-install.sh
@@ -1,77 +1,69 @@
 #!/bin/bash
 
-which radarr > /tmp/checkapp.txt
+ls /opt/Radarr > /tmp/checkapp.txt; 
 clear
 
 if [ -s /tmp/checkapp.txt ]; then
 
-  ALREADYINSTALLED
+	ALREADYINSTALLED
 
 else
 
-  EXPLAINTASK
+	EXPLAINTASK
+
+	CONFIRMATION
+
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+		GOAHEAD
+
+		# Open ports
+
+		sudo ufw allow 7878
+
+		# Dependencies
+
+		sudo apt-get upgrade -y && sudo apt-get upgrade -y
+
+		sudo -s apt-get -y install \
+			libcurl3 \
+			libmono-cil-dev \
+			mono-devel \
+			mediainfo \
+			sqlite3 \
+		denyhosts at sudo software-properties-common
+
+		# Main script
+
+		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+		echo "deb http://download.mono-project.com/repo/debian jessie main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+
+		cd /tmp
+		clear
+		echo -e "Copy latest Linux version from ${YELLOW}https://github.com/Radarr/Radarr/releases/${STD}"
+		echo -e "Remove link below and paste link to Radarr.verson.linux.tar.gz"
+		echo -e "Or you can press Enter to install ${CYAN}v0.2.0.995${STD}:"
+		echo ""
+		read -e -p "Link: " -i "https://github.com/Radarr/Radarr/releases/download/v0.2.0.1120/Radarr.develop.0.2.0.1120.linux.tar.gz" radarr
+		wget $radarr
+
+		sudo tar -xf Radarr* -C /opt/
+		sudo chown -R plexuser:plexuser /opt/Radarr
   
-  CONFIRMATION
+		# Installing Services
 
-  if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+		sudo rsync -a /opt/GooPlex/scripts/radarr.service /etc/systemd/system/radarr.service
+		sudo systemctl enable radarr.service
+		sudo systemctl daemon-reload
+		sudo systemctl start radarr.service
 
-    GOAHEAD
+		TASKCOMPLETE
 
-    # ----------
-    # Open ports
-    # ----------
+	else
 
-    sudo ufw allow 7878
+		CANCELTHIS
 
-    # ------------
-    # Dependencies
-    # ------------
-
-    sudo apt-get upgrade -y && sudo apt-get upgrade -y
-
-    sudo -s apt-get -y install \
-      libcurl3 \
-      libmono-cil-dev \
-      mono-devel \
-      mediainfo \
-      sqlite3 \
-      denyhosts at sudo software-properties-common
-
-    # -----------
-    # Main script
-    # -----------
-
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-    echo "deb http://download.mono-project.com/repo/debian jessie main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-
-    cd /tmp
-    clear
-    echo -e "Copy latest Linux version from ${YELLOW}https://github.com/Radarr/Radarr/releases/${STD}"
-    echo -e "Remove link below and paste link to Radarr.verson.linux.tar.gz"
-    echo -e "Or you can press Enter to install ${CYAN}v0.2.0.995${STD}:"
-    echo ""
-    read -e -p "Link: " -i "https://github.com/Radarr/Radarr/releases/download/v0.2.0.1120/Radarr.develop.0.2.0.1120.linux.tar.gz" radarr
-    wget $radarr
-
-    sudo tar -xf Radarr* -C /opt/
-    sudo chown -R plexuser:plexuser /opt/Radarr
-  
-    # -------------------
-    # Installing Services
-    # -------------------
-
-    sudo rsync -a /opt/GooPlex/scripts/radarr.service /etc/systemd/system/radarr.service
-    sudo systemctl enable radarr.service
-    sudo systemctl daemon-reload
-    sudo systemctl start radarr.service
-	
-	TASKCOMPLETE
-
-  else
-
-    CANCELTHIS
-
-  fi
+	fi
 
 fi
 
