19a0de75bf38bc26298ba3a17bec2bb2c56f7486
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-07 13:08:45 -0600

    Update Sonarr-install.sh

--- install/apps/Sonarr-install.sh
+++ install/apps/Sonarr-install.sh
@@ -1,69 +1,69 @@
 #!/bin/bash
 
-which sonarr > /tmp/checkapp.txt
+ls /opt/sonarr > /tmp/checkapp.txt; 
+ls /opt/nzbdrone >> /tmp/checkapp.txt
 clear
 
 if [ -s /tmp/checkapp.txt ]; then
 
-  ALREADYINSTALLED
+	ALREADYINSTALLED
 
 else
 
-  EXPLAINTASK
-  
-  CONFIRMATION
+	EXPLAINTASK
 
-  if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+	CONFIRMATION
 
-    GOAHEAD
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
-    # ----------
-    # Open ports
-    # ----------
+		GOAHEAD
 
-    sudo ufw allow 8989
+		# Open ports
 
-    # ------------
-    # Dependencies
-    # ------------
+		sudo ufw allow 8989
 
-    sudo apt-get upgrade -y && sudo apt-get upgrade -y
+		# Dependencies
 
-    sudo -s apt-get -y install \
-      libcurl3 \
-      libmono-cil-dev \
-      mono-devel \
-      mediainfo \
-      sqlite3 \
-      denyhosts at sudo software-properties-common
+		sudo apt-get upgrade -y && sudo apt-get upgrade -y
 
-    # -----------
-    # Main script
-    # -----------
+		sudo -s apt-get -y install \
+			libcurl3 \
+			libmono-cil-dev \
+			mono-devel \
+			mediainfo \
+			sqlite3 \
+			apt-transport-https \
+		denyhosts at sudo software-properties-common
+		
+		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+		echo "deb https://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+		sudo apt -y update
 
-    # Execution
+		# Main script
 
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
-    sudo echo "deb http://apt.sonarr.tv/ master main" | sudo tee /etc/apt/sources.list.d/sonarr.list
+		sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 2A9B4BF8
+		sudo echo "deb https://dl.bintray.com/sonarr/phantom wheezy main" | sudo tee /etc/apt/sources.list.d/sonarr.list
 
-    sudo apt-get -y update
-    clear
-    sudo apt-get -y install nzbdrone
+		sudo apt-get -y update
+		clear
+		sudo apt-get -y install sonarr
 
-    sudo chown -R plexuser:plexuser /opt/NzbDrone
+		sudo chown -R plexuser:plexuser /opt/sonarr
 
-    sudo rsync -a /opt/GooPlex/scripts/sonarr.service /etc/systemd/system/sonarr.service
-    sudo systemctl enable sonarr.service
-    sudo systemctl daemon-reload
-    sudo systemctl start sonarr.service
-	
-    TASKCOMPLETE
+		# sudo rsync -a /opt/GooPlex/scripts/sonarr.service /etc/systemd/system/sonarr.service
+		# sudo systemctl enable sonarr.service
+		# sudo systemctl daemon-reload
+		# sudo systemctl start sonarr.service
+		
+		sudo chown -R plexuser:plexuser $HOME
 
-  else
+		TASKCOMPLETE
 
-    CANCELTHIS
+	else
 
-  fi
+		CANCELTHIS
+
+	fi
 
 fi
 
