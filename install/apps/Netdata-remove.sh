60e4cd9150e0ea0b6a5417b94e763a3163274cd2
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-07 14:46:18 -0600

    Update Netdata-remove.sh

--- install/apps/Netdata-remove.sh
+++ install/apps/Netdata-remove.sh
@@ -5,28 +5,28 @@ clear
 
 if [ ! -s /tmp/checkapp.txt ]; then
 
-  NOTINSTALLED
+	NOTINSTALLED
 
 else
 
-  EXPLAINTASK
+	EXPLAINTASK
 
-  CONFIRMATION
+	CONFIRMATION
 
-  if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
-    GOAHEAD
+		GOAHEAD
 
-    echo ""
-    echo -e "Coming soon!"
+		echo ""
+		echo -e "Coming soon!"
 
-	TASKCOMPLETE
+		TASKCOMPLETE
 
-  else
+	else
 
-    CANCELTHIS
+		CANCELTHIS
 
-  fi
+	fi
 
 fi
 
