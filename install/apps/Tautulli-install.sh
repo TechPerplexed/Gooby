2bb9f0c2028bd11acd8bd7cc334f39d4439e5d87
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 19:15:50 -0600

    Update Tautulli-install.sh

--- install/apps/Tautulli-install.sh
+++ install/apps/Tautulli-install.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+which tautulli > /tmp/checkapp.txt
 clear
-read -p "Are you sure you want to ${PERFORM} ${FUNCTION} (y/N)? " -n 1 -r
-echo ""
 
-if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+if [ -s /tmp/checkapp.txt ]; then
 
-  if [ ! -d "/opt/Tautulli" ]; then
+  ALREADYINSTALLED
+
+else
+
+  EXPLAINTASK
+  
+  CONFIRMATION
+
+  if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+    GOAHEAD
 
     # ----------
     # Open ports
@@ -35,16 +44,6 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
     sudo git clone https://github.com/Tautulli/Tautulli.git
     sudo chown plexuser:plexuser -R /opt/Tautulli
   
-  else
-  
-    clear
-    echo -e "Tautulli is already installed!"
-    echo -e "You can update it from within the application itself."
- 
-  fi
-
-  if [ ! -e "/etc/systemd/system/tautulli.service" ]; then
-
     # -------------------
     # Installing Services
     # -------------------
@@ -53,17 +52,16 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
     sudo systemctl enable tautulli.service
     sudo systemctl daemon-reload
     sudo systemctl start tautulli.service
+	
+    TASKCOMPLETE
+
+  else
+
+    CANCELTHIS
 
   fi
 
-  # ----------
-  # Finalizing
-  # ----------
-
-else
-
-  echo -e "You chose ${YELLOW}not${STD} to ${PERFORM} ${FUNCTION}"
-
 fi
 
+rm /tmp/checkapp.txt
 PAUSE
