ea5ab76de9c18bab24a9c7089e1edb29cbe94bc8
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 18:57:17 -0600

    Update Deluge-install.sh

--- install/apps/Deluge-install.sh
+++ install/apps/Deluge-install.sh
@@ -1,85 +1,76 @@
 #!/bin/bash
 
+which deluged > /tmp/checkapp.txt
 clear
 
-# Explanation
+if [ -s /tmp/checkapp.txt ]; then
 
-echo -e "--------------------------------------------------"
-echo -e " This will ${PERFORM} $TASK}"
-echo -e " You probably only need to run this once..."
-echo -e "--------------------------------------------------"
-echo ""
-
-# Confirmation
-
-read -p " Are you sure you want to ${PERFORM} ${TASK} (y/N)? " -n 1 -r
-echo ""
-
-if [[ ${REPLY} =~ ^[Yy]$ ]]; then
-
-  # ----------
-  # Open ports
-  # ----------
-
-  sudo ufw allow 8112
-
-  # ------------
-  # Dependencies
-  # ------------
-
-  sudo apt-get upgrade -y && sudo apt-get upgrade -y
-
-  # -----------
-  # Main script
-  # -----------
-
-  # Execution
-
-  sudo apt-get -y install \
-    deluged \
-    deluge-webui \
-    deluge-console \
-    denyhosts at sudo software-properties-common
-
-  # -------------------
-  # Installing Services
-  # -------------------
-
-  sudo rsync -a /opt/GooPlex/scripts/deluged.service /etc/systemd/system/deluged.service
-  sudo rsync -a /opt/GooPlex/scripts/deluge-web.service /etc/systemd/system/deluge-web.service
-
-  sudo systemctl enable deluged.service
-  sudo systemctl enable deluge-web.service
-
-  sudo systemctl daemon-reload
-
-  sudo systemctl start deluged.service
-  sudo systemctl start deluge-web.service
-
-  # ----------------
-  # Creating Folders
-  # ----------------
-
-  sudo mkdir -p /home/plexuser/downloads/incomplete
-  sudo mkdir -p /home/plexuser/downloads/import
-  sudo chown -R plexuser:plexuser /home/plexuser
-  
-  # Task Completed
-
-  echo -e "${LMAGENTA}"
-  echo -e "--------------------------------------------------"
-  echo -e " ${PERFORM} $TASK completed"
-  echo -e "--------------------------------------------------"
-  echo -e "${STD}"
+  ALREADYINSTALLED
 
 else
 
-  echo ""
-  echo -e "--------------------------------------------------"
-  echo -e " You chose ${YELLOW}not${STD} to ${PERFORM} ${TASK}"
-  echo -e "--------------------------------------------------"
-  echo ""
+  EXPLAINTASK
+  
+  CONFIRMATION
+
+  if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+    GOAHEAD
+
+    # ----------
+    # Open ports
+    # ----------
+
+    sudo ufw allow 8112
+
+    # ------------
+    # Dependencies
+    # ------------
+
+    sudo apt-get upgrade -y && sudo apt-get upgrade -y
+
+    # -----------
+    # Main script
+    # -----------
+
+    sudo apt-get -y install \
+      deluged \
+      deluge-webui \
+      deluge-console \
+      denyhosts at sudo software-properties-common
+
+    # -------------------
+    # Installing Services
+    # -------------------
+
+    sudo rsync -a /opt/GooPlex/scripts/deluged.service /etc/systemd/system/deluged.service
+    sudo rsync -a /opt/GooPlex/scripts/deluge-web.service /etc/systemd/system/deluge-web.service
+
+    sudo systemctl enable deluged.service
+    sudo systemctl enable deluge-web.service
+
+    sudo systemctl daemon-reload
+
+    sudo systemctl start deluged.service
+    sudo systemctl start deluge-web.service
+
+    # ----------------
+    # Creating Folders
+    # ----------------
+
+    sudo mkdir -p /home/plexuser/downloads/incomplete
+    sudo mkdir -p /home/plexuser/downloads/import
+    sudo chown -R plexuser:plexuser /home/plexuser
+	
+  	TASKCOMPLETE
+
+  else
+
+    CANCELTHIS
+
+  fi
 
 fi
 
+rm /tmp/checkapp.txt
 PAUSE
