63d4f9bd5a4ba184727c99aecd714d81b193a77e
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-07 13:42:21 -0600

    Update Radarr-remove.sh

--- install/apps/Radarr-remove.sh
+++ install/apps/Radarr-remove.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-which radarr > /tmp/checkapp.txt
+ls /opt/Radarr > /tmp/checkapp.txt;
+
 clear
 
 if [ ! -s /tmp/checkapp.txt ]; then
 
-  NOTINSTALLED
+	NOTINSTALLED
 
 else
 
-  EXPLAINTASK
+	EXPLAINTASK
 
-  CONFIRMATION
+	CONFIRMATION
 
-  if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
-    GOAHEAD
+		GOAHEAD
 
-    echo ""
-    echo -e "Coming soon!"
+		echo ""
+		echo -e "Coming soon!"
 
-    TASKCOMPLETE
+		TASKCOMPLETE
 
-  else
+	else
 
-    CANCELTHIS
+		CANCELTHIS
 
-  fi
+	fi
 
 fi
 
