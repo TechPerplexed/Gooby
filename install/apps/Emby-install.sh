7ae25d3cc97182b45a2a74d2dc5836ee8557dda1
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 18:09:03 -0600

    Update Emby-install.sh

--- install/apps/Emby-install.sh
+++ install/apps/Emby-install.sh
@@ -1,56 +1,36 @@
 #!/bin/bash
 
+which emby > /tmp/checkapp.txt
 clear
 
-# Explanation
+if [ -s /tmp/checkapp.txt ]; then
 
-echo -e "--------------------------------------------------"
-echo -e " This will ${PERFORM} $TASK}"
-echo -e " You probably only need to run this once..."
-echo -e "--------------------------------------------------"
-echo ""
-
-# Confirmation
-
-read -p " Are you sure you want to ${PERFORM} ${TASK} (y/N)? " -n 1 -r
-echo ""
-
-if [[ ${REPLY} =~ ^[Yy]$ ]]; then
-
-  if [ -d "/folder" ]; then
-
-    echo ""
-	echo -e "--------------------------------------------------"
-    echo -e " ${TASK} seems to be installed already"
-	echo -e "--------------------------------------------------"
-	echo ""
-
-  else
-
-	  # Execution
-	  
-        echo ""
-          echo -e "Coming soon!"
-          echo ""
-	  
-	  # Task Completed
-
-	  echo -e "${LMAGENTA}"
-	  echo -e "--------------------------------------------------"
-	  echo -e " ${PERFORM} ${TASK} completed"
-	  echo -e "--------------------------------------------------"
-	  echo -e "${STD}"
-	  
-  fi
+  ALREADYINSTALLED
 
 else
 
-  echo ""
-  echo -e "--------------------------------------------------"
-  echo -e " You chose ${YELLOW}not${STD} to ${PERFORM} ${TASK}"
-  echo -e "--------------------------------------------------"
-  echo ""
+  EXPLAINTASK
+
+  # Confirmation
+  
+  CONFIRMATION
+
+  if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+    GOAHEAD
+
+    echo ""
+    echo -e "Coming soon!"
+	
+	TASKCOMPLETE
+
+  else
+
+    CANCELTHIS
+
+  fi
 
 fi
 
+rm /tmp/checkapp.txt
 PAUSE
