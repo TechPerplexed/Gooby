f9f716762e5e9370d311d8bc91ea93a367b929e2
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 19:00:09 -0600

    Update Netdata-install.sh

--- install/apps/Netdata-install.sh
+++ install/apps/Netdata-install.sh
@@ -1,62 +1,54 @@
 #!/bin/bash
 
+which netdata > /tmp/checkapp.txt
 clear
 
-# Explanation
+if [ -s /tmp/checkapp.txt ]; then
 
-echo -e "--------------------------------------------------"
-echo -e " This will ${PERFORM} $TASK}"
-echo -e " You probably only need to run this once..."
-echo -e "--------------------------------------------------"
-echo ""
-
-# Confirmation
-
-read -p " Are you sure you want to ${PERFORM} ${TASK} (y/N)? " -n 1 -r
-echo ""
-
-if [[ ${REPLY} =~ ^[Yy]$ ]]; then
-
-  # ----------
-  # Open ports
-  # ----------
-
-  sudo ufw allow 19999
-
-  # ------------
-  # Dependencies
-  # ------------
-
-  sudo apt-get upgrade -y && sudo apt-get upgrade -y
-  sudo -s apt-get -y install \
-    unzip \
-    curl \
-    denyhosts at sudo software-properties-common
-
-  # -----------
-  # Main script
-  # -----------
-
-  # Execution
-
-  clear
-  bash <(curl -Ss https://my-netdata.io/kickstart.sh)
-
-  # Task Completed
-
-  echo -e "${LMAGENTA}"
-  echo -e "--------------------------------------------------"
-  echo -e " ${PERFORM} $TASK completed"
-  echo -e "--------------------------------------------------"
-  echo -e "${STD}"
+  ALREADYINSTALLED
 
 else
 
-  echo ""
-  echo -e "--------------------------------------------------"
-  echo -e " You chose ${YELLOW}not${STD} to ${PERFORM} ${TASK}"
-  echo -e "--------------------------------------------------"
-  echo ""
+  EXPLAINTASK
+  
+  CONFIRMATION
+
+  if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+    GOAHEAD
+
+    # ----------
+    # Open ports
+    # ----------
+
+    sudo ufw allow 19999
+
+    # ------------
+    # Dependencies
+    # ------------
+
+    sudo apt-get upgrade -y && sudo apt-get upgrade -y
+    sudo -s apt-get -y install \
+      unzip \
+      curl \
+      denyhosts at sudo software-properties-common
+
+    # -----------
+    # Main script
+    # -----------
+
+    clear
+    bash <(curl -Ss https://my-netdata.io/kickstart.sh)
+	
+	  TASKCOMPLETE
+
+  else
+
+    CANCELTHIS
+
+  fi
 
 fi
 
+rm /tmp/checkapp.txt
+PAUSE
