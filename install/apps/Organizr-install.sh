c27beba6303667044dee0bb48a8185b55820b862
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-06 19:06:23 -0600

    Update Organizr-install.sh

--- install/apps/Organizr-install.sh
+++ install/apps/Organizr-install.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 
+which organizr > /tmp/checkapp.txt
 clear
-read -p "Are you sure you want to ${PERFORM} ${FUNCTION} (y/N)? " -n 1 -r
-echo ""
 
-if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+if [ -s /tmp/checkapp.txt ]; then
 
-  # ----------
-  # Open ports
-  # ----------
+  ALREADYINSTALLED
 
-  sudo ufw allow 80
+else
 
-  # ------------
-  # Dependencies
-  # ------------
+  EXPLAINTASK
+  
+  CONFIRMATION
 
-  sudo apt-get upgrade -y && sudo apt-get upgrade -y
+  if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
-  # -----------
-  # Main script
-  # -----------
+    GOAHEAD
 
-  if [ -d "/opt/OrganizrInstaller" ];
+    # ----------
+    # Open ports
+    # ----------
 
-  then
+    sudo ufw allow 80
 
-    echo -e "${FUNCTION} is already installed!"
+    # ------------
+    # Dependencies
+    # ------------
 
-  else
+    sudo apt-get upgrade -y && sudo apt-get upgrade -y
 
-    # Execution
+    # -----------
+    # Main script
+    # -----------
 
     sudo git clone https://github.com/elmerfdz/OrganizrInstaller /opt/OrganizrInstaller
     cd /opt/OrganizrInstaller/ubuntu/oui
     clear
     sudo bash ou_installer.sh
     cd ~
-  
+	
+	TASKCOMPLETE
+
+  else
+
+    CANCELTHIS
+
   fi
-
-  # ----------
-  # Finalizing
-  # ----------
-
-else
-
-  echo -e "You chose ${YELLOW}not${STD} to ${PERFORM} ${FUNCTION}"
 
 fi
 
+rm /tmp/checkapp.txt
 PAUSE
