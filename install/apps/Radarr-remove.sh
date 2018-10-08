5443ef9f2f102ae2b5033f0a5347c86b40a07c63
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-08 13:25:18 -0600

    Update Radarr-remove.sh

--- install/apps/Radarr-remove.sh
+++ install/apps/Radarr-remove.sh
@@ -11,14 +11,26 @@ else
 
 	EXPLAINTASK
 
-	CONFIRMATION
+	CONFIRMDELETE
 
 	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 		GOAHEAD
 
-		echo ""
-		echo -e "Coming soon!"
+		# Close ports
+
+		sudo ufw delete allow 7878
+
+		# Main script
+
+		sudo rm -r /opt/Radarr
+  
+		# Removing Services
+
+		sudo systemctl stop radarr.service
+		sudo systemctl disable radarr.service
+		sudo systemctl daemon-reload
+		sudo rm /etc/systemd/system/radarr.service
 
 		TASKCOMPLETE
 
