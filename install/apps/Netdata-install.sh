c5afde525fbb3b325c8bbb1fe1227df77df2bd96
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-08 16:31:25 -0600

    Update Netdata-install.sh

--- install/apps/Netdata-install.sh
+++ install/apps/Netdata-install.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
 
+docker ps -a -f "name=netdata" > /tmp/checkapp.txt
 clear
-EXPLAINTASK
 
-CONFIRMATION
+if read -r && read -r; then
 
-if [[ ${REPLY} =~ ^[Yy]$ ]]; then
-
-	GOAHEAD
-	RUNPATCHES
-
-	# Dependencies
-
-	source /opt/GooPlex/install/misc/docker.sh
-	docker container stop netdata
-	docker container rm netdata
-
-	# Main script
-
-	sudo docker run -d --name=netdata \
-	--restart=always \
-	-p 19999:19999 \
-	-v /proc:/host/proc:ro \
-	-v /sys:/host/sys:ro \
-	-v /var/run/docker.sock:/var/run/docker.sock:ro \
-	--cap-add SYS_PTRACE \
-	--security-opt apparmor=unconfined \
-	netdata/netdata
-
-	TASKCOMPLETE
+	ALREADYINSTALLED
 
 else
 
-	CANCELTHIS
+	EXPLAINTASK
 
-fi
+	CONFIRMATION
 
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+		GOAHEAD
+		RUNPATCHES
+
+		# Dependencies
+
+		source /opt/GooPlex/install/misc/docker.sh
+		docker container stop netdata
+		docker container rm netdata
+
+		# Main script
+
+		sudo docker run -d --name=netdata \
+		--restart=always \
+		-p 19999:19999 \
+		-v /proc:/host/proc:ro \
+		-v /sys:/host/sys:ro \
+		-v /var/run/docker.sock:/var/run/docker.sock:ro \
+		--cap-add SYS_PTRACE \
+		--security-opt apparmor=unconfined \
+		netdata/netdata
+
+		TASKCOMPLETE
+
+	else
+
+		CANCELTHIS
+
+	fi
+
+fi /tmp/checkapp.txt
+
+rm /tmp/checkapp.txt
 PAUSE
