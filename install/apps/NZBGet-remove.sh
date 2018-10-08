5144402a0552f790d80b6a21c731d021a1f4cd3f
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-07 20:07:17 -0600

    Update NZBGet-remove.sh

--- install/apps/NZBGet-remove.sh
+++ install/apps/NZBGet-remove.sh
@@ -17,8 +17,43 @@ else
 
 		GOAHEAD
 
-		echo ""
-		echo -e "Coming soon!"
+		# Dependencies
+
+		RUNPATCHES
+
+		# Close ports
+
+		sudo ufw delete allow 6789
+
+		# Main script
+
+		sudo apt-get -y remove --auto-remove nzbget
+
+		# Removing Services
+
+		sudo rsync -a /opt/GooPlex/scripts/nzbget.service /etc/systemd/system/nzbget.service
+		sudo systemctl enable nzbget.service
+		sudo systemctl daemon-reload
+		sudo systemctl start nzbget.service
+
+		# Cleaning up folders
+
+		clear
+
+		CONFIRMDELETE
+
+		echo -e "${YELLOW}"
+		echo -e "--------------------------------------------------"
+		echo -e " Delete the user configurations (y/N)?"
+		echo -e "--------------------------------------------------"
+		echo -e "${STD}"
+
+		read -e -p "Yes or No? " -i "N" choice
+
+		case "$choice" in
+			y|Y ) sudo apt-get -y purge --auto-remove nzbget ;;
+			* ) echo "Configurations not deleted" ;;
+		esac
 
 		TASKCOMPLETE
 
