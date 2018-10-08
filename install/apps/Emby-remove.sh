5409146da0246441a19363b5793efe8b734fcdb6
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-08 12:22:51 -0600

    Update Emby-remove.sh

--- install/apps/Emby-remove.sh
+++ install/apps/Emby-remove.sh
@@ -11,46 +11,20 @@ else
 
 	EXPLAINTASK
 
-	CONFIRMATION
+	CONFIRMDELETE
 
 	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 		GOAHEAD
-
-		# Dependencies
-
-		RUNPATCHES
 
 		# Close ports
 
 		sudo ufw delete allow 8096
 
 		# Main script
-		
-		clear
-		sudo apt-get remove --auto-remove emby-server -y
+
+		sudo apt-get purge --auto-remove emby-server -y
 		sudo rm -r /opt/emby-server
-
-		# Cleaning up folders
-
-		clear
-
-		CONFIRMDELETE
-		
-		echo -e "${YELLOW}"
-		echo -e "--------------------------------------------------"
-		echo -e " Delete the Emby database and libraries (y/N)?"
-		echo -e " /var/lib/emby/"
-		echo -e "--------------------------------------------------"
-		echo -e "${STD}"
-		
-		read -e -p "Yes or No? " -i "N" choice
-		
-		
-		case "$choice" in
-			y|Y ) sudo rm -r /var/lib/emby ;;
-			* ) echo "Folder not deleted" ;;
-		esac
 
 		TASKCOMPLETE
 
