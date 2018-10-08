60608bab5795825f7cb769c3ac475568eb862044
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-08 14:30:41 -0600

    Update Netdata-install.sh

--- install/apps/Netdata-install.sh
+++ install/apps/Netdata-install.sh
@@ -18,14 +18,20 @@ else
 		GOAHEAD
 		RUNPATCHES
 
-		# Open ports
+		# Install docker
 
-		sudo ufw allow 19999
+		source /opt/GooPlex/install/misc/docker.sh
 
 		# Main script
 
-		clear
-		bash <(curl -Ss https://my-netdata.io/kickstart.sh)
+		docker run -d --name=netdata \
+		-p 19999:19999 \
+		-v /proc:/host/proc:ro \
+		-v /sys:/host/sys:ro \
+		-v /var/run/docker.sock:/var/run/docker.sock:ro \
+		--cap-add SYS_PTRACE \
+		--security-opt apparmor=unconfined \
+		netdata/netdata
 
 		TASKCOMPLETE
 
