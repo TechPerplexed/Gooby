243e00e1599401a82f1ad8bdfaf87d7b564a8176
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-07 18:26:34 -0600

    Update Tautulli-remove.sh

--- install/apps/Tautulli-remove.sh
+++ install/apps/Tautulli-remove.sh
@@ -17,8 +17,45 @@ else
 
 		GOAHEAD
 
-		echo ""
-		echo -e "Coming soon!"
+		# Dependencies
+
+		RUNPATCHES
+
+		# Close ports
+
+		sudo ufw delete allow 8181
+
+		# Main script
+
+		# na
+
+		# Removing Services
+
+		sudo systemctl stop tautulli.service
+		sudo systemctl disable tautulli.service
+		sudo rm /etc/systemd/system/tautulli.service
+		sudo systemctl daemon-reload
+
+		# Cleaning up folders
+
+		clear
+		
+		CONFIRMDELETE
+		
+		echo -e "${YELLOW}"
+		echo -e "--------------------------------------------------"
+		echo -e " Delete the user stats folder (y/N)?"
+		echo -e " /opt/Tautulli"
+		echo -e "--------------------------------------------------"
+		echo -e "${STD}"
+		
+		read -e -p "Yes or No? " -i "N" choice
+		
+		
+		case "$choice" in
+			y|Y ) rm -r /opt/Tautulli ;;
+			* ) echo "Folder not deleted" ;;
+		esac
 
 		TASKCOMPLETE
 
