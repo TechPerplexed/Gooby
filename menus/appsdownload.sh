ffd47b78243b431197616bc7ce2e9155285b6278
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-08 10:42:24 -0600

    Update appsdownload.sh

--- menus/appsdownload.sh
+++ menus/appsdownload.sh
@@ -70,7 +70,7 @@ show_menus() {
 
 read_options(){
   local choice
-    read -p "Choose option: " choice
+    read -n 1 -s -r -p "Choose option: " choice
     case $choice in
       [Aa]) SONARR ;;
       [Bb]) RADARR ;;
