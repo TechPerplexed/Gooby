118e5feef4ee5b61dede9fdabd02da75cadcea88
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-07 19:43:02 -0600

    Update Sonarr-install.sh

--- install/apps/Sonarr-install.sh
+++ install/apps/Sonarr-install.sh
@@ -18,13 +18,9 @@ else
 
 		GOAHEAD
 
-		# Open ports
-
-		sudo ufw allow 8989
-
 		# Dependencies
 
-		sudo apt-get upgrade -y && sudo apt-get upgrade -y
+		RUNPATCHES
 
 		sudo -s apt-get -y install \
 			libcurl3 \
@@ -38,6 +34,10 @@ else
 		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 		echo "deb https://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 		sudo apt -y update
+
+		# Open ports
+
+		sudo ufw allow 8989
 
 		# Main script
 
