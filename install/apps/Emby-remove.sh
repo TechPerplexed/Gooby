50a60ed44ae8bc42dc705ba78e8d49bb0ec56f50
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-07 19:17:27 -0600

    Update Emby-remove.sh

--- install/apps/Emby-remove.sh
+++ install/apps/Emby-remove.sh
@@ -17,8 +17,39 @@ else
 
 		GOAHEAD
 
-		echo ""
-		echo -e "Coming soon!"
+		# Dependencies
+
+		RUNPATCHES
+
+		# Close ports
+
+		sudo ufw delete allow 8096
+
+		# Main script
+		
+		clear
+		sudo apt-get remove emby-server -y
+
+		# Cleaning up folders
+
+		clear
+
+		CONFIRMDELETE
+		
+		echo -e "${YELLOW}"
+		echo -e "--------------------------------------------------"
+		echo -e " Delete the Emby database and libraries (y/N)?"
+		echo -e " /var/lib/emby/"
+		echo -e "--------------------------------------------------"
+		echo -e "${STD}"
+		
+		read -e -p "Yes or No? " -i "N" choice
+		
+		
+		case "$choice" in
+			y|Y ) sudo rm -r /var/lib/emby ;;
+			* ) echo "Folder not deleted" ;;
+		esac
 
 		TASKCOMPLETE
 
