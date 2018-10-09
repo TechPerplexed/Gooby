1f47294d3a75d73757cb9052ca5d684d869a3ffc
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-09 16:58:26 -0600

    Create install.sh

---
+++ install/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install GooPlex
+
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+sudo -s apt-get -y install \
+	git \
+	fail2ban \
+	nano \
+	unzip \
+	wget \
+	curl \
+	ufw \
+	socat \
+	acl \
+	fuse \
+denyhosts at sudo software-properties-common
+
+sudo rm -r /opt/GooPlex
+sudo git clone https://github.com/TechPerplexed/GooPlex.git /opt/GooPlex
+sudo bash /opt/GooPlex/install/setup.sh
