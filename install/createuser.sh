1100d103028c11e5ce911ff63a8bc90c1ea0bc63
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-10 12:35:43 -0600

    Update createuser.sh

--- install/createuser.sh
+++ install/createuser.sh
@@ -24,12 +24,12 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 	read -e -p "${YELLOW}Desired username${STD} (Enter to accept): " -i "plexuser" PU
 
 	if [ "$PU" != "plexuser" ]; then
-		echo ""
-		echo "I'm in the process of creating a new GooPlex."
-		echo "For now, it's only possible to create the user 'plexuser'"
-		echo "Stay tuned for a future update that makes it possible"
-		echo "to choose $PU as your user name!"
-		echo ""
+		echo -e "${CYAN}"
+		echo -e "I'm in the process of creating a new GooPlex."
+		echo -e "For now, it's only possible to create the user 'plexuser'"
+		echo -e "Stay tuned for a future update that makes it possible"
+		echo -e "to choose $PU as your user name!"
+		echo -e "${STD}"
 		PU=plexuser
 	fi
 
