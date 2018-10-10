a9be63e3bfdbef62686066883ffcc9b50cd95cf0
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-09 19:09:36 -0600

    Update apps.sh

--- menus/apps.sh
+++ menus/apps.sh
@@ -8,25 +8,25 @@ clear
 
 # Install
 INSTALL(){
-  PERFORM="install"
-  source /opt/GooPlex/install/apps/${TASK}-${PERFORM}.sh
+	PERFORM="install"
+	source /opt/GooPlex/install/apps/${TASK}-${PERFORM}.sh
 }
 
 # Update
 UPDATE(){
-  PERFORM="update"
-  source /opt/GooPlex/install/apps/${TASK}-${PERFORM}.sh
+	PERFORM="update"
+	source /opt/GooPlex/install/apps/${TASK}-${PERFORM}.sh
 }
 
 # Remove
 REMOVE(){
-  PERFORM="remove"
-  source /opt/GooPlex/install/apps/${TASK}-${PERFORM}.sh
+	PERFORM="remove"
+	source /opt/GooPlex/install/apps/${TASK}-${PERFORM}.sh
 }
 
 # Exit
 QUIT(){
-  exit
+	exit
 }
 
 # ------------
@@ -34,20 +34,15 @@ QUIT(){
 # ------------
 
 show_menus() {
-  clear
-  echo -e " ${LPURPLE}"
-  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  echo -e " G O O P L E X - Visit techperplexed.ga "
-  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  echo -e " $TASK"
-  echo -e " ${STD}"
-  echo -e " ${LPURPLE}I${STD} - Install $TASK"
-  echo -e " ${LPURPLE}U${STD} - Update $TASK"
-  echo -e " ${LPURPLE}R${STD} - Remove $TASK"
-  echo -e " ${WHITE}Z${STD} - EXIT to Main Menu"
-  echo -e " ${LPURPLE}"
-  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  echo -e " ${STD}"
+	clear
+	echo -e " ${LPURPLE}"
+	MENUSTART
+	echo -e " ${LPURPLE}I${STD} - Install $TASK"
+	echo -e " ${LPURPLE}U${STD} - Update $TASK"
+	echo -e " ${LPURPLE}R${STD} - Remove $TASK"
+	echo -e " ${WHITE}Z${STD} - EXIT to Main Menu"
+	echo -e " ${LPURPLE}"
+	MENUEND
 }
 
 # ------------
@@ -55,25 +50,19 @@ show_menus() {
 # ------------
 
 read_options(){
-  local choice
-    read -n 1 -s -r -p "Choose option: " choice
-    case $choice in
-      [Ii]) INSTALL ;;
-      [Uu]) UPDATE ;;
-      [Rr]) REMOVE ;;
-      [Zz]) QUIT ;;
-      *) echo -e "${LRED}Please select a valid option${STD}" && sleep 2
-    esac
+	local choice
+	read -n 1 -s -r -p "Choose option: " choice
+	case $choice in
+		[Ii]) INSTALL ;;
+		[Uu]) UPDATE ;;
+		[Rr]) REMOVE ;;
+		[Zz]) QUIT ;;
+		*) echo -e "${LRED}Please select a valid option${STD}" && sleep 2
+	esac
 }
 
 # ----------
 # Finalizing
 # ----------
 
-trap '' SIGINT SIGQUIT SIGTSTP
-
-while true
-do
-  show_menus
-  read_options
-done
+MENUFINALIZE
