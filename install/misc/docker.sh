77df6442b6e37231944bd6ca0a0b1090b42b0b0c
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-10 11:24:50 -0600

    Update docker.sh

--- install/misc/docker.sh
+++ install/misc/docker.sh
@@ -34,9 +34,9 @@ if [ ! -s /tmp/checkdocker.txt ]; then
 
 	# Install docker-compose
 
-	sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-	chmod +x /usr/local/bin/docker-compose
-	sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+	sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+	sudo chmod +x /usr/local/bin/docker-compose
+	sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
 fi
 
