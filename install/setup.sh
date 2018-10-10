ba73b02ee951af116943ad977af17bef8bf8f0e0
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-09 18:30:30 -0600

    Update setup.sh

--- install/setup.sh
+++ install/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-FUNCTION="install GooPlex"
+PERFORM="install"
+TASK="GooPlex"
 
 # ---------
 # Variables
@@ -12,44 +13,26 @@ sudo chmod +x -R /opt/GooPlex/install
 sudo chmod +x -R /opt/GooPlex/menus
 sudo rsync -a /opt/GooPlex/install/gooplex /bin
 sudo chmod 755 /bin/gooplex
+sudo chown -R $USER:$USER /opt/GooPlex
 
 lsb_release -r -s > /tmp/version
 VERSION=$( cat /tmp/version )
 
-if [ "$VERSION" != "18.04" ];
-then
+if [ "$VERSION" != "18.04" ]; then
 
-  # Confirmation
+	CONFIRMATION
 
-  clear
-  echo -e "You are running version ${LRED}$VERSION${STD} of Linux"
-  echo -e "GooPlex has been tested on ${CYAN}16.04${STD} and ${CYAN}18.04${STD}"
-  echo -e "Proceed at your own risk!"
-  echo ""
-  read -p "Are you sure you want to $FUNCTION (y/N)? " -n 1 -r
-  echo ""
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
 
-  if [[ $REPLY =~ ^[Yy]$ ]]
-  then
+		echo -e "${LRED}Ok, proceeding...${STD}" && sleep 2
 
-    echo -e "${LRED}Ok, proceeding...${STD}" && sleep 2
+	else
 
-  else
+		CANCELTHIS
+		MENUVISIT
+		exit 0
 
-clear
-  echo ""
-  echo "----------------------------------------------"
-  echo -e " You chose ${YELLOW}not${STD} to $FUNCTION"
-  echo -e " Continue any time by typing '${WHITE}gooplex${STD}' "
-  echo -e " Exiting..."
-  echo "----------------------------------------------"
-  echo ""
-  exit
-
-    echo ""
-    exit 0
-
-  fi
+	fi
 
 fi
 
