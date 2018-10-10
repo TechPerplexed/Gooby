5e74a2f0037504a48e197a400a56726481df4e91
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-09 19:21:32 -0600

    Update appsother.sh

--- menus/appsother.sh
+++ menus/appsother.sh
@@ -15,25 +15,25 @@ clear
 
 # Netdata
 NETDATA(){
-  TASK="Netdata"
-  source /opt/GooPlex/menus/apps.sh
+	TASK="Netdata"
+	source /opt/GooPlex/menus/apps.sh
 }
 
 # Organizr
 ORGANIZR(){
-  TASK="Organizr"
-  source /opt/GooPlex/menus/apps.sh
+	TASK="Organizr"
+	source /opt/GooPlex/menus/apps.sh
 }
 
 # Ombi
 OMBI(){
-  TASK="Ombi"
-  source /opt/GooPlex/menus/apps.sh
+	TASK="Ombi"
+	source /opt/GooPlex/menus/apps.sh
 }
 
 # Exit
 QUIT(){
-  exit
+	exit
 }
 
 # ------------
@@ -41,20 +41,15 @@ QUIT(){
 # ------------
 
 show_menus() {
-  clear
-  echo -e " ${LPURPLE}"
-  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  echo -e " G O O P L E X - Visit techperplexed.ga "
-  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  echo -e " $FUNCTION "
-  echo -e " ${STD}"
-  echo -e " ${LPURPLE}A${STD} - Netdata"
-  echo -e " ${LPURPLE}B${STD} - Organizr"
-  echo -e " ${LPURPLE}C${STD} - Ombi"
-  echo -e " ${WHITE}Z${STD} - EXIT to Main Menu"
-  echo -e " ${LPURPLE}"
-  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  echo -e " ${STD}"
+	clear
+	echo -e " ${LPURPLE}"
+	MENUSTART
+	echo -e " ${LPURPLE}A${STD} - Netdata"
+	echo -e " ${LPURPLE}B${STD} - Organizr"
+	echo -e " ${LPURPLE}C${STD} - Ombi"
+	echo -e " ${WHITE}Z${STD} - EXIT to Main Menu"
+	echo -e " ${LPURPLE}"
+	MENUEND
 }
 
 # ------------
@@ -62,25 +57,19 @@ show_menus() {
 # ------------
 
 read_options(){
-  local choice
-    read -n 1 -s -r -p "Choose option: " choice
-    case $choice in      
-      [Aa]) NETDATA ;;
-      [Bb]) ORGANIZR ;;
-      [Cc]) OMBI ;;
-      [Zz]) QUIT ;;
-      *) echo -e "${LRED}Please select a valid option${STD}" && sleep 2
-    esac
+	local choice
+	read -n 1 -s -r -p "Choose option: " choice
+    case $choice in
+		[Aa]) NETDATA ;;
+		[Bb]) ORGANIZR ;;
+		[Cc]) OMBI ;;
+		[Zz]) QUIT ;;
+		*) echo -e "${LRED}Please select a valid option${STD}" && sleep 2
+	esac
 }
  
 # ----------
 # Finalizing
 # ----------
 
-trap '' SIGINT SIGQUIT SIGTSTP
-
-while true
-do 
-  show_menus
-  read_options
-done
+MENUFINALIZE
