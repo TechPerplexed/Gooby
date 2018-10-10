b23c06e118e8d86d9b395bc5469c5fbc7c5b0912
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-09 19:24:27 -0600

    Update misc.sh

--- menus/misc.sh
+++ menus/misc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FUNCTION="Additional Tasks"
+MENU="Additional Tasks"
 
 # ---------
 # Variables
@@ -15,29 +15,29 @@ clear
 
 # Update GooPlex
 SERVER(){
-  PERFORM="update"
-  TASK="GooPlex"
-  source /opt/GooPlex/install/misc/update.sh
+	PERFORM="update"
+	TASK="GooPlex"
+	source /opt/GooPlex/install/misc/update.sh
 }
 
 # Plex Backup
 PBACKUP(){
-  PERFORM="backup"
-  TASK="Plex and Tautulli to Google"
-  source /opt/GooPlex/install/misc/plex-backup.sh
+	PERFORM="backup"
+	TASK="Plex and Tautulli to Google"
+	source /opt/GooPlex/install/misc/plex-backup.sh
 }
 
 
 # Plex Restore
 PRESTORE(){
-  PERFORM="restore"
-  TASK="Plex and Tautulli database"
-  source /opt/GooPlex/install/misc/plex-restore.sh
+	PERFORM="restore"
+	TASK="Plex and Tautulli database"
+	source /opt/GooPlex/install/misc/plex-restore.sh
 }
 
 # Exit
 QUIT(){
-  exit
+	exit
 }
 
 # ------------
@@ -45,20 +45,15 @@ QUIT(){
 # ------------
 
 show_menus() {
-  clear
-  echo -e " ${CYAN}"
-  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  echo -e " G O O P L E X - Visit techperplexed.ga "
-  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  echo -e " $FUNCTION "
-  echo -e " ${STD}"
-  echo -e " ${CYAN}A${STD} - Update GooPlex"
-  echo -e " ${CYAN}B${STD} - Backup Plex and Tautulli"
-  echo -e " ${CYAN}C${STD} - Restore Plex and Tautulli"
-  echo -e " ${WHITE}Z${STD} - EXIT to Main Menu"
-  echo -e " ${CYAN}"
-  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  echo -e " ${STD}"
+	clear
+	echo -e " ${CYAN}"
+	MENUSTART
+	echo -e " ${CYAN}A${STD} - Update GooPlex"
+	echo -e " ${CYAN}B${STD} - Backup Plex and Tautulli"
+	echo -e " ${CYAN}C${STD} - Restore Plex and Tautulli"
+	echo -e " ${WHITE}Z${STD} - EXIT to Main Menu"
+	echo -e " ${CYAN}"
+	MENUEND
 }
 
 # ------------
@@ -66,25 +61,19 @@ show_menus() {
 # ------------
 
 read_options(){
-  local choice
-    read -n 1 -s -r -p "Choose option: " choice
-    case $choice in
-      [Aa]) SERVER ;;
-      [Bb]) PBACKUP ;;
-      [Cc]) PRESTORE ;;
-      [Zz]) QUIT ;;
-      *) echo -e "${LRED}Please select a valid option${STD}" && sleep 2
-    esac
+	local choice
+	read -n 1 -s -r -p "Choose option: " choice
+	case $choice in
+		[Aa]) SERVER ;;
+		[Bb]) PBACKUP ;;
+		[Cc]) PRESTORE ;;
+		[Zz]) QUIT ;;
+		*) echo -e "${LRED}Please select a valid option${STD}" && sleep 2
+	esac
 }
  
 # ----------
 # Finalizing
 # ----------
 
-trap '' SIGINT SIGQUIT SIGTSTP
-
-while true
-do 
-  show_menus
-  read_options
-done
+MENUFINALIZE
