a625e9bb164796901a765fd74c0c701726c95d95
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-11 12:40:38 -0600

    Update url-set.sh

--- install/server/url-set.sh
+++ install/server/url-set.sh
@@ -9,10 +9,37 @@ CONFIRMATION
 if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 	GOAHEAD
-    
-	echo "Coming soon!"
 
-	TASKCOMPLETE
+	sudo mkdir -p $CONFIGS/.config
+	sudo chown -R $USER:$USER $CONFIGS
+
+	if [ ! -f $CONFIGS/.config/seturl ]; then
+
+		echo "You have not set an URL yet"
+
+	else
+
+		echo "Your URL is currently set to $URL"
+
+	fi
+
+	read -p "New url: " SETURL
+
+	if [[ -z "$SETURL" ]]; then
+
+		echo "No input entered... no changes made!"
+
+	else
+
+		echo "$SETURL" > $CONFIGS/.config/seturl
+
+		URL=$( cat $CONFIGS/.config/seturl )
+
+		echo "Your new URL is set to $URL"
+
+		TASKCOMPLETE
+
+	fi
 
 else
 
