8602590e63cd8f33579ddcb7a0301f7d00cdbe69
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-11 10:55:35 -0600

    Update patches-apply.sh

--- install/server/patches-apply.sh
+++ install/server/patches-apply.sh
@@ -2,46 +2,26 @@
 
 clear
 
-# Explanation
+EXPLAINTASK
 
-echo -e "--------------------------------------------------"
-echo -e " This will update your server"
-echo -e " with all the latest patches"
-echo -e " You can run this as often as you like!"
-echo -e "--------------------------------------------------"
-echo ""
-
-# Confirmation
-
-read -p " Are you sure you want to ${PERFORM} ${TASK} (y/N)? " -n 1 -r
-echo ""
+CONFIRMATION
 
 if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
-  # Main script
+	GOAHEAD
+    
+	sudo apt-get update
+	sudo apt-get upgrade -y
+	sudo apt-get dist-upgrade -y
+	sudo apt autoremove -y
+	sudo apt autoclean
+	sudo apt-get autoremove
 
-  sudo apt-get update
-  sudo apt-get upgrade -y
-  sudo apt-get dist-upgrade -y
-  sudo apt autoremove -y
-  sudo apt autoclean
-  sudo apt-get autoremove
-  
-  # Task Completed
-
-  echo -e "${LMAGENTA}"
-  echo -e "--------------------------------------------------"
-  echo -e " ${PERFORM} $TASK completed"
-  echo -e "--------------------------------------------------"
-  echo -e "${STD}"
+	TASKCOMPLETE
 
 else
 
-  echo ""
-  echo -e "--------------------------------------------------"
-  echo -e " You chose ${YELLOW}not${STD} to ${PERFORM} ${TASK}"
-  echo -e "--------------------------------------------------"
-  echo ""
+	CANCELTHIS
 
 fi
 
