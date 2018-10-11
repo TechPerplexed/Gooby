20051f52c8f769798bda21316df09f0b2afbd334
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-11 16:08:57 -0600

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -31,6 +31,6 @@ sudo chmod +x -R /opt/GooPlex/install
 sudo chmod +x -R /opt/GooPlex/menus
 sudo rsync -a /opt/GooPlex/install/gooplex /bin
 sudo chmod 755 /bin/gooplex
-sudo mkdir -p /var/local/GooPlex
+sudo mkdir -p /var/local/GooPlex/.config
 
 gooplex
