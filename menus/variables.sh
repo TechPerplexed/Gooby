02f66d9be6727a906377c43ddda283ec5df455d3
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-11 12:17:36 -0600

    Update variables.sh

--- menus/variables.sh
+++ menus/variables.sh
@@ -6,8 +6,8 @@ USERID=$(id -u $USER)
 GROUPID=$(id -g $USER)
 TIMEZONE=$(cat /etc/timezone)
 CONFIGS=/var/local/GooPlex
-EMAIL= $( cat /var/local/GooPlex/.config/setemail.txt )
-URL= $( cat /var/local/GooPlex/.config/seturl.txt )
+EMAIL=$( cat $CONFIGS/.config/setemail.txt )
+URL=$( cat $CONFIGS/.config/seturl.txt )
 
 # Define colors
 
