3a03726874d149f7ab665963d8282eadca43ac24
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-11 16:26:05 -0600

    Update email-set.sh

--- install/server/email-set.sh
+++ install/server/email-set.sh
@@ -13,15 +13,7 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 	sudo mkdir -p $CONFIGS/.config
 	sudo chown -R $USER:$USER $CONFIGS
 
-	if [ ! -f $CONFIGS/.config/setemail ]; then
-
-		echo "You have not set an email address yet"
-
-	else
-
-		echo "Your email address is currently set to $EMAIL"
-
-	fi
+	echo "Your email address is currently set to $EMAIL"
 
 	read -p "New email address: " SETEMAIL
 
