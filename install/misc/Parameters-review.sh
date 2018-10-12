0902ce3a48241aa068950885620c52a6999a9d8d
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-12 12:31:02 -0600

    Update Parameters-review.sh

--- install/misc/Parameters-review.sh
+++ install/misc/Parameters-review.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 clear
-
-EXPLAINTASK
-
-echo "These are your current settings:"
-echo ""  
-echo "You are currently logged in as $USERID"
+echo " ${CYAN}"
+MENUSTART
+echo " ${CYAN}These are your current settings:${STD}"
+echo "You are currently logged in as ${CYAN}$USER${STD}"
+echo " Your timezone is set to ${CYAN}$TIMEZONE${STD}"
+[[ -s $CONFIGS/.config/setemail ]] && echo " Your email address is currently set to ${CYAN}$EMAIL${STD}" || echo " You have not set an email address yet."
+[[ -s $CONFIGS/.config/seturl ]] && echo " Your URL is currently set to ${CYAN}$URL${STD}" || echo " You have not set an URL yet"
+echo " Your VPS IP address is ${CYAN}$PUBLICIP${STD}"
 echo ""
-echo "Your timezone is set to $TIMEZONE"
-echo ""
-[[ -s $CONFIGS/.config/setemail ]] && echo "Your email address is currently set to $EMAIL" || echo "You have not set an email address yet."
-echo ""
-[[ -s $CONFIGS/.config/seturl ]] && echo "Your URL is currently set to $URL" || echo "You have not set an URL yet"
-echo ""
-echo "Your VPS IP address is $PUBLICIP"
-echo ""
-echo "You can change these settings under menu option A - Server settings"
+echo " You can change these settings under menu option A - Server settings"
+MENUEND
 
 PAUSE
