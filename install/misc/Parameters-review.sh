efbdf34dedd36328736f8d791e439e21f7e78c92
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-12 12:33:16 -0600

    Update Parameters-review.sh

--- install/misc/Parameters-review.sh
+++ install/misc/Parameters-review.sh
@@ -3,14 +3,18 @@
 clear
 echo " ${CYAN}"
 MENUSTART
+
 echo " ${CYAN}These are your current settings:${STD}"
-echo "You are currently logged in as ${CYAN}$USER${STD}"
+echo ""
+echo " You are currently logged in as ${CYAN}$USER${STD}"
 echo " Your timezone is set to ${CYAN}$TIMEZONE${STD}"
 [[ -s $CONFIGS/.config/setemail ]] && echo " Your email address is currently set to ${CYAN}$EMAIL${STD}" || echo " You have not set an email address yet."
 [[ -s $CONFIGS/.config/seturl ]] && echo " Your URL is currently set to ${CYAN}$URL${STD}" || echo " You have not set an URL yet"
 echo " Your VPS IP address is ${CYAN}$PUBLICIP${STD}"
 echo ""
-echo " You can change these settings under menu option A - Server settings"
+echo " You can change these settings"
+echo " under menu option A - Server settings"
+
 MENUEND
 
 PAUSE
