fa5f43b36333a6f1236ef368c80f0b5f4aadc7d6
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-13 14:37:47 -0600

    Update environment-check.sh

--- install/misc/environment-check.sh
+++ install/misc/environment-check.sh
@@ -2,6 +2,9 @@
 
 MENU="Check Environment"
 
+[[ -s $CONFIGS/.config/seturl ]] && CHECKURL="$URL" || CHECKURL="Not set yet"
+[[ -s $CONFIGS/.config/email ]] && CHECKEMAIL="$EMAIL" || CHECKEMAIL="Not set yet"
+
 # Menu Options
 
 QUIT(){
@@ -15,9 +18,10 @@ show_menus() {
 	echo " ${CYAN}"
 	MENUSTART
 	echo " Timezone: ${CYAN}$TIMEZONE${STD}"
-	echo " Your Domain: ${CYAN}$URL${STD}"
-	echo " Your Email Address: ${CYAN}$EMAIL${STD}"
+	echo " Your Domain: ${CYAN}$CHECKURL${STD}"
+	echo " Your Email Address: ${CYAN}$CHECKEMAIL${STD}"
 	echo " Your Server IP Address: ${CYAN}$PUBLICIP${STD}"
+	echo ""
 	echo " ${WHITE}Z${STD} - EXIT to Main Menu"
 	echo " ${CYAN}"
 	MENUEND
