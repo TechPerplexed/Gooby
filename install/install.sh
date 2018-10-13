ae9b179b1261f9d41549f7a4cb9e2cd55241f850
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-12 21:55:56 -0700

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -22,7 +22,7 @@ denyhosts at sudo software-properties-common
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
-# sudo ufw --force enable
+sudo ufw --force enable
 
 sudo rm -r /opt/GooPlex
 sudo git clone https://github.com/TechPerplexed/GooPlex.git /opt/GooPlex
