3b939f8b2c17a5b38131d310d666df73359a42a3
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-13 13:46:03 -0600

    Update environment-check.sh

--- install/misc/environment-check.sh
+++ install/misc/environment-check.sh
@@ -4,6 +4,30 @@ MENU="Check Environment"
 
 # Menu Options
 
-echo "coming soon"
+QUIT(){
+	exit
+}
 
-PAUSE
+# Display menu
+
+show_menus() {
+	clear
+	echo " ${CYAN}"
+	MENUSTART
+echo "test"
+	echo " ${CYAN}"
+	MENUEND
+}
+
+# Read Choices
+
+read_options(){
+	local choice
+	read -n 1 -s -r -p "Choose option: " choice
+	case $choice in
+		[Zz]) QUIT ;;
+		*) echo "${LRED}Please select a valid option${STD}" && sleep 2
+	esac
+}
+
+MENUFINALIZE
