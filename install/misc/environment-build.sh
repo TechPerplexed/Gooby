fd1e5572822d8c37a4badd1b74a159f9bf7acb22
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-14 12:58:16 -0600

    Update environment-build.sh

--- install/misc/environment-build.sh
+++ install/misc/environment-build.sh
@@ -1,1 +1,55 @@
-echo "Coming soon"
+#!/bin/bash
+
+source /opt/GooPlex/menus/variables.sh
+
+DOCKERHOME=$CONFIGS/Docker
+sudo mkdir -p $DOCKERHOME/components
+sudo chown -R $USER:$USER $CONFIGS
+ENV=${DOCKERHOME}/.env
+touch ${ENV}
+
+# Load existing variables and use them as defaults, if available
+
+	while IFS='' read -r line || [[ -n "${line}" ]]; do
+		newline=$(echo ${line} | sed 's/=/&\"/g; s/:$//')\"
+		eval ${newline} > /dev/null 2>&1
+	done < ${ENV}
+
+if [[ ${1,,} != "rebuild" ]]; then
+
+	echo Checking system dependencies...
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - > /dev/null 2>&1
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /dev/null 2>&1
+	apt-get update > /dev/null 2>&1
+	sudo apt-get install -y docker-ce dialog > /dev/null 2>&1
+	sudo usermod -aG docker ${USER} > /dev/null 2>&1
+	sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose > /dev/null 2>&1
+
+	clear
+	echo "Creating configuration file..."
+
+fi
+
+echo "###########################" > ${ENV}
+echo "#  Environment variables  #" >> ${ENV}
+echo "#     for GooPlex v$VERSION    #" >> ${ENV}
+echo "###########################" >> ${ENV}
+echo >> ${ENV}
+echo "HOME=${HOME}" >> ${ENV}
+echo "USERID=$(id -u)" >> ${ENV}
+echo "GROUPID=$(id -g)" >> ${ENV}
+echo "TIMEZONE=$(cat /etc/timezone)" >> ${ENV}
+echo "CONFIGS=${CONFIGS}" >> ${ENV}
+echo "DOWNLOADS=${HOME}/Downloads" >> ${ENV}
+echo "DOWNLOADS2=/mnt/Google/Downloads" >> ${ENV}
+echo "GOOGLE=/media/Google" >> ${ENV}
+echo "MYDOMAIN=$(cat $CONFIGS/.config/mydomain)" >> ${ENV}
+echo "MYEMAIL=$(cat $CONFIGS/.config/myemail)" >> ${ENV}
+echo "MEDIA=${MEDIA}" >> ${ENV}
+echo "TV=${TV}" >> ${ENV}
+echo "MOVIES=${MOVIES}" >> ${ENV}
+echo "RCLONETARGET=${RCLONETARGET}" >> ${ENV}
+echo "MENU=${MENU}" >> ${ENV}
+
+cat ${CONFIGS}/Docker/components/??-* > ${DOCKERHOME}/docker-compose.yaml
+echo done
