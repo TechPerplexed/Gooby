f7cb0a9ebd25fb80d1494157999c794619a13321
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-14 13:15:13 -0600

    Update environment-build.sh

--- install/misc/environment-build.sh
+++ install/misc/environment-build.sh
@@ -17,7 +17,7 @@ touch ${ENV}
 
 if [[ ${1,,} != "rebuild" ]]; then
 
-	echo Checking system dependencies...
+	echo Checking system dependencies... This might be a minute.
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - > /dev/null 2>&1
 	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /dev/null 2>&1
 	apt-get update > /dev/null 2>&1
