94cecb2320036a9695a60fe45d4c1b28b6701b0d
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-14 19:22:21 -0600

    Update docker-install.sh

--- install/server/docker-install.sh
+++ install/server/docker-install.sh
@@ -1,50 +1,35 @@
 #!/bin/bash
 
-docker -v > /tmp/checkdocker.txt
+# install docker
 
-if [ ! -s /tmp/checkdocker.txt ]; then
-
-	# install docker
-
-	sudo apt-get -y install \
-		apt-transport-https \
-		acl \
-		ca-certificates \
-		curl \
-		software-properties-common
+sudo apt-get -y install \
+	apt-transport-https \
+	acl \
+	ca-certificates \
+	curl \
+	software-properties-common
 
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-	sudo add-apt-repository \
-		"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-		$(lsb_release -cs) \
-		stable"
+sudo add-apt-repository \
+	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) \
+	stable"
 
-	sudo apt-get update
+sudo apt-get update
 
-	sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-ce
 
-	sudo usermod -a -G docker $USER
-	sudo gpasswd -a $USER docker
-	sudo setfacl -m user:$USER:rw /var/run/docker.sock
+sudo usermod -a -G docker $USER
+sudo gpasswd -a $USER docker
+sudo setfacl -m user:$USER:rw /var/run/docker.sock
 
-fi
+# latest docker compose released tag
 
-docker-compose -v  > /tmp/checkcompose.txt
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
 
-if [ ! -s /tmp/checkcompose.txt ]; then
-	
-	# get latest docker compose released tag
+# Install docker-compose
 
-	COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
-
-	# Install docker-compose
-
-	sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-	sudo chmod +x /usr/local/bin/docker-compose
-	sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
-
-fi
-
-rm /tmp/checkdocker.txt
-rm /tmp/checkcompose.txt
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
