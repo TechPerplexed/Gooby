128be6791b68e0ee4d9f11066e06d3ddd49af935
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-18 16:39:52 -0600

    Update install-plex.sh

--- install/apps/install-plex.sh
+++ install/apps/install-plex.sh
@@ -17,6 +17,25 @@ else
 
 		GOAHEAD
 
+		echo ""
+		echo "Please choose what version you want to install:"
+		echo ""
+		echo "Plex regular without CloudFlare (A) - default"
+		echo "Plex regular with CloudFlare (B)?
+		echo "Plex Pass without CloudFlare (C)"
+		echo "Plex Pass with CloudFlare (D)"
+		echo ""
+		echo "(When in doubt, choose A)"
+		read -n 1 -s -r -p " ---> "
+		
+		case "$REPLY" in
+			a|A ) echo "Regular installation without CloudFlare..." ;;
+			b|B ) APPLOC=$APPLOC-cf; echo "Regular installation with CloudFlare..." ;;
+			c|C ) APPLOC=$APPLOC-pp; echo "Plex Pass without CloudFlare..." ;;
+			d|D ) APPLOC=$APPLOC-pp-cf; echo "Plas Pass with CloudFlare..." ;;
+			* ) echo "No choice made, will use regular installation..." ;;
+		esac
+
 		cd $CONFIGS/Docker
 		sudo rsync -a /opt/GooPlex/scripts/components/$APPLOC.yaml $CONFIGS/Docker/components
 		/usr/local/bin/docker-compose down
