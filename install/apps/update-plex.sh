980f50572f6b81bd243dbbd2854ae8bbf00e5244
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-18 13:07:14 -0600

    Create update-plex.sh

---
+++ install/apps/update-plex.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+docker ps -q -f name=$APP > /tmp/checkapp.txt
+clear
+
+if [ ! -s /tmp/checkapp.txt ]; then
+
+	NOTINSTALLED
+
+else
+
+	EXPLAINTASK
+
+	CONFIRMATION
+
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+		GOAHEAD
+		RUNPATCHES
+
+		docker stop $APP
+		docker start $APP
+
+		TASKCOMPLETE
+
+	else
+
+		CANCELTHIS
+
+	fi
+
+fi
+
+rm /tmp/checkapp.txt
+PAUSE
