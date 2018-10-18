efbed012d32197ac4eab8a6fdba42fc1b4adac03
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-18 13:02:55 -0600

    Update install-emby.sh

--- install/apps/install-emby.sh
+++ install/apps/install-emby.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-ls /opt/sonarr > /tmp/checkapp.txt; 
-ls /opt/nzbdrone >> /tmp/checkapp.txt
+docker ps -q -f name=$APP > /tmp/checkapp.txt
 clear
 
 if [ -s /tmp/checkapp.txt ]; then
@@ -10,51 +9,24 @@ if [ -s /tmp/checkapp.txt ]; then
 
 else
 
-	EXPLAINTASK
+	EXPLAINAPP
 
 	CONFIRMATION
 
 	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 		GOAHEAD
-		RUNPATCHES
 
-		# Dependencies
+		cd $CONFIGS/Docker
+		sudo rsync -a /opt/GooPlex/scripts/components/$APPLOC $CONFIGS/Docker/components
+		/usr/local/bin/docker-compose down
+		echo "Just a moment while $APP is being installed..."
+		source /opt/GooPlex/install/misc/environment-build.sh rebuild
+		/usr/local/bin/docker-compose up -d --remove-orphans ${@:2}
+		sudo chown -R $USER:$USER $CONFIGS/$TASK
+		cd "${CURDIR}"
 
-		sudo -s apt-get -y install \
-			libcurl3 \
-			libmono-cil-dev \
-			mono-devel \
-			mediainfo \
-			sqlite3 \
-			apt-transport-https \
-		denyhosts at sudo software-properties-common
-		
-		sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-		echo "deb https://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-		sudo apt -y update
-
-		# Open ports
-
-		sudo ufw allow 8989
-
-		# Main script
-
-		sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 2A9B4BF8
-		sudo echo "deb https://dl.bintray.com/sonarr/phantom wheezy main" | sudo tee /etc/apt/sources.list.d/sonarr.list
-
-		sudo apt-get -y update
-		clear
-		sudo apt-get -y install sonarr
-
-		# sudo chown -R plexuser:plexuser /opt/sonarr
-
-		# sudo rsync -a /opt/GooPlex/scripts/sonarr.service /etc/systemd/system/sonarr.service
-		# sudo systemctl enable sonarr.service
-		# sudo systemctl daemon-reload
-		# sudo systemctl start sonarr.service
-		
-		# sudo chown -R plexuser:plexuser $HOME
+		APPINSTALLED
 
 		TASKCOMPLETE
 
