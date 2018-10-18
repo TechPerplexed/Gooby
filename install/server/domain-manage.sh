c997b40a8394aa3dc4cd78544c47ad6cd90db4d3
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-18 13:47:10 -0600

    Update domain-manage.sh

--- install/server/domain-manage.sh
+++ install/server/domain-manage.sh
@@ -24,9 +24,13 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 		MYDOMAIN=$(cat $CONFIGS/.config/mydomain)
 
-		/opt/GooPlex/install/misc/environment-build.sh rebuild
-		source $CONFIGS/Docker/.env
+		/usr/local/bin/docker-compose down
+		echo "Just a moment while your new domain is being installed..."
+		source /opt/GooPlex/install/misc/environment-build.sh rebuild
+		/usr/local/bin/docker-compose up -d --remove-orphans ${@:2}
 
+		clear
+		source $CONFIGS/Docker/.env
 		echo "Your new domain is set to $MYDOMAIN"
 		echo "Remember to point it to IP address $IP"
 
