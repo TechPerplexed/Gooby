ccb7f71911612fadfe5ba16854a8c658dc655005
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-18 11:48:34 -0600

    Create install-netdata.sh

---
+++ install/apps/install-netdata.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+docker ps -q -f name=$APP > /tmp/checkapp.txt
+clear
+
+if [ -s /tmp/checkapp.txt ]; then
+
+	ALREADYINSTALLED
+
+else
+
+	EXPLAINAPP
+
+	CONFIRMATION
+
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+		GOAHEAD
+
+		cd $CONFIGS/Docker
+		sudo rsync -a /opt/GooPlex/scripts/components/$APPLOC $CONFIGS/Docker/components
+		/usr/local/bin/docker-compose down
+		echo "Just a moment while $APP is being installed..."
+		source /opt/GooPlex/install/misc/environment-build.sh rebuild
+		/usr/local/bin/docker-compose up -d --remove-orphans ${@:2}
+		cd "${CURDIR}"
+
+		APPINSTALLED
+
+		TASKCOMPLETE
+
+	else
+
+		CANCELTHIS
+
+	fi
+
+fi
+
+rm /tmp/checkapp.txt
+PAUSE
