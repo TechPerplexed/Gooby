5235737e789233236e6ab53215e7378658dd1ee3
techperplexed <sophiaburns@gmail.com>
2018-10-19 10:42:13 -0700

    Revert "Delete remove-generic.sh"
    This reverts commit 69f5f44a7b58afdb88b7645ce7ecb14915869909.

---
+++ install/apps/remove-generic.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+docker ps -q -f name=$APP > $CONFIGS/.config/checkapp.txt
+clear
+
+if [ ! -s /tmp/checkapp.txt ]; then
+
+	NOTINSTALLED
+
+else
+
+	EXPLAINAPP
+
+	CONFIRMATION
+
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+		GOAHEAD
+
+		cd $CONFIGS/Docker
+		sudo rm $CONFIGS/Docker/components/$APPLOC*
+		/usr/local/bin/docker-compose down
+		echo "Just a moment while $APP is being uninstalled..."
+		source /opt/GooPlex/install/misc/environment-build.sh rebuild
+		/usr/local/bin/docker-compose up -d --remove-orphans ${@:2}
+		cd "${CURDIR}"
+		
+		clear
+
+		echo "$TASK has been removed".
+
+		CONFIRMDELETE
+
+		case "$REPLY" in
+			y|Y ) sudo rm -r $CONFIGS/$TASK ;;
+			* ) echo "User settings not deleted" ;;
+		esac
+
+		TASKCOMPLETE
+
+	else
+
+		CANCELTHIS
+
+	fi
+
+fi
+
+rm $CONFIGS/.config/checkapp.txt
+PAUSE
