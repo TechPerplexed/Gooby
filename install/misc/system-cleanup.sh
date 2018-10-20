9870aac6b2e664d024afe2fff2fcf7c4ffed00f0
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-20 17:38:27 -0600

    Update system-cleanup.sh

--- install/misc/system-cleanup.sh
+++ install/misc/system-cleanup.sh
@@ -11,27 +11,23 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 	echo ""
 	echo "Giving your system a nice little spring cleaning..."
 	echo ""
+
 	cd $CONFIGS/Docker
 	/usr/local/bin/docker-compose down
 	sudo systemctl daemon-reload
 	# sudo systemctl stop mergerfs
 	sudo systemctl stop rclone
 
-	sudo rm -r ${MOUNTTO}
-	echo -n >  ${LOGS}
-	rm ${HOME}/logs/*.?.txt > /dev/null
-
 	sudo systemctl start rclone
 	sleep 10
 	# sudo systemctl start mergerfs
-
-	docker system prune -a -f --volumes
 
 	docker system prune -a -f --volumes
 	source /opt/GooPlex/install/misc/environment-build.sh rebuild
 	/usr/local/bin/docker-compose up -d --remove-orphans ${@:2}
 
 	sudo chown -R $USER:$USER $CONFIGS
+	sudo chown -R $USER:$USER $HOME
 	cd "${CURDIR}"
 
 	clear
