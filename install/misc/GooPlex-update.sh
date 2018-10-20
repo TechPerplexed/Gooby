398492eb5274c3f48c2a2f3577702577d7e5829f
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-20 17:17:39 -0600

    Update GooPlex-update.sh

--- install/misc/GooPlex-update.sh
+++ install/misc/GooPlex-update.sh
@@ -4,25 +4,13 @@ clear
 
 EXPLAINTASK
 
-echo "${LRED}"
-echo "--------------------------------------------------"
-echo " DANGER ZONE - EXTREME CAUTION!!!"
-echo " You are about to switch back to"
-echo " the original branch of GooPlex"
-echo " There is NO downgrade path!!"
-echo " Many functions won't work any longer."
-echo " Please only downgrade if you haven't"
-echo " made any changes yet!"
-echo "--------------------------------------------------"
-echo "${STD}"
-
 CONFIRMATION
 
 if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 	clear
 	sudo rm -r /opt/GooPlex
-	sudo git clone -b legacy https://github.com/TechPerplexed/GooPlex /opt/GooPlex
+	sudo git clone -b develop https://github.com/TechPerplexed/GooPlex /opt/GooPlex
 	sudo chmod +x -R /opt/GooPlex/install
 	sudo chmod +x -R /opt/GooPlex/menus
 	sudo rsync -a /opt/GooPlex/install/gooplex /bin
