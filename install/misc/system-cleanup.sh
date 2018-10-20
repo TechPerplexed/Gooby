4527d3fc8f4b35a7f94e30c2fd18b789ea346b46
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-20 17:32:09 -0600

    Create system-cleanup.sh

---
+++ install/misc/system-cleanup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+clear
+
+EXPLAINTASK
+
+CONFIRMATION
+
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+	echo ""
+	echo "Giving your system a nice little spring cleaning..."
+	echo ""
+	cd $CONFIGS/Docker
+	/usr/local/bin/docker-compose down
+	sudo systemctl daemon-reload
+	# sudo systemctl stop mergerfs
+	sudo systemctl stop rclone
+
+	sudo rm -r ${MOUNTTO}
+	echo -n >  ${LOGS}
+	rm ${HOME}/logs/*.?.txt > /dev/null
+
+	sudo systemctl start rclone
+	sleep 10
+	# sudo systemctl start mergerfs
+
+	docker system prune -a -f --volumes
+
+	docker system prune -a -f --volumes
+	source /opt/GooPlex/install/misc/environment-build.sh rebuild
+	/usr/local/bin/docker-compose up -d --remove-orphans ${@:2}
+
+	sudo chown -R $USER:$USER $CONFIGS
+	cd "${CURDIR}"
+
+	clear
+
+	TASKCOMPLETE
+
+else
+
+	CANCELTHIS
+
+fi
+
+PAUSE
