9fd8cdc1fd08f2ed450806339a94974c6605824e
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-20 18:38:26 -0600

    Update server-init.sh

--- install/server/server-init.sh
+++ install/server/server-init.sh
@@ -11,8 +11,8 @@ echo "Then you get can a free domain for example at Freenom."
 echo ""
 echo "${YELLOW}Please answer the following two questions:${STD}"
 echo ""
-read -e -p 'What ${YELLOW}domain${STD} will you be using to access your server (ex: mydomain.com)?  ' -i "${MYDOMAIN}" MYDOMAIN
-read -e -p 'What is your ${YELLOW}email${STD} address for certificate registration (ex: myname@mydomain.com)  ' -i "${MYEMAIL}" MYEMAIL
+read -e -p 'What domain will you be using to access your server (ex: mydomain.com)?  ' -i "${MYDOMAIN}" MYDOMAIN
+read -e -p 'What is your email address for certificate registration (ex: myname@mydomain.com)  ' -i "${MYEMAIL}" MYEMAIL
 echo ""
 echo "${YELLOW}Thank you! Please hang tight while we get some things ready...${STD}"
 echo "" && sleep 10
