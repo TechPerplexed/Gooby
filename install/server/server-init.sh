0a5fa51ef3bd2fb527bca2b107ed424c693b9e60
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-23 11:55:10 -0600

    Update server-init.sh

--- install/server/server-init.sh
+++ install/server/server-init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-source /opt/GooPlex/menus/variables.sh
+source /opt/Gooby/menus/variables.sh
 
 clear
 echo ""
-echo "${YELLOW}Welcome to the renewed GooPlex $VERSION!${STD}"
+echo "${YELLOW}Welcome to Gooby $VERSION!${STD}"
 echo ""
 echo "You will need to own a domain... if you don't have one"
 echo "Then you get can a free domain for example at Freenom."
@@ -16,11 +16,13 @@ read -e -p 'What is your email address for certificate registration (ex: myname@
 echo ""
 echo "${YELLOW}Thank you! Please hang tight while we get some things ready...${STD}"
 echo "" && sleep 10
-source /opt/GooPlex/install/server/docker-install.sh
+source /opt/Gooby/install/server/docker-install.sh
 sudo mkdir -p $CONFIGS/.config
+sudo mkdir -p $TCONFIGS
 sudo mkdir -p $CONFIGS/Docker/components
 sudo chown -R $USER:$USER $CONFIGS
+sudo chown -R $USER:$USER $TCONFIGS
 echo "$MYDOMAIN" > $CONFIGS/.config/mydomain
 echo "$MYEMAIL" > $CONFIGS/.config/myemail
-sudo rsync -a /opt/GooPlex/scripts/components/{00-AAA.yaml,01-proxy.yaml} $CONFIGS/Docker/components
-source /opt/GooPlex/install/misc/environment-build.sh
+sudo rsync -a /opt/Gooby/scripts/components/{00-AAA.yaml,01-proxy.yaml} $CONFIGS/Docker/components
+source /opt/Gooby/install/misc/environment-build.sh
