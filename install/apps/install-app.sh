cc99c907bfd94f6ae43606ed354f2cc9db8a2c52
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-24 15:51:27 -0600

    Update install-app.sh

--- install/apps/install-app.sh
+++ install/apps/install-app.sh
@@ -60,7 +60,20 @@ else
 
 		if [[ $TASK = "Plex" && ! -d $OLDLOC && ! -d $CONFIGS/$TASK ]]; then
 
-			source /opt/Gooby/install/apps/plex-token.sh
+			clear
+			echo "${YELLOW}"
+			echo "--------------------------------------------------"
+			echo " It seems you are new to $TASK"
+			echo " In order to proceed, you will need to visit"
+			echo " https://www.${LYELLOW}plex.tv/claim${YELLOW}"
+			echo " And copy the token to clipboard"
+			echo " Make sure you active the token within 4 minutes
+			echo " at ${WHITE}$APP.$MYDOMAIN$:8443{YELLOW}"
+			echo "--------------------------------------------------"
+			echo "${STD}"
+			read -e -p " Paste token here: " PLEXCLAIM
+			echo ""
+			echo "$PLEXCLAIM" > $CONFIGS/.config/plexclaim
 
 		fi
 
