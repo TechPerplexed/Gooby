79eb2516e2c3216cabb7d169b906cb8a3fb5ee38
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-23 22:18:47 -0700

    Update backup-create.sh

--- install/misc/backup-create.sh
+++ install/misc/backup-create.sh
@@ -46,9 +46,9 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 		if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
-			(crontab -l 2>/dev/null; echo "5 1 * * Sun /opt/Gooby/scripts/backup.sh > /dev/null 2>&1") | crontab -
+			(crontab -l 2>/dev/null; echo "15 2 * * SUN /opt/Gooby/scripts/cron/backup.sh > /dev/null 2>&1") | crontab -
 			touch $TCONFIGS/cronbackup
-			echo " Backup scheduled to run at 01:15 every Sunday"
+			echo " Backup scheduled to run at 02:15 every Sunday"
 			echo " You can always change this by typing ${LYELLOW}crontab -e${STD}"
 
 		else
