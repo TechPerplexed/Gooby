3672ba55842685028b711fcdf8c25a0921f0e2b0
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-23 20:48:57 -0600

    Create backup.sh

---
+++ scripts/backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /opt/Gooby/menus/variables.sh
+
+BACKUP=/tmp/$(date +%F).tar.gz
+sudo tar -cf ${BACKUP} $CONFIGS --exclude-caches-all
+sudo chown $USER:$USER ${BACKUP}
+/usr/bin/rclone copy ${BACKUP} Gdrive:/Backup/$(hostname) --checksum --drive-chunk-size=64M
+sudo rm ${BACKUP}
+sudo chown $USER:$USER ${CONFIGS}
+sudo chown $USER:$USER ${HOME}
