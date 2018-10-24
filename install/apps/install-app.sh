8ecaeed25a815c4c8ee1bd369425bc7616f4737c
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-24 13:12:26 -0600

    Update install-app.sh

--- install/apps/install-app.sh
+++ install/apps/install-app.sh
@@ -60,18 +60,7 @@ else
 
 		if [[ $TASK = "Plex" && ! -d $OLDLOC && ! -d $CONFIGS/$TASK ]]; then
 
-			clear
-			echo "${YELLOW}"
-			echo "--------------------------------------------------"
-			echo " It seems you are new to $TASK"
-			echo " In order to proceed, you will need to visit"
-			echo " https://www.${LYELLOW}plex.tv/claim${YELLOW}"
-			echo " And copy the token to clipboard"
-			echo "--------------------------------------------------"
-			echo "${STD}"
-			read -e -p " Paste token here: " PLEXCLAIM
-			echo ""
-			echo "$PLEXCLAIM" > $CONFIGS/.config/plexclaim
+			source /opt/Gooby/install/apps/plex-token.sh
 
 		fi
 
