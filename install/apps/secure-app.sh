374dd3471f97204e8d58698059e378263dd14071
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-26 14:23:05 -0600

    Update secure-app.sh

--- install/apps/secure-app.sh
+++ install/apps/secure-app.sh
@@ -17,10 +17,60 @@ else
 
 		GOAHEAD
 
-		echo "Coming soon!"
+		FILENAME=$APP.$MYDOMAIN
+		cd $CONFIGS/Security
 
-#		docker stop $APP
-#		docker start $APP
+		if [ -f $FILENAME ]; then
+
+			echo " Current users for $TASK:"
+			echo
+			cat -s -n $FILENAME
+
+			# Remove user
+			echo " Removing users coming soon..."
+			echo
+
+	else
+
+		echo "You haven't added any passwords to $TASK yet"
+
+	fi
+
+	# Add user
+
+		echo
+		read -p "Name: " USERNAME
+
+	try_install() {
+		dpkg -l "$1" | grep -q ^ii && return 1		# Package already installed
+		echo
+		echo Installing ${@}
+		echo
+		sudo apt -y install "$@"
+		return 0
+	}
+
+		# Check to see if apache2-utils are installed.  If not, then install them.
+
+		try_install apache2-utils
+
+		echo
+
+		if [[ -e ${FILENAME} ]]
+		then							# File exists so append with new user
+			echo Updating file ${FILENAME}
+			echo Adding  user ${USERNAME}
+			htpasswd ${FILENAME} ${USERNAME}
+		else							# Create file then create first user
+			echo Creating file ${FILENAME}
+			echo Adding user ${USERNAME}
+			htpasswd -c ${FILENAME} ${USERNAME}
+		fi
+
+		docker stop $APP
+		docker start $APP
+
+		cd $CURDIR
 
 		TASKCOMPLETE
 
