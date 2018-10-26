573ef40fbdfcf4fc35bcb28ddb2baccca00bb9e2
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-26 10:18:11 -0600

    Create secure-app.sh

---
+++ install/apps/secure-app.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+docker ps -q -f name=$APP > $TCONFIGS/checkapp
+clear
+
+if [ ! -s $TCONFIGS/checkapp ]; then
+
+	NOTINSTALLED
+
+else
+
+	EXPLAINTASK
+
+	CONFIRMATION
+
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+		GOAHEAD
+		RUNPATCHES
+
+		echo "Coming soon!"
+
+		docker stop $APP
+		docker start $APP
+
+		TASKCOMPLETE
+
+	else
+
+		CANCELTHIS
+
+	fi
+
+fi
+
+rm $TCONFIGS/checkapp
+PAUSE
