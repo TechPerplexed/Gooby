9c1616c4ad276f6c674aef9b34d1e59abefe6c64
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-27 19:30:12 -0600

    Update backup.sh

--- scripts/cron/backup.sh
+++ scripts/cron/backup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 source /opt/Gooby/menus/variables.sh
-BACKUP=/tmp/$(date +%F).tar.gz
+
+echo -e "${LMAGENTA}Creating backup file...${STD}"
+
 sudo tar -cf ${BACKUP} $CONFIGS --exclude-caches-all
+
 sudo chown $USER:$USER ${BACKUP}
+
+echo -e "${GREEN}Copying to Google...${STD}"
+
 /usr/bin/rclone copy ${BACKUP} Gdrive:/Backup/$(hostname) --checksum --drive-chunk-size=64M
 sudo rm ${BACKUP}
+
 sudo chown $USER:$USER ${CONFIGS}
 sudo chown $USER:$USER ${HOME}
