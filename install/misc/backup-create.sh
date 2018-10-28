a27e953a56e93a6cfd3e90ee59060ed80eae7dd5
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-27 19:31:01 -0600

    Update backup-create.sh

--- install/misc/backup-create.sh
+++ install/misc/backup-create.sh
@@ -16,19 +16,7 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 	echo -e "Please don't exit the terminal until it's done!"
 	echo ""
 
-	echo -e "${LMAGENTA}Creating backup file...${STD}"
-
-	sudo tar -cf ${BACKUP} $CONFIGS --exclude-caches-all
-
-	sudo chown $USER:$USER ${BACKUP}
-
-	echo -e "${GREEN}Copying to Google...${STD}"
-
-	/usr/bin/rclone copy ${BACKUP} Gdrive:/Backup/$(hostname) --checksum --drive-chunk-size=64M
-	sudo rm ${BACKUP}
-
-	sudo chown $USER:$USER ${CONFIGS}
-	sudo chown $USER:$USER ${HOME}
+	source /opt/Gooby/scripts/cron/backup.sh
 
 	clear
 	echo "${YELLOW}"
