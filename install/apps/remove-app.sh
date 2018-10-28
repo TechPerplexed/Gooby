0364d5c783533966befd343b2ece30867508d628
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-27 19:45:27 -0600

    Update remove-app.sh

--- install/apps/remove-app.sh
+++ install/apps/remove-app.sh
@@ -20,14 +20,14 @@ else
 		cd $CONFIGS/Docker
 		sudo rm $CONFIGS/Docker/components/$APPLOC*
 		/usr/local/bin/docker-compose down
-		echo "Just a moment while $APP is being uninstalled..."
+		echo " Just a moment while $APP is being uninstalled..."
 		source /opt/Gooby/install/misc/environment-build.sh rebuild
 		/usr/local/bin/docker-compose up -d --remove-orphans ${@:2}
 		cd "${CURDIR}"
 		
 		clear
 
-		echo "$TASK has been removed".
+		echo " $TASK has been removed".
 
 		CONFIRMDELETE
 
