2a84c3c972197db27dc51b545cc201918991757b
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-10-27 19:26:22 -0600

    Update rclean.sh

--- scripts/cron/rclean.sh
+++ scripts/cron/rclean.sh
@@ -2,22 +2,25 @@
 
 source /opt/Gooby/menus/variables.sh
 
-echo Taking containers down
+echo "${LYELLOW}${LYELLOW}Taking containers down${STD}"
+echo
 
 cd $CONFIGS/Docker
 /usr/local/bin/docker-compose down
 
-echo Taking services down
+echo "${LYELLOW}Taking services down${STD}"
+echo
 
 sudo systemctl daemon-reload
 # sudo systemctl stop mergerfs
 sudo systemctl stop rclone
 
-echo Making sure components are up to date
+echo "${LYELLOW}Making sure components are up to date
 
 sudo rsync -a /opt/Gooby/scripts/components/{00-AAA.yaml,01-proxy.yaml} $CONFIGS/Docker/components
 
-# Update Rclone to latest version
+echo "${LYELLOW}Update Rclone to latest version${STD}"
+echo
 
 touch $TCONFIGS/rclonev
 if [ $( cat $TCONFIGS/rclonev ) = "Stable" ]; then
@@ -26,26 +29,28 @@ elif [ $( cat $TCONFIGS/rclonev ) = "Beta" ]; then
 	curl https://rclone.org/install.sh | sudo bash -s beta
 fi
 
-echo Bringing services back up
+echo "${LYELLOW}Bringing services back up${STD}"
+echo
 
 sudo systemctl start rclone
 sleep 10
 # sudo systemctl start mergerfs
 
-echo Pruning old volumes
+echo "${LYELLOW}Pruning old volumes${STD}"
+echo
 
 docker system prune -a -f --volumes
 
-echo Bringing system back online
+echo "${LYELLOW}Bringing system back online${STD}"
+echo
 
 source /opt/Gooby/install/misc/environment-build.sh rebuild
 /usr/local/bin/docker-compose up -d --remove-orphans ${@:2}
 
-echo Restoring permissions
+echo "${LYELLOW}Restoring permissions... this could take a few minutes${STD}"
+echo
 
 sudo chown -R $USER:$USER $CONFIGS
 sudo chown -R $USER:$USER $TCONFIGS
 sudo chown -R $USER:$USER $HOME
 cd "${CURDIR}"
-
-echo Finalizing
