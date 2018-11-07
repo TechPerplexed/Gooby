af88da9e855ecf931ee2644aee69a00a4eaf6c9f
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-11-07 13:50:53 -0700

    Update install-rclone.sh

--- install/apps/install-rclone.sh
+++ install/apps/install-rclone.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source $CONFIGS/Docker/.env
 which rclone > $TCONFIGS/checkapp
 clear
 
@@ -9,7 +10,7 @@ if [ -s $TCONFIGS/checkapp ]; then
 
 	echo "Here is a list of the root folders in your mount:"
 	echo
-	ls -1 /media/Google
+	ls -1 ${MOUNTTO}
 	echo
 	echo "If you're getting an error here, please"
 	echo "check your Rclone config settings."
@@ -25,23 +26,25 @@ else
 
 		GOAHEAD
 		RUNPATCHES
-
+		
 		# Install MergerFS (for future use)
 
-		lsb_release -r -s > /tmp/version
-		VERSION=$( cat /tmp/version )
+		lsb_release -r -s > /$CONFIGS/osversion
+		VERSION=$( cat /$CONFIGS/osversion )
 
-		if [ "$VERSION" = "18.04" ]; then sudo apt-get -y install mergerfs; fi
+		if [ "$VERSION" = "18.04" ]; then
+			sudo apt-get -y install mergerfs
+		else
+			sudo wget https://github.com/trapexit/mergerfs/releases/download/2.24.2/mergerfs_2.24.2.ubuntu-xenial_amd64.deb -O /tmp/mergerfs.deb
+			sudo dpkg -i /tmp/mergerfs.deb
+		fi
 
-		rm /tmp/version
+		rm /$CONFIGS/osversion
 
 		# Main script
 
+		sudo mkdir -p $TCONFIGS
 		cd /tmp
-		sudo mkdir $TCONFIGS
-		
-		clear
-
 		read -n 1 -s -r -p "Stable ${YELLOW}(S)${STD} or Beta installation ${YELLOW}(B)?${STD} " -i "S" choice
 
 		case "$choice" in 
@@ -53,29 +56,42 @@ else
 
 		echo "${YELLOW}Please follow the instructions to setup Rclone${STD}"
 		echo
-		echo "Make sure you name the config ${YELLOW}Gdrive${STD}"
-		echo
 		sudo rclone config
+		echo
+		read -e -p "What name did you choose for your mount? " -i "Gdrive" RCLONESERVICE
+		echo
+		read -e -p "Folder in $GDRIVE to mount: (press Enter for root): " -i "" FOLDER
+
+		echo
 
 		# Installing Services
 
-		sudo mkdir -p /var/local/Gooby/.config
-		sudo mkdir -p /var/local/.
+		RCLONESERVICE=${RCLONESERVICE#:}; echo $RCLONESERVICE > $CONFIGS/.config/rcloneservice
+		RCLONEFOLDER=${RCLONEFOLDER%/}; RCLONEFOLDER=${RCLONEFOLDER#/}; echo $RCLONEFOLDER > $CONFIGS/.config/rclonefolder
+		RCLONEMOUNT=/mnt/rclone; echo $RCLONEMOUNT > $CONFIGS/.config/rclonemount
+		MOUNTTO=/mnt/merger; echo $MOUNTTO > $CONFIGS/.config/mountto
+		UPLOADS=/mnt/uploads; echo $UPLOADS > $CONFIGS/.config/uploads
+
 		sudo mkdir -p $HOME/logs
 		sudo mkdir -p $HOME/Downloads
 		sudo mkdir -p /media/Google
 
-		sudo rsync -a $HOME/.config/rclone/rclone.conf $CONFIGS/.config
-		sudo rsync -a /opt/Gooby/scripts/rclone.service /etc/systemd/system/rclone.service
+		sudo rsync -a /opt/Gooby/scripts/rclonefs.service /etc/systemd/system/rclonefs.service
+		sudo rsync -a /opt/Gooby/scripts/merger.service /etc/systemd/system/mergerfs.service
 
 		sudo chown -R $USER:$USER $CONFIGS
 		sudo chown -R $USER:$USER $TCONFIGS
 		sudo chown -R $USER:$USER $HOME
-		sudo chown -R $USER:$USER /media/Google
-
-		sudo systemctl enable rclone.service
+		sudo chown -R $USER:$USER $RCLONEMOUNT
+		sudo chown -R $USER:$USER $MOUNTTO
+		sudo chown -R $USER:$USER $UPLOADS
+		
+		sudo systemctl enable mergerfs.service
+		sudo systemctl enable rclonefs.service
 		sudo systemctl daemon-reload
-		sudo systemctl start rclone.service
+		sudo systemctl start mergerfs.service
+		wait 10
+		sudo systemctl start rclonefs.service
 
 		echo
 		echo "Done!"
