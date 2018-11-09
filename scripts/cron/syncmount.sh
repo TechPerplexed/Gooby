6ee90462b5f9ed557188a648bf4c01c0820f4a4a
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-11-09 11:39:15 -0700

    Update syncmount.sh

--- scripts/cron/syncmount.sh
+++ scripts/cron/syncmount.sh
@@ -4,38 +4,47 @@ if pidof -o %PPID -x "$(basename $0)"; then
 	exit 1
 fi
 
+source ~/.bashrc ~/.profile
 source /var/local/Gooby/Docker/.env
 
 PID=${$}
+CDIR=/var/local/Gooby/Docker
+ENV=${CDIR}/.env
+
+
+# Load existing variables and use them as defaults, if available
+
+source ${ENV}
+
 AGE=2		# How many minutes old a file must be before copying/deleting
-SYNCTIME=5	# How many minutes does it take your mount to notice a change (max)
-LOG=${HOME}/logs/mounter-sync.log
-COMMANDS=${HOME}/logs/commands.sh
-META=".unionfs-fuse"
-SUFFIX="_HIDDEN~"
+LOG=${LOGS}/mounter-sync.log
+COMMANDS=${LOGS}/commands.sh
 
 echo Starting sync at $(date) | tee -a ${LOG}
 
 # Identify files needing to be copied
 
-find ${UPLOADS} ! -path "*Downloads*" ! -path *${META}* -type f -mmin +${AGE} | sed 's|${UPLOADS}||' | sort > $CONFIGS/.config/filesmissing
+find ${UPLOADS}/ ! -path "*Downloads*" -type f -mmin +${AGE} | sed 's|'${UPLOADS}'||' | sort > /tmp/filesmissing
 
 # Copy files
 
-if [[ -s $CONFIGS/.config/filesmissing ]]
+if [[ -s /tmp/filesmissing ]]
 then
 	echo Copying files | tee -a ${LOG}
-	cat $CONFIGS/.config/filesmissing | tee -a ${LOG}
-	echo Files to copy: $(cat $CONFIGS/.config/filesmissing |wc -l) | tee -a ${LOG}
-	/usr/bin/rclone move ${UPLOADS} ${RCLONESERVICE}:${RCLONEFOLDER} --checkers 3 --fast-list --tpslimit 3 --delete-during --delete-excluded --checksum --transfers 3 --drive-chunk-size=16M --files-from $CONFIGS/.config/filesmissing -v
+	cat /tmp/filesmissing | tee -a ${LOG}
+	echo Files to copy: $(cat /tmp/filesmissing |wc -l) | tee -a ${LOG}
+	echo /usr/bin/rclone move ${UPLOADS} ${RCLONESERVICE}:${RCLONEFOLDER} --checkers 3 --fast-list --tpslimit 2 --delete-during --delete-excluded --checksum --transfers 3 --drive-chunk-size=16M --exclude "Downloads/**" --min-age ${AGE}m
+	/usr/bin/rclone move ${UPLOADS} ${RCLONESERVICE}:${RCLONEFOLDER} --checkers 3 --fast-list --tpslimit 2 --delete-during --delete-excluded --checksum --transfers 3 --drive-chunk-size=16M --exclude "Downloads/**" --min-age ${AGE}m
+	#/usr/bin/rclone move ${UPLOADS} ${RCLONESERVICE}:${RCLONEFOLDER} --checkers 3 --fast-list --tpslimit 2 --delete-during --delete-excluded --checksum --transfers 3 --drive-chunk-size=16M --files-from /tmp/filesmissing -v
 else
 	echo Nothing to copy | tee -a ${LOG}
 fi
 
 # Cleanup letovers
 
-rm $CONFIGS/.config/filesmissing
+rm /tmp/filesmissing
 cd ${UPLOADS}
 find . -type d -empty -delete
+mkdir -p ${UPLOADS} ${UPLOADS}/Downloads
 echo Finished at $(date) | tee -a ${LOG}
 echo --------------------------------------------------- | tee -a ${LOG}
