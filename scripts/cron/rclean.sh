e54c831a0abaf5673f7357faf1c5cc5e44d1a5e1
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-11-14 16:48:47 -0700

    Update rclean.sh

--- scripts/cron/rclean.sh
+++ scripts/cron/rclean.sh
@@ -59,7 +59,7 @@ if [ -f /etc/systemd/system/rclone.service ]; then sudo systemctl start rclone; 
 sudo mkdir -p ${RCLONEMOUNT} ${MOUNTTO}
 sudo chown -R $USER:$USER $RCLONEMOUNT $MOUNTTO
 
-wait 10; sudo systemctl start gooby
+sleep 10; sudo systemctl start gooby
 
 echo
 echo "${LYELLOW}Checking for updated containers${STD}"
