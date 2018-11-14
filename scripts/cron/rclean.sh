e16d6c8ab9e72a49bc8609e16f06976a91f90003
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-11-14 16:55:45 -0700

    Update rclean.sh

--- scripts/cron/rclean.sh
+++ scripts/cron/rclean.sh
@@ -4,33 +4,40 @@ source /opt/Gooby/menus/variables.sh
 source $CONFIGS/Docker/.env
 
 echo
-echo "${LYELLOW}Taking services down${STD}"
-echo
-
-sudo systemctl daemon-reload
-
-if [ -f /etc/systemd/system/rclone.service ]; then sudo systemctl stop rclone; fi
-
-sudo systemctl stop gooby
-
-echo "Waiting a few seconds for mount to clear"; sleep 20
-
-sudo rmdir ${RCLONEMOUNT} > /dev/null 2>&1
-sudo rmdir ${MOUNTTO} > /dev/null 2>&1
-
-echo
 echo "${LYELLOW}Making sure components are up to date${STD}"
 echo
 
 sudo rm -r /opt/Gooby
-sudo git clone -b rcleantest https://github.com/TechPerplexed/Gooby /opt/Gooby
+sudo git clone -b master https://github.com/TechPerplexed/Gooby /opt/Gooby
 sudo chmod +x -R /opt/Gooby/install
 sudo chmod +x -R /opt/Gooby/menus
 sudo chmod +x -R /opt/Gooby/scripts/cron
 sudo rsync -a /opt/Gooby/install/gooby /bin
 sudo chmod 755 /bin/gooby
 
+echo
+echo "${LYELLOW}${LYELLOW}Taking containers down${STD}"
+echo
+
+cd $CONFIGS/Docker
+/usr/local/bin/docker-compose down
+
 sudo rsync -a /opt/Gooby/scripts/components/{00-AAA.yaml,01-proxy.yaml} $CONFIGS/Docker/components
+
+echo
+echo "${LYELLOW}Taking services down${STD}"
+echo
+
+if [ -f /etc/systemd/system/rclone.service ]; then sudo systemctl stop rclone; fi
+
+sudo systemctl stop gooby
+
+sudo systemctl daemon-reload
+
+echo "Waiting a few seconds for mount to clear"; sleep 20
+
+sudo rmdir ${RCLONEMOUNT} > /dev/null 2>&1
+sudo rmdir ${MOUNTTO} > /dev/null 2>&1
 
 echo
 echo "${LYELLOW}Update Rclone${STD}"
@@ -52,14 +59,7 @@ if [ -f /etc/systemd/system/rclone.service ]; then sudo systemctl start rclone; 
 sudo mkdir -p ${RCLONEMOUNT} ${MOUNTTO}
 sudo chown -R $USER:$USER $RCLONEMOUNT $MOUNTTO
 
-wait 10; sudo systemctl start gooby
-
-echo
-echo "${LYELLOW}${LYELLOW}Taking containers down${STD}"
-echo
-
-cd $CONFIGS/Docker
-/usr/local/bin/docker-compose down
+sleep 10; sudo systemctl start gooby
 
 echo
 echo "${LYELLOW}Checking for updated containers${STD}"
