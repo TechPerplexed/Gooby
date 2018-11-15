381b858676038c5cfbbdecc45e1b3d292192dd54
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-11-14 17:01:37 -0700

    Update rclean.sh

--- scripts/cron/rclean.sh
+++ scripts/cron/rclean.sh
@@ -36,8 +36,10 @@ sudo systemctl daemon-reload
 
 echo "Waiting a few seconds for mount to clear"; sleep 20
 
-sudo rmdir ${RCLONEMOUNT} > /dev/null 2>&1
-sudo rmdir ${MOUNTTO} > /dev/null 2>&1
+sudo rmdir ${RCLONEMOUNT}
+sudo rmdir ${MOUNTTO}
+
+ls /tmp
 
 echo
 echo "${LYELLOW}Update Rclone${STD}"
