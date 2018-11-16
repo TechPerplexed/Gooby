6bf602a6f6a601e4c7c33e6f6ad95e98a3288ad6
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-11-16 09:30:30 -0700

    Update update-app.sh

--- install/apps/update-app.sh
+++ install/apps/update-app.sh
@@ -18,7 +18,8 @@ else
 		GOAHEAD
 
 		cd $CONFIGS/Docker
-		/usr/local/bin/docker-compose up -d --no-deps --build $APP
+		/usr/local/bin/docker-compose pull
+		/usr/local/bin/docker-compose up --remove-orphans --build -d $APP
 		cd ${CURDIR}
 
 		TASKCOMPLETE
