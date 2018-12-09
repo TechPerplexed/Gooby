d357fe7cb729cb422864332bd452fe12a842eedc
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-12-09 15:22:13 -0700

    Update docker-install.sh

--- install/server/docker-install.sh
+++ install/server/docker-install.sh
@@ -10,7 +10,7 @@ sudo apt-get -y install \
 	gpg-agent \
 	software-properties-common
 
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository \
 	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -21,16 +21,32 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce
 
-sudo usermod -a -G docker $USER
-sudo gpasswd -a $USER docker
-sudo setfacl -m user:$USER:rw /var/run/docker.sock
+# Alternatively you can use the official docker install script
 
-# latest docker compose released tag
-
-COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+# which docker > $TCONFIGS/checkapp
+# if [ ! -s $TCONFIGS/checkapp ]; then
+#	sudo true
+#	wget -qO- https://get.docker.com/ | sh
+# fi
+# rm $TCONFIGS/checkapp
 
 # Install docker-compose
 
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
 sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
 sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
+# Install docker-cleanup command
+
+cd /tmp
+git clone https://gist.github.com/76b450a0c986e576e98b.git
+cd 76b450a0c986e576e98b
+sudo mv docker-cleanup /usr/local/bin/docker-cleanup
+sudo chmod +x /usr/local/bin/docker-cleanup
+
+# Give proper privileges
+
+sudo usermod -a -G docker $USER
+sudo gpasswd -a $USER docker
+sudo setfacl -m user:$USER:rw /var/run/docker.sock
