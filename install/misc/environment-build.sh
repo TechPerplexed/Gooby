8ca53c41f9a13d60a27918f5243e9f26305b6011
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2018-12-09 15:04:13 -0700

    Update environment-build.sh

--- install/misc/environment-build.sh
+++ install/misc/environment-build.sh
@@ -5,63 +5,53 @@ source /opt/Gooby/menus/variables.sh
 ENV=$CONFIGS/Docker/.env
 touch ${ENV}
 
+# Since Docker processes " as part of the paths, we don't actually store it in the variables.  However, BASH does require
+#	the " if the string is going to have odd characters.  As a result, we have to load the existing environment line
+#	by line and insert the " around the values.
+
 # Load existing variables and use them as defaults, if available
 
-	while IFS='' read -r line || [[ -n "${line}" ]]; do
-		newline=$(echo ${line} | sed 's/=/&\"/g; s/:$//')\"
-		eval ${newline} > /dev/null 2>&1
-	done < ${ENV}
+while IFS='' read -r line || [[ -n "${line}" ]]; do
+	newline=$(echo ${line} | sed 's/=/&\"/g; s/:$//')\"
+	eval ${newline} > /dev/null 2>&1
+done < ${ENV}
 
 if [[ ${1,,} != "rebuild" ]]; then
-
-	echo Checking system dependencies... This might be a minute.
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - > /dev/null 2>&1
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /dev/null 2>&1
-	apt-get update > /dev/null 2>&1
-	sudo apt-get install -y docker-ce dialog > /dev/null 2>&1
-	sudo usermod -aG docker ${USER} > /dev/null 2>&1
-	sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose > /dev/null 2>&1
-	sudo chmod +x /usr/local/bin/docker-compose
 	echo "Creating configuration file..."
-
 fi
 
-echo "###########################" > ${ENV}
-echo "#  Environment variables  #" >> ${ENV}
-echo "#     for Gooby v$VERSION    #" >> ${ENV}
-echo "###########################" >> ${ENV}
-echo >> ${ENV}
-echo "CONFIGS=${CONFIGS}" >> ${ENV}
-echo "TCONFIGS=${TCONFIGS}" >> ${ENV}
-echo "HOMEDIR=$HOME" >> ${ENV}
-echo "LOGS=$HOME/logs" >> ${ENV}
-echo "USERID=$(id -u)" >> ${ENV}
-echo "GROUPID=$(id -g)" >> ${ENV}
-echo "USERNAME=$USER" >> ${ENV}
-echo "GROUPNAME=$USER" >> ${ENV}
-echo "IP=$(curl ifconfig.me)" >> ${ENV}
-echo "TIMEZONE=$(cat /etc/timezone)" >> ${ENV}
-echo "MYDOMAIN=$(cat ${CONFIGS}/.config/mydomain)" >> ${ENV}
-echo "MYEMAIL=$(cat ${CONFIGS}/.config/myemail)" >> ${ENV}
-echo "ORGMENU=menu" >> ${ENV}
-echo "RCLONESERVICE=$(cat ${CONFIGS}/.config/rcloneservice)" >> ${ENV}
-echo "RCLONEFOLDER=$(cat ${CONFIGS}/.config/rclonefolder)" >> ${ENV}
-echo "RCLONEMOUNT=/mnt/rclone" >> ${ENV}
-echo "MOUNTTO=/mnt/google" >> ${ENV}
-echo "UPLOADS=/mnt/uploads" >> ${ENV}
-echo "GOOGLE=/mnt/google" >> ${ENV}
-echo "MEDIA=/mnt/google" >> ${ENV}
-echo "MOVIES=/mnt/google/Movies" >> ${ENV}
-echo "TV=/mnt/google/TV" >> ${ENV}
-echo "DOWNLOADS=${HOMEDIR}/Downloads" >> ${ENV}
-[[ -f "${TCONFIGS}/plexclaim" ]] && echo "PLEXCLAIM=$(cat ${TCONFIGS}/plexclaim)" >> ${ENV}
+cat > ${ENV} << EOF
+
+###########################
+#  Environment variables  #
+#     for Gooby v$VERSION    #
+###########################
+
+CONFIGS=${CONFIGS}
+DOWNLOADS=${HOMEDIR}/Downloads
+GOOGLE=/mnt/google
+GROUPID=$(id -g)
+GROUPNAME=$USER
+HOMEDIR=${HOME}
+IP=$(curl ifconfig.me)
+LOGS=${HOME}/logs
+MEDIA=/mnt/google
+ORGMENU=menu
+MOUNTTO=/mnt/google
+MYDOMAIN=$(cat ${CONFIGS}/.config/mydomain)
+MYEMAIL=$(cat ${CONFIGS}/.config/myemail)
+PLEXCLAIM=$(cat ${TCONFIGS}/plexclaim)
+RCLONEFOLDER=$(cat ${CONFIGS}/.config/rclonefolder)
+RCLONEHOME=${HOME}/.config/rclone
+RCLONESERVICE=$(cat ${CONFIGS}/.config/rcloneservice)
+RCLONEMOUNT=/mnt/rclone
+TCONFIGS=${TCONFIGS}
+TIMEZONE=$(cat /etc/timezone)
+UNSYNCED=/mnt/local
+UPLOADS=/mnt/uploads
+USERID=$(id -u)
+USERNAME=$USER
+EOF
 
 cat ${CONFIGS}/Docker/components/??-* > ${CONFIGS}/Docker/docker-compose.yaml
 echo done
-
-if [ -e /etc/systemd/system/rclone.service ]; then
-	sudo sed -i "s/media/mnt/g" /etc/systemd/system/rclone.service
-	sudo sed -i "s/Google/google/g" /etc/systemd/system/rclone.service
-	sudo systemctl daemon-reload
-	sudo systemctl restart rclone.service	
-fi
