f8f894b12cd53cf45058d761654e3d673b2beb6c
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-01-14 13:32:52 -0700

    Update docker-install.sh

--- install/server/docker-install.sh
+++ install/server/docker-install.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
 
-# install docker
+VERSION=$(lsb_release -si)
+echo Distribution is ${VERSION}
 
-sudo apt-get -y install \
-	apt-transport-https \
-	acl \
-	ca-certificates \
-	curl \
-	gpg-agent \
-	software-properties-common
+sudo apt update > /dev/null 2>&1
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common python3-pip acl > /dev/null 2>&1
 
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/${VERSION,,}/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/${VERSION,,} $(lsb_release -cs) stable"
 
-sudo add-apt-repository \
-	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-	$(lsb_release -cs) \
-	stable"
-
-sudo apt-get update
-
-sudo apt-get install -y docker-ce
-
-sudo usermod -a -G docker $USER
-sudo gpasswd -a $USER docker
-sudo setfacl -m user:$USER:rw /var/run/docker.sock
-
-# latest docker compose released tag
-
-COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+sudo apt update > /dev/null 2>&1
+apt-cache policy docker-ce
+sudo apt install -y docker-ce > /dev/null 2>&1
 
 # Install docker-compose
 
-sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-sudo chmod +x /usr/local/bin/docker-compose
-sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+sudo -H pip3 install --upgrade pip
+sudo -H  pip install --upgrade docker-compose
+
+sudo usermod -aG docker ${USER}
+sudo gpasswd -a ${USER} docker 
+sudo setfacl -m user:${USER}:rw /var/run/docker.sock
+
+clear
+
+docker version
+echo
+docker-compose version
