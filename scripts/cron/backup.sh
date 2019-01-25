79e51ec677b3b20112943961af75f033586d2aa7
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-01-25 11:43:25 -0700

    Update backup.sh

--- scripts/cron/backup.sh
+++ scripts/cron/backup.sh
@@ -19,5 +19,11 @@ echo
 echo "${GREEN}Copying to Google...${STD}"
 echo
 
+APILOG=$LOGS/api.log
+
+BYTES=$(find /tmp -name '${BACKUP}' -exec du -bc {} + | grep total$ | cut -f1 | awk '{ total += $1 }; END { print total }')
+echo $(date '+%F %H:%M:%S'),START,1,${BYTES} >> ${APILOG}
 /usr/bin/rclone copy ${BACKUP} $RCLONESERVICE:/Backup/$(hostname) --checksum --drive-chunk-size=64M
+echo $(date '+%F %H:%M:%S'),STOP,1,${BYTES} >> ${APILOG}
+
 sudo rm ${BACKUP}
