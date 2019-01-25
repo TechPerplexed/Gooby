d6f0c4e43e20b2cbb7e8bdcb91a28eeb396a7b7a
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-01-25 14:57:55 -0700

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -17,6 +17,7 @@ sudo git clone -b master https://github.com/TechPerplexed/Gooby /opt/Gooby
 
 sudo chmod +x -R /opt/Gooby/install
 sudo chmod +x -R /opt/Gooby/menus
+sudo chmod +x -R /opt/Gooby/scripts/bin
 sudo chmod +x -R /opt/Gooby/scripts/cron
 sudo rsync -a /opt/Gooby/scripts/bin/gooby /bin
 sudo chmod 755 /bin/gooby
