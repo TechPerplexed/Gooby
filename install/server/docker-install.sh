f63d3bfeb850fa130d475f819f69610549fb5892
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-01-26 15:27:15 -0700

    Update docker-install.sh

--- install/server/docker-install.sh
+++ install/server/docker-install.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-# install docker
+VERSION=$(lsb_release -si)
+echo Distribution is ${VERSION}
 
-sudo apt-get -y install \
-	apt-transport-https \
-	acl \
-	ca-certificates \
-	curl \
-	gpg-agent \
-	software-properties-common
+sudo apt -y update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common python3-pip acl
 
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/${VERSION,,}/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/${VERSION,,} $(lsb_release -cs) stable"
 
-sudo add-apt-repository \
-	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-	$(lsb_release -cs) \
-	stable"
-
-sudo apt-get update
-
-sudo apt-get install -y docker-ce
-
-sudo usermod -a -G docker $USER
-sudo gpasswd -a $USER docker
-sudo setfacl -m user:$USER:rw /var/run/docker.sock
+sudo apt -y update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
 
 # latest docker compose released tag
 
@@ -34,3 +22,13 @@ COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/l
 sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
 sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+
+sudo usermod -aG docker ${USER}
+sudo gpasswd -a ${USER} docker 
+sudo setfacl -m user:${USER}:rw /var/run/docker.sock
+
+clear
+
+docker version
+echo
+docker-compose version
