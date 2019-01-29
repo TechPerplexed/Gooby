c0f0f06b516b8bcd2b58f5fd136698d96346429a
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-01-28 18:44:47 -0700

    Update Gooby-update.sh

--- install/misc/Gooby-update.sh
+++ install/misc/Gooby-update.sh
@@ -8,15 +8,19 @@ CONFIRMATION
 
 if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
-	sudo rm -r /opt/Gooby
-	sudo git clone -b master https://github.com/TechPerplexed/Gooby /opt/Gooby
+	sudo rm -r /opt/.Gooby > /dev/null 2>&1
+	sudo git clone -b master https://github.com/TechPerplexed/Gooby /opt/.Gooby
 
-	sudo chmod +x -R /opt/Gooby/install
-	sudo chmod +x -R /opt/Gooby/menus
-	sudo chmod +x -R /opt/Gooby/scripts/bin
-	sudo chmod +x -R /opt/Gooby/scripts/cron
-	sudo rsync -a /opt/Gooby/scripts/bin/gooby /bin
-	sudo chmod 755 /bin/gooby
+	if [ -d /opt/.Gooby ]; then
+		sudo rm -r /opt/Gooby
+		sudo mv /opt/.Gooby /opt/Gooby
+		sudo chmod +x -R /opt/Gooby/install
+		sudo chmod +x -R /opt/Gooby/menus
+		sudo chmod +x -R /opt/Gooby/scripts/bin
+		sudo chmod +x -R /opt/Gooby/scripts/cron
+		sudo rsync -a /opt/Gooby/scripts/bin/gooby /bin
+		sudo chmod 755 /bin/gooby
+	fi
 
 	clear
 
