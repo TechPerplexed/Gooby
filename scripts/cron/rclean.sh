631a0024b089d6704986bb61839ac47681d453a3
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-01-28 18:44:05 -0700

    Update rclean.sh

--- scripts/cron/rclean.sh
+++ scripts/cron/rclean.sh
@@ -17,10 +17,12 @@ sudo git clone -b master https://github.com/TechPerplexed/Gooby /opt/.Gooby
 if [ -d /opt/.Gooby ]; then
 	sudo rm -r /opt/Gooby
 	sudo mv /opt/.Gooby /opt/Gooby
+
 	sudo chmod +x -R /opt/Gooby/install
 	sudo chmod +x -R /opt/Gooby/menus
+	sudo chmod +x -R /opt/Gooby/scripts/bin
 	sudo chmod +x -R /opt/Gooby/scripts/cron
-	sudo rsync -a /opt/Gooby/install/gooby /bin
+	sudo rsync -a /opt/Gooby/scripts/bin/gooby /bin
 	sudo chmod 755 /bin/gooby
 fi
 
