ea39f653ea1bdd8834f2a7a527cda37085c3ed49
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-02-08 13:21:12 -0700

    Update backup-create.sh

--- install/misc/backup-create.sh
+++ install/misc/backup-create.sh
@@ -17,27 +17,27 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 		echo
 		echo "You currently have no backup cron scheduled."
 		echo
-		read -n 1 -s -r -p " Would you like to schedule a weekly backup (y/N)? "
+		read -n 1 -s -r -p "Would you like to schedule a weekly backup (y/N)? "
 		echo
 
 		if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 			crontab -l | grep 'backup.sh' || (crontab -l 2>/dev/null; echo "15 2 * * SUN /opt/Gooby/scripts/cron/backup.sh > /dev/null 2>&1") | crontab -
 			echo
-			echo " Backup scheduled to run at 02:15 every Sunday"
-			echo " You can always change this by typing ${LYELLOW}crontab -e${STD}"
+			echo "Backup scheduled to run at 02:15 every Sunday"
+			echo "You can always change this by typing ${LYELLOW}crontab -e${STD}"
 
 		else
 
-			echo " No worries, you can always add a backup schedule later!"
+			echo "No worries, you can always add a backup schedule later!"
 
 		fi
 
 	fi
 
 	echo
-	echo " The backup can take several hours"
-	echo " Please don't exit the terminal until it's done!"
+	echo "The backup can take several hours"
+	echo "Please don't exit the terminal until it's done!"
 	echo
 
 	source /opt/Gooby/scripts/cron/backup.sh
