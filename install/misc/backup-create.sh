701ddd80133eedb9a3be18f970e554889040f687
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-02-08 13:03:46 -0700

    Update backup-create.sh

--- install/misc/backup-create.sh
+++ install/misc/backup-create.sh
@@ -10,7 +10,7 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 	GOAHEAD
 
-	if [ ! -e $TCONFIGS/cronbackup ]; then
+	if ! crontab -l | grep 'backup.sh'; then
 
 		echo
 		read -n 1 -s -r -p " Would you like to schedule a weekly backup (y/N)? "
@@ -18,8 +18,8 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 		if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
-			(crontab -l 2>/dev/null; echo "15 2 * * SUN /opt/Gooby/scripts/cron/backup.sh > /dev/null 2>&1") | crontab -
-			touch $TCONFIGS/cronbackup
+			crontab -l | grep 'backup.sh' || (crontab -l 2>/dev/null; echo "15 2 * * SUN /opt/Gooby/scripts/cron/backup.sh > /dev/null 2>&1") | crontab -
+			echo
 			echo " Backup scheduled to run at 02:15 every Sunday"
 			echo " You can always change this by typing ${LYELLOW}crontab -e${STD}"
 			echo
