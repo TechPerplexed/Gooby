7df3cc340c559a7f5d56ba5ea539898684fa4aec
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-02-09 21:13:50 -0700

    Update system-cleanup.sh

--- install/misc/system-cleanup.sh
+++ install/misc/system-cleanup.sh
@@ -12,13 +12,6 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 	echo "${GREEN}Giving your system a nice little spring cleaning...${STD}"
 	echo
 
-	# Add rlean to bootup cron if not added yet
-
-	if [ ! -f $TCONFIGS/cronboot ]; then
-		(crontab -l 2>/dev/null; echo "@reboot /opt/Gooby/scripts/cron/rclean.sh > /dev/null 2>&1") | crontab -
-		touch $TCONFIGS/cronboot
-	fi
-
 	source /opt/Gooby/scripts/cron/rclean.sh
 
 	clear
