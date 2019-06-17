b6846f834d154c88a6397ab82c16683296c6aed2
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-06-17 16:41:58 -0600

    Update upgradeservice.sh

--- scripts/cron/upgradeservice.sh
+++ scripts/cron/upgradeservice.sh
@@ -1,1 +1,9 @@
-# Coming soon...
+# Moving all Proxy folders to Docker
+
+if [ -d $CONFIGS/Security ]; then
+	sudo mv /$CONFIGS/Certs $CONFIGS/Docker
+	sudo mv $CONFIGS/Docker/Certs $CONFIGS/Docker/certs
+	sudo mv /$CONFIGS/nginx $CONFIGS/Docker
+	sudo mv /$CONFIGS/Security $CONFIGS/Docker	
+	sudo mv $CONFIGS/Docker/Security $CONFIGS/Docker/security
+fi
