a9b9972b04058643a34d1e4efc55977363d768d8
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-06-21 17:13:03 -0600

    Update upgradeservice.sh

--- scripts/cron/upgradeservice.sh
+++ scripts/cron/upgradeservice.sh
@@ -1,3 +1,16 @@
+# Check if necessary apps are installed
+
+sudo apt-get update
+
+APPLIST="acl apt-transport-https ca-certificates curl fail2ban fuse git gpg-agent grsync jq mergerfs nano rsyncufw socat unzip wget"
+
+for i in $APPLIST; do
+	echo Installing $i...
+	echo
+	sudo apt-get -y install $i
+	echo; sleep 2
+done
+
 # Moving all Proxy folders to Docker
 
 if [ -d $CONFIGS/Security ]; then
@@ -7,3 +20,21 @@ if [ -d $CONFIGS/Security ]; then
 	sudo mv /$CONFIGS/Security $CONFIGS/Docker	
 	sudo mv $CONFIGS/Docker/Security $CONFIGS/Docker/security
 fi
+
+# Upgrade Rclone service 
+
+cat /etc/systemd/system/rclone.service | grep "RCLONEUSERNAME" > /dev/null
+if ! [[ ${?} -eq 0 ]]; then
+	sudo mv /etc/systemd/system/rclone* /tmp
+	sudo rsync -a /opt/Gooby/scripts/services/rclonefs* /etc/systemd/system/
+	sudo sed -i "s/GOOBYUSER/${USER}/g" /etc/systemd/system/rclonefs.service
+fi
+
+cat $HOME/.config/rclone/rclone.conf | grep "[Local]" > /dev/null
+if ! [[ ${?} -eq 0 ]]; then
+	echo [Local] >> $HOME/.config/rclone/rclone.conf
+	echo type = local >> $HOME/.config/rclone/rclone.conf
+	echo nounc = >> $HOME/.config/rclone/rclone.conf
+fi
+
+sudo systemctl daemon-reload
