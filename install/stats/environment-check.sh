1c5989a9d6602ef25a2cc850c52c1288a61a25ef
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-06-21 19:31:40 -0600

    Update environment-check.sh

--- install/stats/environment-check.sh
+++ install/stats/environment-check.sh
@@ -19,14 +19,14 @@ show_menus() {
 	clear
 	echo " ${LBLUE}"
 	MENUSTART
-	echo -n " OS version: ${LBLUE}" ; lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 || uname -om ; echo -n "${STD}" ; echo -n "${STD}"
-	echo " Timezone: ${LBLUE}$( cat /etc/timezone )${STD}"
-	echo " Your Domain: ${LBLUE}$MYDOMAIN${STD}"
-	echo " Your Email Address: ${LBLUE}$MYEMAIL${STD}"
-	echo " Your Server IP Address: ${LBLUE}$IP${STD}"
-	echo " In app media location: ${LBLUE}/Media${STD}"
-	echo " In app download location: ${LBLUE}/Media/Downloads${STD}"
-	echo " Gooby version: ${LBLUE}$VERSION${STD}"
+	echo -n " OS version        : ${LBLUE}" ; lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 || uname -om ; echo -n "${STD}" ; echo -n "${STD}"
+	echo " Timezone          : ${LBLUE}$( cat /etc/timezone )${STD}"
+	echo " Domain            : ${LBLUE}$MYDOMAIN${STD}"
+	echo " Email Address     : ${LBLUE}$MYEMAIL${STD}"
+	echo " IP Address        : ${LBLUE}$IP${STD}"
+	echo " Media location    : ${LBLUE}/Media${STD}"
+	echo " Download location : ${LBLUE}/Media/Downloads${STD}"
+	echo " Gooby version     : ${LBLUE}$VERSION${STD}"
 	echo
 	echo " ${WHITE}Z${STD} - EXIT to Main Menu"
 	echo " ${LBLUE}"
