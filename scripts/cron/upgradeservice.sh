5fd0bb29751429f20636e95cded8ea07417ae08b
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-06-21 19:00:52 -0600

    Update upgradeservice.sh

--- scripts/cron/upgradeservice.sh
+++ scripts/cron/upgradeservice.sh
@@ -17,7 +17,7 @@ if [ -d $CONFIGS/Security ]; then
 	sudo mv /$CONFIGS/Certs $CONFIGS/Docker
 	sudo mv $CONFIGS/Docker/Certs $CONFIGS/Docker/certs
 	sudo mv /$CONFIGS/nginx $CONFIGS/Docker
-	sudo mv /$CONFIGS/Security $CONFIGS/Docker	
+	sudo mv /$CONFIGS/Security $CONFIGS/Docker
 	sudo mv $CONFIGS/Docker/Security $CONFIGS/Docker/security
 fi
 
@@ -37,4 +37,21 @@ if ! [[ ${?} -eq 0 ]]; then
 	echo nounc = >> $HOME/.config/rclone/rclone.conf
 fi
 
+# Upgrade Rclone service 
+
+cat /etc/systemd/system/rclone.service | grep "RCLONEUSERNAME" > /dev/null
+if ! [[ ${?} -eq 0 ]]; then
+	sudo mv /etc/systemd/system/rclone* /tmp
+	sudo rsync -a /opt/Gooby/scripts/services/rclonefs* /etc/systemd/system/
+	sudo sed -i "s/GOOBYUSER/${USER}/g" /etc/systemd/system/rclonefs.service
+fi
+
+cat $HOME/.config/rclone/rclone.conf | grep "[Local]" > /dev/null
+if ! [[ ${?} -eq 0 ]]; then
+	echo [Local] >> $HOME/.config/rclone/rclone.conf
+	echo type = local >> $HOME/.config/rclone/rclone.conf
+	echo nounc = >> $HOME/.config/rclone/rclone.conf
+fi
+
 sudo systemctl daemon-reload
+
