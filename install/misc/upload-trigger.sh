0a0b5e0b79a40595dad8cc2a8e3ed0ba5220081b
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-06-30 10:40:12 -0600

    Update upload-trigger.sh

--- install/misc/upload-trigger.sh
+++ install/misc/upload-trigger.sh
@@ -9,7 +9,8 @@ CONFIRMATION
 if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 	echo
-	echo "${GREEN}This will manually trigger your upload process (runs every 15 minutes regardless)${STD}"
+	echo "${GREEN}This will manually trigger your uploads."
+	echo "This process runs every 15 minutes regardless!${STD}"
 	echo
 
 	source /opt/Gooby/scripts/cron/syncmount.sh
