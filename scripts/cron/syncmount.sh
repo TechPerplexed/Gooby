2bf5b97879ca89fd0592e46fc07b7b12e46e1310
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-07-04 17:06:37 -0600

    Update syncmount.sh

--- scripts/cron/syncmount.sh
+++ scripts/cron/syncmount.sh
@@ -42,11 +42,11 @@ then
 			echo $(date '+%F %H:%M:%S'),START,1,${BYTES} "# ${FILE}" >> ${APILOG}
 			echo Queuing ${FILE} of size ${BYTESH}
 	                ## Fix for Rclone RC creating multiple directories
-			if [[ ! -d "${RCLONEMOUNT}${RCLONEFOLDER}$(dirname ${FILE})" ]]; then
-				mkdir -p "${RCLONEMOUNT}${RCLONEFOLDER}$(dirname ${FILE})"
-			fi
+			# if [[ ! -d "${RCLONEMOUNT}${RCLONEFOLDER}$(dirname ${FILE})" ]]; then
+			#	mkdir -p "${RCLONEMOUNT}${RCLONEFOLDER}$(dirname ${FILE})"
+			# fi
 			rclone rc operations/movefile _async=true srcFs=Local: srcRemote="${UPLOADS}${FILE}" dstFs=${RCLONESERVICE}:${RCLONEFOLDER} dstRemote="${FILE}" --user $RCLONEUSERNAME --pass $RCLONEPASSWORD > /dev/null
-			# echo "Sleeping 1 second - temp fix for duplicate folders" ; sleep 1
+			echo "Sleeping 1 second - temp fix for duplicate folders" ; sleep 1
 		else
 			echo Skipping ${FILE}:  Already in queue
 		fi
