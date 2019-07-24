88e88f94e1feb0895a4ca6446e31a6bfc8915fe9
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-07-24 15:53:31 -0700

    Update backup.sh

--- scripts/cron/backup.sh
+++ scripts/cron/backup.sh
@@ -6,21 +6,72 @@ fi
 
 source /opt/Gooby/menus/variables.sh
 source $CONFIGS/Docker/.env
-BACKUP=$(date +%F).tar.gz
 
-echo
-echo "${LMAGENTA}Creating backup file...${STD}"
-echo
+SERVER=$(hostname)
+APILOG="${LOGS}/api.log"
 
-sudo tar -cf /tmp/${BACKUP} $CONFIGS --exclude-caches-all
-sudo chown $USER:$USER /tmp/${BACKUP}
+# Make sure there's a place to store backup index for full versus differential
+mkdir -p ${HOME}/backup/snapshots
+SNAPSHOTS="${HOME}/backup/snapshots"
 
-echo
-echo "${GREEN}Copying to Google...${STD}"
-echo
+rmove() {
+	SOURCE=${1}
+	TARGET=${2}
+	SOURCEDIR=$(dirname "${SOURCE}")
+	SOURCEFILE=$(basename "${SOURCE}")
+	BYTES=$(stat --printf="%s" "${SOURCE}")
+	echo "${TARGET} @ ${BYTES} Bytes"
+	echo $(date '+%F %H:%M:%S'),START,1,${BYTES} "# ${SOURCE}" >> ${APILOG}
+	/usr/bin/rclone rc operations/movefile _async=true srcFs=Local: srcRemote=${SOURCE} dstFs=${RCLONESERVICE}: dstRemote=${TARGET} --user ${RCLONEUSERNAME} --pass ${RCLONEPASSWORD} > /dev/null
+}
 
-APILOG=$LOGS/api.log
+cd ${HOME}
+echo Creating backup
+sudo crontab -u ${USER} -l > /home/${USER}/backup/cron
+echo -n "/tmp/${SERVER}-backup.tar.gz --> "
+sudo tar -cpf /tmp/${SERVER}-backup.tar.gz \
+	--exclude=/home/${USER}/Downloads \
+	/home/${USER} > /dev/null 2>&1
+sudo chown ${USERID}:${GROUPID} /tmp/${SERVER}-*.tar.gz
+rmove "/tmp/${SERVER}-backup.tar.gz" "/Backup/${SERVER}/${SERVER}-backup.tar.gz"
 
-BYTES=$(find /tmp -name '${BACKUP}' -exec du -bc {} + | grep total$ | cut -f1 | awk '{ total += $1 }; END { print total }')
-echo $(date '+%F %H:%M:%S'),START,1,${BYTES} >> ${APILOG}
-/usr/bin/rclone rc operations/movefile _async=true srcFs=Local: srcRemote=/tmp/${BACKUP} dstFs=$RCLONESERVICE: dstRemote=/Backup/$(hostname)/${BACKUP} --user $RCLONEUSERNAME --pass $RCLONEPASSWORD > /dev/null
+cd ${CONFIGS}
+
+FILES=${CONFIGS}/*
+for f in ${FILES}
+do
+	FILENAME="$(basename "${f}")"
+	echo -n "${f} --> "
+	if [[ ${f: -3} == 'tar' ]] || [[ ${f: -3} == ".gz" ]] || [[ ${f: -3} == "bz2" ]]
+	then
+		cp "${f}" /tmp/
+		rmove "/tmp/${FILENAME}" "/Backup/${SERVER}/Gooby/${FILENAME}"
+	else
+		if [[ -f "${SNAPSHOTS}/${FILENAME}.snar" ]]
+		then
+			# We already have a snar, which means that this is a differential
+			FULL=0
+			cp "${SNAPSHOTS}/${FILENAME}.snar" "${SNAPSHOTS}/${FILENAME}.bak"
+			FILENAME2="${FILENAME}-diff"
+		else
+			# No snar found, so we're doing the full backup
+			FULL=1
+			FILENAME2="${FILENAME}-full"
+		fi
+		sudo tar --listed-incremental "${SNAPSHOTS}/${FILENAME}.snar" \
+			-cpf "/tmp/${FILENAME2}.tar.gz" \
+			--exclude=.cache \
+			"${FILENAME}" > /dev/null 2>&1
+		sudo chown ${USERID}:${GROUPID} "/tmp/${FILENAME2}.tar.gz" "${SNAPSHOTS}/${FILENAME}.snar"
+		if [[ ${FULL} -eq 0 ]]
+		then
+			# If this is a differential, we want to remember the original so that
+			# Future differential backups are based on the original, not the previous
+			# Differential
+			mv "${SNAPSHOTS}/${FILENAME}.bak" "${SNAPSHOTS}/${FILENAME}.snar"
+		fi
+		rmove "/tmp/${FILENAME2}.tar.gz" "/Backup/${SERVER}/Gooby/${FILENAME2}.tar.gz"
+		cp "${SNAPSHOTS}/${FILENAME}.snar" /tmp/
+		rmove "/tmp/${FILENAME}.snar" "/Backup/${SERVER}/Gooby/snapshots/${FILENAME}.snar"
+	fi
+done
