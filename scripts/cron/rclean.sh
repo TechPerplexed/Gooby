5cd43abeb0840ac40186d0258a8d9e9af8187de6
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-08-08 17:16:06 -0700

    Update rclean.sh

--- scripts/cron/rclean.sh
+++ scripts/cron/rclean.sh
@@ -23,6 +23,7 @@ if [ -d /opt/.Gooby ]; then
 	sudo chmod +x -R /opt/Gooby/scripts/cron
 	sudo rsync -a /opt/Gooby/scripts/bin/* /bin
 	sudo chmod 755 /bin/gooby
+	sudo chmod 755 /bin/gbackup
 	sudo chmod 755 /bin/plexstats
 	sudo chmod 755 /bin/rclean
 	sudo chmod 755 /bin/rstats
