a6a1f289d43d316b18e1468d4994dbac91678876
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-11-06 12:25:17 -0700

    Update and rename password-change.sh to proxy-change.sh

---
+++ install/server/proxy-change.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+clear
+
+EXPLAINTASK
+
+CONFIRMATION
+
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+
+	GOAHEAD
+    
+		clear
+
+		echo
+		Coming soon! Stay tuned...
+		echo
+
+	TASKCOMPLETE
+
+else
+
+	CANCELTHIS
+
+fi
+
+PAUSE
