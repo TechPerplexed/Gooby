880a5bd4371b628a3919d8006efcd72c23c5e581
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-11-06 09:03:39 -0700

    Update backup-create.sh

--- install/misc/backup-create.sh
+++ install/misc/backup-create.sh
@@ -29,14 +29,14 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 		fi
 	fi
 
-	echo "Current backup schedule:"
+	echo " Current backup schedule:"
 
 	if ! crontab -l | grep 'backup.sh'; then
 
 		echo
-		echo "You currently have no backup cron scheduled."
+		echo "Y ou currently have no backup cron scheduled."
 		echo
-		read -n 1 -s -r -p "Would you like to schedule a weekly backup (y/N)? "
+		read -n 1 -s -r -p " Would you like to schedule a weekly backup (y/N)? "
 		echo
 
 		if [[ ${REPLY} =~ ^[Yy]$ ]]; then
@@ -44,22 +44,23 @@ if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 			crontab -l | grep 'backup.sh' || (crontab -l 2>/dev/null; echo "15 2 * * SUN /opt/Gooby/scripts/cron/backup.sh > /dev/null 2>&1") | crontab -
 			crontab -l | grep 'resetbackup' || (crontab -l 2>/dev/null; echo "10 2 1 * * /bin/resetbackup > /dev/null 2>&1") | crontab -
 			echo
-			echo "Backup scheduled to run at 02:15 every Sunday."
-			echo "The incremental backup will reset on the first of each month."
+			echo " Backup scheduled to run at 02:15 every Sunday."
+			echo " The incremental backup will reset on the first of each month."
 			echo
-			echo "You can always change these settings by typing ${LYELLOW}crontab -e${STD}"
+			echo " You can always change these settings by typing ${LYELLOW}crontab -e${STD}"
+			wait 10
 
 		else
 
-			echo "No worries, you can always add a backup schedule later!"
+			echo " No worries, you can always add a backup schedule later!"; wait 10
 
 		fi
 
 	fi
 
 	echo
-	echo "The backup can take several hours"
-	echo "Please don't exit the terminal until it's done!"
+	echo " The backup can take several hours"
+	echo " Please don't exit the terminal until it's done!"
 	echo
 
 	source /opt/Gooby/scripts/cron/backup.sh
