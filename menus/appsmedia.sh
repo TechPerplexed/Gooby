3a3e53a1f8c16b14f4e6a94d197d43d6a71a3fef
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-11-07 15:28:28 -0700

    Update appsmedia.sh

--- menus/appsmedia.sh
+++ menus/appsmedia.sh
@@ -69,7 +69,7 @@ read_options(){
 		[Cc]) TAUTULLI ;;
 		[Dd]) EMBY ;;      
 		[Zz]) QUIT ;;
-		*) echo "${LRED}Please select a valid option${STD}" && sleep 2
+		*) echo "${LRED}Please select a valid option${STD}" && sleep 1
 	esac
 }
 
