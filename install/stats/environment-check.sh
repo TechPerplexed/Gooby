4647ffe2cc1df7e5bf5d0204a4298af47211cff2
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-11-07 15:16:24 -0700

    Update environment-check.sh

--- install/stats/environment-check.sh
+++ install/stats/environment-check.sh
@@ -20,14 +20,15 @@ show_menus() {
 	echo " ${LBLUE}"
 	MENUSTART
 	echo -n " OS version        : ${LBLUE}" ; lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 || uname -om ; echo -n "${STD}" ; echo -n "${STD}"
-	echo " Timezone          : ${LBLUE}$( cat /etc/timezone )${STD}"
+	echo " Timezone          : ${LBLUE}${TIMEZONE}${STD}"
 	echo " Domain            : ${LBLUE}${MYDOMAIN}${STD}"
 	echo " Email Address     : ${LBLUE}${MYEMAIL}${STD}"
 	echo " IP Address        : ${LBLUE}${IP}${STD}"
 	echo " Proxy version     : ${LBLUE}${PROXYVERSION^^}${STD}"
+	echo " Gooby version     : ${LBLUE}${VERSION}${STD}"
+	echo
 	echo " Media location    : ${LBLUE}/Media${STD}"
 	echo " Download location : ${LBLUE}/Media/Downloads${STD}"
-	echo " Gooby version     : ${LBLUE}${VERSION}${STD}"
 	echo
 	echo " ${WHITE}Z${STD} - EXIT to Main Menu"
 	echo " ${LBLUE}"
