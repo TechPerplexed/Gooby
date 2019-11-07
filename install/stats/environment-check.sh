00d692b3181ac1b11cfb96f624e8b5d18b6e970c
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-11-07 15:12:07 -0700

    Update environment-check.sh

--- install/stats/environment-check.sh
+++ install/stats/environment-check.sh
@@ -2,7 +2,7 @@
 
 MENU="Check Environment"
 
-source $CONFIGS/Docker/.env
+source ${CONFIGS}/Docker/.env
 
 clear
 echo "Fetching your settings..."
@@ -21,13 +21,13 @@ show_menus() {
 	MENUSTART
 	echo -n " OS version        : ${LBLUE}" ; lsb_release -ds 2>/dev/null || cat /etc/*release 2>/dev/null | head -n1 || uname -om ; echo -n "${STD}" ; echo -n "${STD}"
 	echo " Timezone          : ${LBLUE}$( cat /etc/timezone )${STD}"
-	echo " Domain            : ${LBLUE}$MYDOMAIN${STD}"
-	echo " Email Address     : ${LBLUE}$MYEMAIL${STD}"
-	echo " IP Address        : ${LBLUE}$IP${STD}"
-	echo " Proxy version     : ${LBLUE}${PROXYVERSION^^}{STD}"
+	echo " Domain            : ${LBLUE}${MYDOMAIN}${STD}"
+	echo " Email Address     : ${LBLUE}${MYEMAIL}${STD}"
+	echo " IP Address        : ${LBLUE}${IP}${STD}"
+	echo " Proxy version     : ${LBLUE}${PROXYVERSION^^}${STD}"
 	echo " Media location    : ${LBLUE}/Media${STD}"
 	echo " Download location : ${LBLUE}/Media/Downloads${STD}"
-	echo " Gooby version     : ${LBLUE}$VERSION${STD}"
+	echo " Gooby version     : ${LBLUE}${VERSION}${STD}"
 	echo
 	echo " ${WHITE}Z${STD} - EXIT to Main Menu"
 	echo " ${LBLUE}"
