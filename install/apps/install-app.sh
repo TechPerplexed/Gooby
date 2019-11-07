4f145f96ca83b62862caa2f1a67281e1e68eb7dd
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-11-07 15:37:14 -0700

    Update install-app.sh

--- install/apps/install-app.sh
+++ install/apps/install-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker ps -q -f name=$APP > ${CONFIGVARS}/checkapp
+docker ps -q -f name=${APP} > ${CONFIGVARS}/checkapp
 clear
 
 if [ -s ${CONFIGVARS}/checkapp ]; then
@@ -21,20 +21,20 @@ else
 		echo "--------------------------------------------------"
 		echo " Please choose what version you want to install:"
 		echo ""
-		[[ -f "/opt/Gooby/scripts/${PROXYVERSION}/$APPLOC.yaml" ]] && echo " ${LYELLOW}S${STD} - $TASK Stable"
-		[[ -f "/opt/Gooby/scripts/${PROXYVERSION}/$APPLOC-beta.yaml" ]] && echo " ${LYELLOW}B${STD} - $TASK Beta"
+		[[ -f "/opt/Gooby/scripts/${PROXYVERSION}/${APPLOC}.yaml" ]] && echo " ${LYELLOW}S${STD} - $TASK Stable"
+		[[ -f "/opt/Gooby/scripts/${PROXYVERSION}/${APPLOC}-beta.yaml" ]] && echo " ${LYELLOW}B${STD} - $TASK Beta"
 		echo "--------------------------------------------------"
 		echo ""
 		read -n 1 -s -r -p " ---> "
 		echo ""
 
 		case "$REPLY" in
-			s|S ) APPLOC=$APPLOC ;;
-			b|B ) [[ -f "/opt/Gooby/scripts/${PROXYVERSION}/$APPLOC-beta.yaml" ]] && APPLOC=$APPLOC-beta ;;
-			* ) APPLOC=$APPLOC ;;
+			s|S ) APPLOC=${APPLOC} ;;
+			b|B ) [[ -f "/opt/Gooby/scripts/${PROXYVERSION}/${APPLOC}-beta.yaml" ]] && APPLOC=${APPLOC}-beta ;;
+			* ) APPLOC=${APPLOC} ;;
 		esac
 
-		if [[ -d $OLDLOC && ! -d $CONFIGS/$TASK ]]; then
+		if [[ -d ${OLDLOC} && ! -d ${CONFIGS}/${TASK} ]]; then
 
 			clear
 			echo "${YELLOW}"
@@ -48,12 +48,12 @@ else
 
 			case "$REPLY" in
 				n|N ) echo "Settings not imported"; echo "You chose to start from scratch" ;;
-				* ) echo "Great, importing settings"; sudo mv $OLDLOC $CONFIGS/$TASK ;;
+				* ) echo "Great, importing settings"; sudo mv ${OLDLOC} ${CONFIGS}/${TASK} ;;
 			esac
 
 		fi
 
-		if [[ $TASK = "Plex" && ! -d $OLDLOC && ! -d $CONFIGS/$TASK ]]; then
+		if [[ $TASK = "Plex" && ! -d ${OLDLOC} && ! -d ${CONFIGS}/${TASK} ]]; then
 
 			clear
 			echo "${YELLOW}"
@@ -64,24 +64,24 @@ else
 			echo " and copy the token to clipboard"
 			echo ""
 			echo " Make sure you activate the token within 4 minutes"
-			echo " at ${LYELLOW}$APP.$MYDOMAIN:8443${YELLOW}"
+			echo " at ${LYELLOW}${APP}.${MYDOMAIN}:8443${YELLOW}"
 			echo "--------------------------------------------------"
 			echo "${STD}"
 			read -e -p " Paste token here: " PLEXCLAIM
 			echo ""
-			echo "$PLEXCLAIM" > ${CONFIGVARS}/plexclaim
+			echo "${PLEXCLAIM}" > ${CONFIGVARS}/plexclaim
 
 		fi
 
 		echo ""
 		cd $CONFIGS/Docker
-		sudo rsync -a /opt/Gooby/scripts/${PROXYVERSION}/$APPLOC.yaml $CONFIGS/Docker/components
-		echo "Just a moment while $APP is being installed..."
+		sudo rsync -a /opt/Gooby/scripts/${PROXYVERSION}/${APPLOC}.yaml $CONFIGS/Docker/components
+		echo "Just a moment while ${APP} is being installed..."
 		source /opt/Gooby/install/misc/environment-build.sh rebuild
 		/usr/local/bin/docker-compose up -d --remove-orphans
 		cd "${CURDIR}"
 
-		if [ $APP == organizr ]; then APP=$ORGMENU; fi
+		if [ ${APP} == organizr ]; then APP=${ORGMENU}; fi
 
 		APPINSTALLED
 
