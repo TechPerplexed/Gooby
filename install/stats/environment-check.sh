09cbd340b1cfa2ba4d5cba92a515a50400e7840a
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-11-07 15:21:06 -0700

    Update environment-check.sh

--- install/stats/environment-check.sh
+++ install/stats/environment-check.sh
@@ -43,7 +43,7 @@ read_options(){
 	read -n 1 -s -r -p "Choose option: " choice
 	case $choice in
 		[Zz]) QUIT ;;
-		*) echo "${LRED}Please select a valid option${STD}" && sleep 2
+		*) echo "${LRED}Please select a valid option${STD}" && sleep 1
 	esac
 }
 
