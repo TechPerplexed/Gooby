1caeee13a346f046985048702d3682f0e681535e
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-11-07 17:22:47 -0700

    Update misc.sh

--- menus/misc.sh
+++ menus/misc.sh
@@ -58,9 +58,9 @@ show_menus() {
 # Read Choices
 
 read_options(){
-	local choice
-	read -n 1 -s -r -p "Choose option: " choice
-	case $choice in
+	local CHOICE
+	read -n 1 -s -r -p "Choose option: " CHOICE
+	case ${CHOICE} in
 		[Aa]) GOOBY ;;
 		[Bb]) UPLOAD ;;
 		[Cc]) RCLEAN ;;
