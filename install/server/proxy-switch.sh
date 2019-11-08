baf381fa4518364cb83774da60c170179f91deba
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-11-08 08:46:14 -0700

    Update proxy-switch.sh

--- install/server/proxy-switch.sh
+++ install/server/proxy-switch.sh
@@ -9,12 +9,12 @@ CONFIRMATION
 if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 	GOAHEAD
-    
-		clear
 
-		echo
-		echo Coming soon! Stay tuned...
-		echo
+	clear
+
+	echo
+	echo " Coming soon! Stay tuned..."
+	echo
 
 	TASKCOMPLETE
 
