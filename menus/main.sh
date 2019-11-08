50109f72412f3b4e0a4fae047b81dc621bdb1768
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2019-11-07 17:22:22 -0700

    Update main.sh

--- menus/main.sh
+++ menus/main.sh
@@ -54,9 +54,9 @@ show_menus() {
 # Read Choices
 
 read_options(){
-	local choice
-	read -n 1 -s -r -p "Choose option: " choice
-	case $choice in
+	local CHOICE
+	read -n 1 -s -r -p "Choose option: " CHOICE
+	case ${CHOICE} in
 		[Aa]) server ;;
 		[Bb]) media ;;
 		[Cc]) download ;;
