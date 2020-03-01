a7d1c0c3338aa9f3e414dcbb2d3e9ad7a5d1be01
doob187 <60312740+doob187@users.noreply.github.com>
2020-03-01 10:05:57 +0100

    Add files via upload

--- install/apps/install-app.sh
+++ install/apps/install-app.sh
@@ -23,6 +23,7 @@ else
 		echo ""
 		[[ -f "/opt/Gooby/scripts/${PROXYVERSION}/${APPLOC}.yaml" ]] && echo " ${LYELLOW}S${STD} - ${TASK} Stable"
 		[[ -f "/opt/Gooby/scripts/${PROXYVERSION}/${APPLOC}-beta.yaml" ]] && echo " ${LYELLOW}B${STD} - ${TASK} Beta"
+        [[ -d "/dev/dri" "/opt/Gooby/scripts/${PROXYVERSION}/${APPLOC}-hwdecode.yaml" ]] && echo " ${LYELLOW}S${STD} - ${TASK} Hardware decode"
 		echo "--------------------------------------------------"
 		echo ""
 		read -n 1 -s -r -p " ---> "
@@ -31,6 +32,7 @@ else
 		case "${REPLY}" in
 			s|S ) APPLOC=${APPLOC} ;;
 			b|B ) [[ -f "/opt/Gooby/scripts/${PROXYVERSION}/${APPLOC}-beta.yaml" ]] && APPLOC=${APPLOC}-beta ;;
+			h|H ) [[ [[ -d "/dev/dri" "/opt/Gooby/scripts/${PROXYVERSION}/${APPLOC}-hwdecode.yaml" ]] && APPLOC=${APPLOC}-hwdecode ;;
 			* ) APPLOC=${APPLOC} ;;
 		esac
 
@@ -82,6 +84,32 @@ else
 		cd "${CURDIR}"
 
 		if [ ${APP} == organizr ]; then APP=${ORGMENU}; fi
+
+        if [[ ${TASK} = "Plex" && -d "/dev/dri"  && ! -d ${CONFIGS}/${TASK} ]]; then
+			clear
+			echo "${YELLOW}"
+			echo "--------------------------------------------------"
+			echo " Plex use the Hardware transcode over iGPU starting "
+			echo " Please wait Plex Docker will be patched"
+			echo "--------------------------------------------------"
+			echo ""
+			intel="$(lspci | grep VGA | cut -d ":" -f3 | awk '{print $1}' | grep Intel)"
+			if [[ "$intel" == "Intel" ]]; then apt-get install intel-gpu-tools -yqq; fi
+			chmod -R 777 /dev/dri
+			docker exec plex apt-get -yqq update
+			docker exec plex apt-get -yqq install i965-va-driver vainfo
+			docker restart plex
+			GPU=$(lspci | grep VGA | cut -d ":" -f3)
+			RAM=$(cardid=$(lspci | grep VGA |cut -d " " -f1);lspci -v -s $cardid | grep " prefetchable"| cut -d "=" -f2)
+			echo "${GREEN}"
+			echo "--------------------------------------------------"
+			echo " Plex Docker patch for Hardware transcode over iGPU finished "
+			echo "--------------------------------------------------"
+			echo " Used " $GPU "and "$RAM "RAM"
+			echo "--------------------------------------------------"
+			echo ""
+
+		fi
 
 		APPINSTALLED
 
--- install/misc/environment-build.sh
+++ install/misc/environment-build.sh
@@ -10,6 +10,14 @@ touch ${ENV}
 #	by line and insert the " around the values.
 
 # Load existing variables and use them as defaults, if available
+touch cat /var/local/Gooby/useragent
+echo ${USER} >/var/local/Gooby/useragent
+uagentrandom="$(cat /var/local/Gooby/useragent)"
+if [[ "$uagentrandom" == ${USER} || "$uagentrandom" == "" || "$uagentrandom" == "rclone/v1.*" ]]; then
+    randomagent=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+    uagent=$(cat /var/plexguide/uagent)
+    echo "$randomagent" >/var/local/Gooby/useragent
+    echo $(sed -e 's/^"//' -e 's/"$//' <<<$(cat /var/plexguide/uagent)) >/var/local/Gooby/useragent; fi
 
 while IFS='' read -r line || [[ -n "${line}" ]]; do
 	newline=$(echo ${line} | sed 's/=/&\"/g; s/:$//')\"
@@ -61,6 +69,14 @@ UPLOADS=/mnt/uploads
 USERID=$(id -u)
 USERNAME=${USER}
 VERSION=$(cat ${CONFIGVARS}/version)
+USERAGENT=$(cat /var/local/Gooby/useragent)
+DIR_CACHE=5m
+CACHE_MODE=writes
+CACHE_MAX_AGE=1h
+CACHE_MAX_SIZE=10G
+HUNK_SIZE_LIMIT=2048M
+READ_CHUNK_SIZE=64M
+BUFFER_SIZE=16M
 EOF
 
 cat ${CONFIGS}/Docker/components/??-* > ${CONFIGS}/Docker/docker-compose.yaml
