73c25c36d64f8047b7ae566a65377f65107178b3
doob187 <amderkum@gmail.com>
2020-03-01 15:38:29 +0100

    update docker install
    update docker-install for debian 10
    add failsafe option over bash command from docker self
    add check for docker is running

--- install/server/docker-install.sh
+++ install/server/docker-install.sh
@@ -3,15 +3,56 @@
 VERSION=$(lsb_release -si)
 echo Distribution is ${VERSION}
 
-sudo apt -y update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common python3-pip acl
+if [[ "$(lsb_release -cs)" == "buster" ]]; then
+    echo -ne '                         (0%)\r'
+    sudo apt-get update -yqq && apt-get autoremove -yqq >/dev/null 2>&1
+        export DEBIAN_FRONTEND=noninteractive
+    echo -ne '#####                    (20%)\r'
+    sudo apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common python3-pip acl -yqq >/dev/null 2>&1
+        export DEBIAN_FRONTEND=noninteractive
+    echo -ne '##########                (40%)\r'
+    curl -fsSL https://download.docker.com/linux/${VERSION,,}/gpg | sudo apt-key add - >/dev/null 2>&1
+        export DEBIAN_FRONTEND=noninteractive
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" >/dev/null 2>&1
+        export DEBIAN_FRONTEND=noninteractive
+    echo -ne '###############            (60%)\r'
+    sudo apt-get update -yqq >/dev/null 2>&1
+        export DEBIAN_FRONTEND=noninteractive
+    echo -ne '####################       (80%)\r'
+    sudo apt-get install docker-ce docker-ce-cli -yqq >/dev/null 2>&1
+        export DEBIAN_FRONTEND=noninteractive
+    echo -ne '#########################    (100%)\r'
+    echo -ne '\n'
+else
+    echo -ne '                         (0%)\r'
+    sudo apt-get update -yqq && apt-get autoremove -yqq >/dev/null 2>&1
+        export DEBIAN_FRONTEND=noninteractive
+    echo -ne '#####                    (20%)\r'
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common python3-pip acl -yqq >/dev/null 2>&1
+        export DEBIAN_FRONTEND=noninteractive
+    echo -ne '##########                (40%)\r'
+    curl -fsSL https://download.docker.com/linux/${VERSION,,}/gpg | sudo apt-key add - >/dev/null 2>&1
+        export DEBIAN_FRONTEND=noninteractive
+    echo -ne '###############            (60%)\r'
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/${VERSION,,} $(lsb_release -cs) stable" >/dev/null 2>&1
+        export DEBIAN_FRONTEND=noninteractive
+    sudo apt-get update -yqq >/dev/null 2>&1
+        export DEBIAN_FRONTEND=noninteractive
+    echo -ne '####################       (80%)\r'
+    sudo apt-get install docker-ce -yqq >/dev/null 2>&1
+        export DEBIAN_FRONTEND=noninteractive
+    echo -ne '#########################    (100%)\r'
+    echo -ne '\n'
+fi
 
-curl -fsSL https://download.docker.com/linux/${VERSION,,}/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/${VERSION,,} $(lsb_release -cs) stable"
+checkdocker=$(sudo systemctl status docker | grep "Active:" | awk '{print $2}')
 
-sudo apt -y update
-apt-cache policy docker-ce
-sudo apt install -y docker-ce
+if [[ ${checkdocker} != "active" ]]; then
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+else 
+   echo "Docker Installed - docker vesion = $(docker --version)"
+fi
 
 # latest docker compose released tag
 
