f73f59454f74057bb3201943ab1cfd0f67c6ec52
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2021-11-14 17:53:39 -0700

    Update docker-install.sh

--- install/server/docker-install.sh
+++ install/server/docker-install.sh
@@ -21,7 +21,7 @@ COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/l
 
 sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
-sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+# sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
 sudo usermod -aG docker ${USER}
 sudo gpasswd -a ${USER} docker 
