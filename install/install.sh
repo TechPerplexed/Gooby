96226fdd70229653757326c5788a06fed7c6f2ae
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2021-11-17 16:24:58 -0700

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -4,66 +4,83 @@ clear
 echo
 echo "--------------------------------------------------"
 echo " This will install Gooby"
-echo " Please sit back while we initialize dependencies"
 echo " For best results, run as user 'root'"
+echo
+echo " Do you wish to install Gooby now? (y/N)? "
 echo "--------------------------------------------------"
-echo; sleep 5
+echo
+read -n 1 -s -r -p " ---> "
 
-# Installing needed apps
+if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
+	echo "--------------------------------------------------"
+	echo " Great! Let's begin."
+	echo " Please sit back while we initialize dependencies"
+	echo "--------------------------------------------------"
 
-APPLIST="acl apt-transport-https ca-certificates curl fail2ban fuse git gpg-agent grsync jq mergerfs nano rsync sqlite3 screen socat ufw unzip wget"
+	# Installing needed apps
 
-for i in ${APPLIST}; do
-	echo Installing $i...
+	sudo apt-get update -y
+	sudo apt-get upgrade -y
+
+	APPLIST="acl apt-transport-https ca-certificates curl fail2ban fuse git gpg-agent grsync jq mergerfs nano rsync sqlite3 screen socat ufw unzip wget"
+
+	for i in ${APPLIST}; do
+		echo Installing $i...
+		echo
+		sudo apt-get -y install $i
+		echo; sleep 2
+	done
+
+	# Setting up firewall
+
+	echo Setting UFW firewall...
 	echo
-	sudo apt-get -y install $i
-	echo; sleep 2
-done
+	sudo ufw default deny incoming
+	sudo ufw default allow outgoing
+	sudo ufw allow ssh
+	sudo ufw --force enable
+	echo
 
-# Setting up firewall
+	# Cloning Gooby from Github
 
-echo Setting UFW firewall...
-echo
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow ssh
-sudo ufw --force enable
-echo
+	sudo rm -r /opt/.Gooby > /dev/null 2>&1
+	sudo git clone -b master https://github.com/TechPerplexed/Gooby /opt/.Gooby > /dev/null 2>&1
 
-# Cloning Gooby from Github
+	if [ -d /opt/.Gooby ]; then
+		sudo rm -r /opt/Gooby > /dev/null 2>&1
+		sudo mv /opt/.Gooby /opt/Gooby
+		sudo chmod +x -R /opt/Gooby/install
+		sudo chmod +x -R /opt/Gooby/menus
+		sudo chmod +x -R /opt/Gooby/scripts/bin
+		sudo chmod +x -R /opt/Gooby/scripts/cron
+		sudo rsync -a /opt/Gooby/scripts/bin/* /bin
+		sudo chmod 755 /bin/gooby
+		sudo chmod 755 /bin/gbackup
+		sudo chmod 755 /bin/plexstats
+		sudo chmod 755 /bin/rclean
+		sudo chmod 755 /bin/resetbackup
+		sudo chmod 755 /bin/rstats
+		sudo chmod 755 /bin/sizer
+		sudo chmod 755 /bin/syncmount
+	fi
 
-sudo rm -r /opt/.Gooby > /dev/null 2>&1
-sudo git clone -b master https://github.com/TechPerplexed/Gooby /opt/.Gooby > /dev/null 2>&1
+	# Finalizing
 
-if [ -d /opt/.Gooby ]; then
-	sudo rm -r /opt/Gooby > /dev/null 2>&1
-	sudo mv /opt/.Gooby /opt/Gooby
-	sudo chmod +x -R /opt/Gooby/install
-	sudo chmod +x -R /opt/Gooby/menus
-	sudo chmod +x -R /opt/Gooby/scripts/bin
-	sudo chmod +x -R /opt/Gooby/scripts/cron
-	sudo rsync -a /opt/Gooby/scripts/bin/* /bin
-	sudo chmod 755 /bin/gooby
-	sudo chmod 755 /bin/gbackup
-	sudo chmod 755 /bin/plexstats
-	sudo chmod 755 /bin/rclean
-	sudo chmod 755 /bin/resetbackup
-	sudo chmod 755 /bin/rstats
-	sudo chmod 755 /bin/sizer
-	sudo chmod 755 /bin/syncmount
+	source /opt/Gooby/menus/variables.sh
+
+	echo "${GREEN}"
+	echo "--------------------------------------------------"
+	echo " Server initialization and dependencies complete!"
+	echo " Type ${WHITE}gooby${GREEN} to continue installation."
+	echo " Visit ${LGREEN}techperplexed.blogspot.com${GREEN} for instructions"
+	echo "--------------------------------------------------"
+	echo "${STD}"
+
+else
+
+	echo "--------------------------------------------------"
+	echo " No worries. You can install Gooby at any time"
+	echo "--------------------------------------------------"
+
 fi
-
-# Finalizing
-
-source /opt/Gooby/menus/variables.sh
-
-echo "${GREEN}"
-echo "--------------------------------------------------"
-echo " Server initialization and dependencies complete!"
-echo " Type ${WHITE}gooby${GREEN} to continue installation."
-echo " Visit ${LGREEN}techperplexed.blogspot.com${GREEN} for instructions"
-echo "--------------------------------------------------"
-echo "${STD}"
