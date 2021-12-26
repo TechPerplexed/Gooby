e42d3a8e42e60738a8796658a45de247fea468bf
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2021-12-26 09:28:57 -0700

    Update rclean.sh

--- scripts/cron/rclean.sh
+++ scripts/cron/rclean.sh
@@ -112,15 +112,13 @@ do
         CODE=$[${CODE}+${?}]
 done
 
-echo
-echo "${LYELLOW}Updating and starting containers${STD}"
-echo
-
 # Future upgrade to v3
 
 source /opt/Gooby/scripts/cron/upgradev3.sh
 
-# Bring up containers
+echo
+echo "${LYELLOW}Updating and starting containers${STD}"
+echo
 
 /usr/local/bin/docker-compose pull
 /usr/local/bin/docker-compose up --remove-orphans --build -d
