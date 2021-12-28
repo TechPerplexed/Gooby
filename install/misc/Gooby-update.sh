c434707a71603c2e917c26558cef5a72b035349d
Tech Perplexed <15312691+TechPerplexed@users.noreply.github.com>
2021-12-28 10:49:09 -0700

    Update Gooby-update.sh

--- install/misc/Gooby-update.sh
+++ install/misc/Gooby-update.sh
@@ -11,7 +11,7 @@ CONFIRMATION
 if [[ ${REPLY} =~ ^[Yy]$ ]]; then
 
 	read -t 3 -p "What Gooby branch? (just wait for default): " GOOBYBRANCH
-	[[ -z ${GOOBYBRANCH} ]] && GOOBYBRANCH=${GOOBYBRANCH}
+	[[ -z ${GOOBYBRANCH} ]] && GOOBYBRANCH=v2
 	echo "${GOOBYBRANCH}" > ${CONFIGVARS}/goobybranch
 	echo
 	echo
